7627ed646e99773c447001d39a339bca66f8098b
jkarminski <jkarminski@wikia-inc.com>
2016-12-05 00:31:08 +0100

    add basic routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () { return redirect('/course/1'); } );
+
+Route::get( '/course/{cid}', '' );
+
+Route::get( '/course/1/module/{mid}', '' );
+
+Route::get( '/course/1/module/{mid}/topic/{tid}', '' );
+

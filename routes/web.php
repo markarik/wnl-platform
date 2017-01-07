84d0760d9af07efc00dcd1110a0a79040e1dd995
jkarminski <jkarminski@wikia-inc.com>
2017-01-07 11:45:58 +0100

    Add payment routes

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,21 @@
 
 Auth::routes();
 
-Route::get( '/', function () { return redirect('/course/1'); } );
+Route::get('/payment/step1', 'Payment\StepOneController@index');
+Route::get('/payment/step2', 'Payment\StepTwoContoller@index');
+Route::post('/payment/step2', 'Payment\StepTwoContoller@handle');
+Route::get('/payment/step3', 'Payment\PaymentController@step3');
 
-Route::get('/course/1', 'HomeController@index');
+Route::group(['middleware' => 'auth'], function () {
 
-//Route::get( '/course/1/module/{moduleId}/chapter/{chapterId}', 'ModuleController@index' );
+	Route::get('/', function() { return redirect('/course/1'); });
 
-Route::get( '/course/1/module/{moduleId}/chapter/{chapterId}/section/{sectionId}', 'ChapterController@index' );
+	Route::get('/course/1', 'Course\CourseController@index');
 
+	//Route::get( '/course/1/module/{moduleId}/chapter/{chapterId}', 'ModuleController@index' );
+
+	Route::get('/course/1/module/{moduleId}/chapter/{chapterId}/section/{sectionId}', 'ChapterController@index');
+
+	Route::get('/dashboard', 'User\DashboardController@index');
+
+});

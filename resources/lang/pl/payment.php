a64e67755ac9a7001e63415ac410578f38bea78c
kubakarminski <jlkarminski@gmail.com>
2017-01-14 19:14:13 +0100

    Add payment translations;

--- resources/lang/pl/payment.php
+++ resources/lang/pl/payment.php
@@ -2,27 +2,23 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reset Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
-
-	'back-button-content' => 'Powrót',
-	'back-to-website' => 'Powrót do strony kursu',
-	'bank-transfer-button' => 'Płacę przelewem bankowym',
-	'online-payment-button' => 'Płacę szybkim przelewem',
-	'personal-info-title' => 'Świetnie, teraz pora się poznać!',
-	'personal-info-account-data' => 'Do zalogowania będziesz używać swojego maila oraz hasła.',
-	'personal-info-adress' => 'Na jakie dane i adres mamy wysłać materiały?',
-	'personal-info-consents-title' => 'Aby zgodnie z prawem zrealizować zamówienie, potrzebujemy Twojej zgody na przetwarzanie danych osobowych.',
-	'personal-info-consent-optional' => 'Ostatnia zgoda nie jest obowiązkowa, ale pozwoli Ci otrzymywać informacje o kursie, nowych wpisach na blogu i produktach. Nie obawiaj się spamu! Będziemy pisać tylko z wartościowymi informacjami.',
-	'personal-info-confirm-button-content' => 'Przejdź dalej',
-	'your-personal-info' => 'Twoje dane:',
+    'back-button-content' => 'Powrót',
+    'back-to-website' => 'Powrót do strony kursu',
+    'bank-transfer-button' => 'Płacę przelewem bankowym',
+    'online-payment-button' => 'Płacę szybkim przelewem',
+    'personal-info-title' => 'Świetnie, teraz pora się poznać!',
+    'personal-info-account-data' => 'Do zalogowania będziesz używać swojego maila oraz hasła.',
+    'personal-info-adress' => 'Na jakie dane i adres mamy wysłać materiały?',
+    'personal-info-consents-title' => 'Aby zgodnie z prawem zrealizować zamówienie, potrzebujemy Twojej zgody na przetwarzanie danych osobowych.',
+    'personal-info-consent-optional' => 'Ostatnia zgoda nie jest obowiązkowa, ale pozwoli Ci otrzymywać informacje o kursie, nowych wpisach na blogu i produktach. Nie obawiaj się spamu! Będziemy pisać tylko z wartościowymi informacjami.',
+    'personal-info-confirm-button-content' => 'Przejdź dalej',
+    'your-personal-info' => 'Twoje dane:',
+    'first-name' => 'Imię',
+    'last-name' => 'Nazwisko',
+    'address' => 'Ulica i numer domu / mieszkania',
+    'zip' => 'Kod pocztowy',
+    'city' => 'Miasto',
+    'email' => 'Adres e-mail',
+    'edit-account' => 'Popraw dane',
 
 ];

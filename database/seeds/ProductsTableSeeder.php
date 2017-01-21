f25fec435edd625d9e9adc4f71a2b7ac115a4607
kubakarminski <jlkarminski@gmail.com>
2017-01-21 22:32:22 +0100

    Update seeders;

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -4,21 +4,23 @@ use Illuminate\Database\Seeder;
 
 class ProductsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('products')->insert([
-            'name'  => 'Więcej niż LEK (Kurs internetowy + warsztaty)',
-            'price' => 2000.00,
-        ]);
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('products')->insert([
+			'name'  => 'Więcej niż LEK (Kurs internetowy + warsztaty)',
+			'price' => 2000.00,
+			'slug'  => 'wnl-online-onsite',
+		]);
 
-        DB::table('products')->insert([
-            'name'  => 'Więcej niż LEK (Kurs internetowy)',
-            'price' => 1.00,
-        ]);
-    }
+		DB::table('products')->insert([
+			'name'  => 'Więcej niż LEK (Kurs internetowy)',
+			'price' => 1.00,
+			'slug'  => 'wnl-online',
+		]);
+	}
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,25 +4,29 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('users')->insert([
-            'first_name' => 'Kuba',
-            'last_name'  => 'Karmiński',
-            'email'      => 'jlkarminski@gmail.com',
-            'password'   => bcrypt('secret'),
-        ]);
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('users')->insert([
+			'first_name' => encrypt('Kuba'),
+			'last_name'  => encrypt('Karmiński'),
+			'email'      => 'jlkarminski@gmail.com',
+			'phone'      => encrypt('000000000'),
+			'address'    => encrypt(''),
+			'password'   => bcrypt('secret'),
+		]);
 
-        DB::table('users')->insert([
-            'first_name' => 'Adam',
-            'last_name'  => 'Karmiński',
-            'email'      => 'adamkarminski@gmail.com',
-            'password'   => bcrypt('secret'),
-        ]);
-    }
+		DB::table('users')->insert([
+			'first_name' => encrypt('Adam'),
+			'last_name'  => encrypt('Karmiński'),
+			'email'      => 'adamkarminski@gmail.com',
+			'phone'      => encrypt('000000000'),
+			'address'    => encrypt(''),
+			'password'   => bcrypt('secret'),
+		]);
+	}
 }

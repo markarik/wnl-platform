66a25ecd2599ca01b4ea4c3039e6e7f9d320a5b2
kubakarminski <jlkarminski@gmail.com>
2017-01-21 18:52:19 +0100

    Refactor payment step 2 variables;

--- app/Http/Controllers/Payment/StepThreeController.php
+++ app/Http/Controllers/Payment/StepThreeController.php
@@ -11,66 +11,56 @@ use Illuminate\Support\Facades\Auth;
 
 class StepThreeController extends Controller
 {
-    public function index(Payment $payment) {
+	public function index(Payment $payment)
+	{
 
-        $user = Auth::user();
+		$user = Auth::user();
 
-        if (!$user){
-            return redirect(url('/payment/step1'));
-        }
+		if (!$user) {
+			return redirect(url('/payment/step1'));
+		}
 
-        $order = $user->orders()->recent();
+		$order = $user->orders()->recent();
 
-        $order = [
-            'session_id'  => $order->session_id,
-            'merchant_id' => config('przelewy24.merchant_id'),
-            'amount'      => (int) $order->product->price * 100,
-            'currency'    => 'PLN',
-            'description' => $order->product->name,
-            'client'      => $user->full_name,
-            'address'     => $user->address,
-            'zip'         => $user->zip,
-            'city'        => $user->city,
-            'country'     => 'PL',
-            'email'       => $user->email,
-            'language'    => 'pl',
-            'url_return'  => url('/profile/orders'),
-            'url_status'  => url('/payment/status'),
-        ];
+		$checksum = $payment::generateChecksum($order->session_id, (int)$order->product->price * 100);
 
-        $order['sign'] = $payment::generateChecksum($order['session_id'], $order['amount']);
+		return view('payment.step3', [
+			'order'    => $order,
+			'user'     => $user,
+			'checksum' => $checksum,
+		]);
+	}
 
-        return view('payment.step3', [ 'order' => $order ]);
-    }
+	public function handle(Request $request)
+	{
+		$user = Auth::user();
+		$order = $user->orders()->recent();
+		$order->method = $request->input('method');
+		$order->save();
 
-    public function handle(Request $request) {
-        $user = Auth::user();
-        $order = $user->orders()->recent();
-        $order->method = $request->input('method');
-        $order->save();
+		return redirect(url('/profile/orders'));
+	}
 
-        return redirect(url('/profile/orders'));
-    }
+	public function status(Request $request, Payment $payment)
+	{
+		//TODO: IP filtering
+		Log::debug('request:' . json_encode($request->request->all(), JSON_PRETTY_PRINT));
+		Log::debug('headers:' . json_encode($request->headers->all(), JSON_PRETTY_PRINT));
 
-    public function status(Request $request, Payment $payment){
-        //TODO: IP filtering
-        Log::debug('request:' . json_encode($request->request->all(), JSON_PRETTY_PRINT));
-        Log::debug('headers:' . json_encode($request->headers->all(), JSON_PRETTY_PRINT));
+		$transactionValid = $payment->verifyTransaction(
+			$request->get('p24_session_id'),
+			$request->get('p24_amount'),
+			$request->get('p24_order_id')
+		);
 
-        $transactionValid = $payment->verifyTransaction(
-            $request->get('p24_session_id'),
-            $request->get('p24_amount'),
-            $request->get('p24_order_id')
-        );
+		if ($transactionValid) {
+			$order = Order::where(['session_id' => $request->get('p24_session_id')])->first();
+			$order->paid = true;
+			$order->external_id = $request->get('p24_order_id');
+			$order->transfer_title = $request->get('p24_statement');
+			$order->save();
+		}
 
-        if ($transactionValid){
-            $order = Order::where(['session_id' => $request->get('p24_session_id')])->first();
-            $order->paid = true;
-            $order->external_id = $request->get('p24_order_id');
-            $order->transfer_title = $request->get('p24_statement');
-            $order->save();
-        }
-
-    }
+	}
 
 }
--- resources/views/payment/step3.blade.php
+++ resources/views/payment/step3.blade.php
@@ -1,89 +1,91 @@
 @extends('layouts.payment')
 
 @section('content')
-<div class="row">
-	<div class="col-xs-12">
-		<h2 class="text-center">Czy wszystko się zgadza?</h2>
+	<div class="row">
+		<div class="col-xs-12">
+			<h2 class="text-center">Czy wszystko się zgadza?</h2>
 
-		<div class="panel panel-default">
-			<div class="panel-heading">@lang('payment.personal-data-order-heading')</div>
-			<div class="panel-body">
-				<p class="lead">Więcej niż LEK - Kurs internetowy + stacjonarny</p>
-				<p>2000zł netto</p>
+			<div class="panel panel-default">
+				<div class="panel-heading">@lang('payment.personal-data-order-heading')</div>
+				<div class="panel-body">
+					<p class="lead">{{ $order->product->name }}</p>
+					<p>{{ $order->product->price }}zł netto</p>
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">@lang('payment.personal-data-basic-heading')</div>
+				<div class="panel-body">
+					<p class="lead">{{ $user->email }}</p>
+					<ul class="list-unstyled">
+						<li><strong>{{ $user->full_name }}</strong></li>
+						<li>{{ $user->address }}</li>
+						<li>{{ $user->zip }}, {{ $user->city }}</li>
+					</ul>
+				</div>
+			</div>
+
+			@if($user->invoice)
+				<div class="panel panel-default">
+					<div class="panel-heading">@lang('payment.personal-data-invoice-heading')</div>
+					<div class="panel-body">
+						<ul class="list-unstyled">
+							<li><strong>{{ $user->invoice_name }}</strong></li>
+							<li>{{ $user->invoice_address }}</li>
+							<li>{{ $user->invoice_zip }}, {{ $user->invoice_city }}</li>
+							<li>{{ $user->invoice_country }}</li>
+						</ul>
+					</div>
+				</div>
+			@endif
+
+			<div>
+				<p class="text-center">
+					<a href="{{ url('/payment/step2') }}">@lang('payment.edit-account')</a>
+				</p>
 			</div>
 		</div>
+	</div>
 
-		<div class="panel panel-default">
-			<div class="panel-heading">@lang('payment.personal-data-basic-heading')</div>
-			<div class="panel-body">
-				<p class="lead">{{$order['email']}}</p>
-				<ul class="list-unstyled">
-					<li><strong>{{$order['client']}}</strong></li>
-					<li>{{$order['address']}}</li>
-					<li>{{$order['zip']}}, {{$order['city']}}</li>
-				</ul>
-			</div>
+	<hr>
+
+	<div class="row text-center">
+		<div class="col-xs-12">
+			<h2>Jeżeli tak, to wszystko gotowe!</h2>
+			<p class="lead">Kliknij&nbsp;na&nbsp;jeden&nbsp;z&nbsp;przycisków, aby&nbsp;wybrać&nbsp;metodę&nbsp;płatności&nbsp;i&nbsp;złożyć&nbsp;zamówienie.</p>
 		</div>
-
-		<div class="panel panel-default">
-			<div class="panel-heading">@lang('payment.personal-data-invoice-heading')</div>
-			<div class="panel-body">
-				<ul class="list-unstyled">
-					<li><strong>bethink s.c.</strong></li>
-					<li>{{$order['address']}}</li>
-					<li>{{$order['zip']}}, {{$order['city']}}</li>
-					<li>Polska</li>
-				</ul>
-			</div>
+		<div class="col-xs-12 col-md-6">
+			<form action="{{url('/payment/step3')}}" method="post">
+				{!! csrf_field() !!}
+				<input type="hidden" name="method" value="transfer"/>
+				<button class="btn btn-default">@lang('payment.bank-transfer-button')</button>
+			</form>
 		</div>
+		<div class="col-xs-12 col-md-6">
+			<form action="{{ config('przelewy24.transaction_url') }}" method="post" class="p24_form">
 
-		<div>
-			<p class="text-center">
-				<a href="{{ url('/payment/step2') }}">@lang('payment.edit-account')</a>
-			</p>
+				<input type="hidden" name="p24_session_id" value="{{ $order->session_id }}"/>
+				<input type="hidden" name="p24_merchant_id" value="{{ config('przelewy24.merchant_id') }}"/>
+				<input type="hidden" name="p24_pos_id" value="{{ config('przelewy24.merchant_id') }}"/>
+				<input type="hidden" name="p24_amount" value="{{ (int)$order->product->price * 100 }}"/>
+				<input type="hidden" name="p24_currency" value="PLN"/>
+				<input type="hidden" name="p24_description" value="{{ $order->product->name }}"/>
+				<input type="hidden" name="p24_client" value="{{ $user->full_name }}"/>
+				<input type="hidden" name="p24_address" value="{{ $user->address }}"/>
+				<input type="hidden" name="p24_zip" value="{{ $user->zip }}"/>
+				<input type="hidden" name="p24_city" value="{{ $user->city }}"/>
+				<input type="hidden" name="p24_country" value="PL"/>
+				<input type="hidden" name="p24_email" value="{{ $user->email }}"/>
+				<input type="hidden" name="p24_language" value="pl"/>
+				<input type="hidden" name="p24_url_return" value="{{ url('/profile/orders') }}"/>
+				<input type="hidden" name="p24_url_status" value="{{ url('/payment/status')  }} "/>
+				<input type="hidden" name="p24_api_version" value="{{config('przelewy24.api_version')}}"/>
+				<input type="hidden" name="p24_sign" value="{{ $checksum }}"/>
+
+			</form>
+
+			<button class="btn btn-primary p24_submit">@lang('payment.online-payment-button')</button>
 		</div>
 	</div>
-</div>
-
-<hr>
-
-<div class="row text-center">
-	<div class="col-xs-12">
-		<h2>Jeżeli tak, to wszystko gotowe!</h2>
-		<p class="lead">Kliknij&nbsp;na&nbsp;jeden&nbsp;z&nbsp;przycisków, aby&nbsp;wybrać&nbsp;metodę&nbsp;płatności&nbsp;i&nbsp;złożyć&nbsp;zamówienie.</p>
-	</div>
-	<div class="col-xs-12 col-md-6">
-		<form action="{{url('/payment/step3')}}" method="post">
-			{!! csrf_field() !!}
-			<input type="hidden" name="method" value="transfer" />
-			<button class="btn btn-default">@lang('payment.bank-transfer-button')</button>
-		</form>
-	</div>
-	<div class="col-xs-12 col-md-6">
-		<form action="{{ config('przelewy24.transaction_url') }}" method="post" class="p24_form">
-
-			<input type="hidden" name="p24_session_id" value="{{$order['session_id']}}"/>
-			<input type="hidden" name="p24_merchant_id" value="{{$order['merchant_id']}}"/>
-			<input type="hidden" name="p24_pos_id" value="{{$order['merchant_id']}}"/>
-			<input type="hidden" name="p24_amount" value="{{$order['amount']}}"/>
-			<input type="hidden" name="p24_currency" value="{{$order['currency']}}"/>
-			<input type="hidden" name="p24_description" value="{{$order['description']}}"/>
-			<input type="hidden" name="p24_client" value="{{$order['client']}}"/>
-			<input type="hidden" name="p24_address" value="{{$order['address']}}"/>
-			<input type="hidden" name="p24_zip" value="{{$order['zip']}}"/>
-			<input type="hidden" name="p24_city" value="{{$order['city']}}"/>
-			<input type="hidden" name="p24_country" value="{{$order['country']}}"/>
-			<input type="hidden" name="p24_email" value="{{$order['email']}}"/>
-			<input type="hidden" name="p24_language" value="{{$order['language']}}"/>
-			<input type="hidden" name="p24_url_return" value="{{$order['url_return']}}"/>
-			<input type="hidden" name="p24_url_status" value="{{$order['url_status']}}"/>
-			<input type="hidden" name="p24_api_version" value="{{config('przelewy24.api_version')}}"/>
-			<input type="hidden" name="p24_sign" value="{{$order['sign']}}"/>
-
-		</form>
-
-		<button class="btn btn-primary p24_submit">@lang('payment.online-payment-button')</button>
-	</div>
-</div>
 
 @endsection

3b9722e6947c660db44deb423f8d9f1cc9207006
adamkarminski <adamk@wikia-inc.com>
2017-02-08 12:51:14 +0100

    Styleguide is ready to use

---
+++ resources/views/styleguide.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<h2>Typography</h2>
+
+		<h1>h1. Zażółć gęślą jaźń</h1>
+		<h2>h2. Zażółć gęślą jaźń</h2>
+		<h3>h3. Zażółć gęślą jaźń</h3>
+		<h4>h4. Zażółć gęślą jaźń</h4>
+		<h5>h5. Zażółć gęślą jaźń</h5>
+
+		<h2>Buttons</h2>
+		<div class="block">
+			<a class="button">Button</a>
+			<a class="button is-white">White</a>
+			<a class="button is-light">Light</a>
+			<a class="button is-dark">Dark</a>
+			<a class="button is-black">Black</a>
+			<a class="button is-link">Link</a>
+		</div>
+		<div class="block">
+			<a class="button is-primary">Primary</a>
+			<a class="button is-info">Info</a>
+			<a class="button is-success">Success</a>
+			<a class="button is-warning">Warning</a>
+			<a class="button is-danger">Danger</a>
+		</div>
+	</div>
+@endsection
+

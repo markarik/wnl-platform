f44afa6c79a7fbe572844869d0969b6da9aaefe9
kubakarminski <jlkarminski@gmail.com>
2017-02-09 12:50:39 +0100

    Switch to Laravel Mix;

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -23,11 +23,12 @@ require('vue-resource');
  * included with Laravel will automatically verify the header's value.
  */
 
-Vue.http.interceptors.push((request, next) => {
-    request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
+window.axios = require('axios');
 
-    next();
-});
+window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+};
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening

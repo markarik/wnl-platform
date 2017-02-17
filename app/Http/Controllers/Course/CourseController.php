92a787b83cd67dfd553907a478d7802abf48a4ae
adamkarminski <adamkarminski@gmail.com>
2017-02-17 18:24:50 +0100

    Add transfering PHP vars to JS

--- app/Http/Controllers/Course/CourseController.php
+++ app/Http/Controllers/Course/CourseController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Course;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use JavaScript;
 
 class CourseController extends Controller
 {
-    public function index(){
-    	return view('course.course');
+	public function index() {
+		return view('course.course');
 	}
 }

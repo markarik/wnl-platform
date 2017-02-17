661ad046826d4972227cab3e78a041906ad4abea
adamkarminski <adamkarminski@gmail.com>
2017-02-17 13:38:01 +0100

    Correctly display static data in Navbar

--- resources/assets/js/components/global/Navbar.vue
+++ resources/assets/js/components/global/Navbar.vue
@@ -1,51 +1,54 @@
 <template>
-    <nav class="nav has-shadow">
-        <div class="container">
-            <div class="nav-left">
-                <!--<a class="nav-item" href="{{ route('home') }}">-->
-                    <!--<img src="{{ asset('/images/wnl-logo.svg') }}" alt="Logo Więcej niż LEK">-->
-                <!--</a>-->
-            </div>
+	<nav class="nav has-shadow">
+		<div class="columns is-gapless wnl-nav-container">
+			<div class="column wnl-nav-column">
+				<div class="wnl-left-content wnl-nav-item">
+					<!-- TODO: Load data dynamically -->
+					<a class="wnl-logo-link" href="wnl-platform.app/course/1">
+						<img src="http://wnl-platform.app/images/wnl-logo.svg" alt="Logo Więcej niż LEK">
+					</a>
+				</div>
+			</div>
+			<div class="column is-half wnl-nav-column">
 
-            <span class="nav-toggle">
-                <span></span>
-                <span></span>
-                <span></span>
-            </span>
-
-            <div class="nav-right nav-menu">
-                <!--&lt;!&ndash; Authentication Links &ndash;&gt;-->
-                <!--@if (Auth::guest())-->
-                <!--<a href="{{ url('/login') }}" class="nav-item">-->
-                    <!--@lang('nav.login-link-content')-->
-                <!--</a>-->
-                <!--@else-->
-                <!--<a href="{{ url('/logout') }}" class="nav-item"-->
-                   <!--onclick="event.preventDefault();-->
-                                         <!--document.getElementById('logout-form').submit();">-->
-                    <!--@lang('nav.logout-link-content')-->
-                <!--</a>-->
-
-                <!--<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">-->
-                    <!--{{ csrf_field() }}-->
-                <!--</form>-->
-                <!--@endif-->
-                <div class="nav-item">
-                  {{ currentUserName }}
-                </div>
-            </div>
-        </div>
-    </nav>
+			</div>
+			<div class="column wnl-nav-column">
+				<div class="nav-menu wnl-nav-item">
+					{{ currentUserName }}
+				</div>
+			</div>
+		</div>
+	</nav>
 </template>
 
+<style lang="sass">
+	.wnl-nav-column
+		height: 100%
+
+	.wnl-nav-item
+		align-items: center
+		display: flex
+		height: 100%
+
+	.wnl-nav-container
+		width: 100%
+
+	.wnl-logo-link
+		display: block
+		max-width: 175px
+
+		img
+			display: block
+</style>
+
 <script>
-    export default {
-        name: 'Navbar',
-        computed: {
-            currentUserName () {
-                const currentUser = this.$store.state.users.current
-                return currentUser.first_name + ' ' + currentUser.last_name
-            }
-        }
-    }
+	export default {
+		name: 'Navbar',
+		computed: {
+			currentUserName () {
+				const currentUser = this.$store.state.users.current
+				return currentUser.first_name + ' ' + currentUser.last_name
+			}
+		}
+	}
 </script>

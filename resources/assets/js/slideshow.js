583d09fecc698c0ddfafde75b80330b127b87a35
adamkarminski <adamkarminski@gmail.com>
2017-03-03 14:11:39 +0100

    Update route on slide change

--- resources/assets/js/slideshow.js
+++ resources/assets/js/slideshow.js
@@ -1,20 +1,21 @@
 import Postmate from 'postmate'
 
 const Reveal = require('../../vendor/reveal/reveal.js')
+const handshake = new Postmate.Model({
+	goToSlide: (slideNumber) => {
+		Reveal.slide(slideNumber)
+	}
+})
+
+handshake.then(parent => {
+	parent.emit('loaded', true)
+});
 
 Reveal.initialize({
+	embedded: true,
 	slideNumber: true,
 	overview: false,
 	transition: 'none',
+	postMessage: true,
+	postMessageEvents: true
 })
-
-const handshake = new Postmate.Model({
-	goToSlide: (slideNumber) => {
-		console.log('slideNumber ' + slideNumber)
-		Reveal.slide(slideNumber)
-	}
-});
-
-handshake.then(parent => {
-	parent.emit('loaded', true);
-});

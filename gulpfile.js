0a7753ca12a1e315a24edf70d63f9d79ebecaa35
adamkarminski <adamkarminski@gmail.com>
2017-03-06 14:40:33 +0100

    Add fullscreen functionality

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,8 @@ svgSprite = require('gulp-svg-sprite');
 var config = {
 	mode: {
 		symbol: { // symbol mode to build the SVG
-			render: {
-				css: false, // CSS output option for icon sizing
-				scss: false // SCSS output option for icon sizing
-			},
 			dest: '', // destination folder
-			prefix: '.svg--%s', // BEM-style prefix if styles rendered
-			sprite: 'sprite.svg', //generated sprite name
-			example: true // Build a sample page, please!
+			sprite: 'sprite.svg' //generated sprite name
 		}
 	}
 }

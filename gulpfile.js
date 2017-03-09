9be72aba44a919792ce78354537d3eedf0ce2630
kubakarminski <jlkarminski@gmail.com>
2017-03-09 18:28:59 +0100

    Unit tests configuration + auto-run with gulp!

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+const gulp      = require('gulp'),
+	  svgSprite = require('gulp-svg-sprite'),
+	  phpunit   = require('gulp-phpunit'),
+	  notify    = require('gulp-notify');
 
-var config = {
+const config = {
 	mode: {
 		symbol: { // symbol mode to build the SVG
 			dest: '', // destination folder
@@ -10,8 +12,32 @@ var config = {
 	}
 }
 
-gulp.task('svg', function () {
+gulp.task('svg', () => {
 	return gulp.src('**/*.svg', {cwd: 'resources/assets/svg'})
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('public/images/icons'));
+});
+
+gulp.task('test', () => {
+	const param   = process.argv[3],
+		  options = {
+			  debug: true,
+			  statusLine: false,
+			  notify: false
+		  };
+	gulp.src('./phpunit.xml')
+		.pipe(phpunit(`./vendor/bin/phpunit ${param}`, options))
+		.on('error', notify.onError({
+			title: 'Dupa',
+			message: 'Coś nie tak',
+			// icon: __dirname + '/fail.png'
+		}))
+		.pipe(notify({
+			title: 'OK',
+			message: 'Zajebisty kod milordzie.'
+		}));
+});
+
+gulp.task('watchtest', () => {
+	gulp.watch(['tests/Unit/*.php', 'tests/Api/*.php', 'app/**/*.php'], ['test']);
 });

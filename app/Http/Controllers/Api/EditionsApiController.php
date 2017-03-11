70a1bbeb3b7ee4e99bf554c152e6689c307069e9
kubakarminski <jlkarminski@gmail.com>
2017-03-11 01:49:00 +0100

    Edition api controller cleanup;

--- app/Http/Controllers/Api/EditionsApiController.php
+++ app/Http/Controllers/Api/EditionsApiController.php
@@ -4,12 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\Transformers\EditionTransformer;
 use App\Models\Edition;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
-use League\Fractal\Serializer\ArraySerializer;
-use League\Fractal\Serializer\DataArraySerializer;
 
 class EditionsApiController extends ApiController
 {
@@ -19,75 +14,13 @@ class EditionsApiController extends ApiController
 	 */
 	public function get($editionId)
 	{
-		$data = [];
-
-		return response()->json($data);
-	}
-
-	/**
-	 * @param $editionId
-	 * @param $lessonId
-	 * @return \Illuminate\Http\JsonResponse
-	 */
-	public function getWithLessonAvailability($editionId, $lessonId)
-	{
-		$data = [
-			'gorup' => [
-				'lessons' => [
-					[
-						'id'          => 1,
-						'name'        => '123',
-						'isAvailable' => true,
-					],
-				],
-			],
-		];
-
-		return response()->json();
-	}
-
-	/**
-	 * @param $editionId
-	 * @param $userId
-	 * @return \Illuminate\Http\JsonResponse
-	 */
-	public function getWithUserProgress($editionId, $userId)
-	{
-		$data = [
-			'lessons' => [
-				1 => [
-					'status' => 'done',
-					'route'  => [],
-				],
-			],
-		];
-
-		return response()->json($data);
-	}
-
-	public function putUserProgress($editionId, $userId)
-	{
-		$data = [];
-		return response('Created', 201)->json($data);
-//		return response('OK', 200)->json($data);
-	}
-
-	public function getStructure($editionId)
-	{
 		$this->fractal->parseIncludes('groups.lessons');
 		$edition = Edition::find($editionId);
 		$resource = new Item($edition, new EditionTransformer, 'edition');
 
 		$data = $this->fractal->createData($resource)->toArray();
 
-//		return response(($data))->header('Content-Type', 'html; charset=utf-8');
 		return response()->json($data);
-	}
-
-	protected function edition($id)
-	{
-
-
 	}
 
 }

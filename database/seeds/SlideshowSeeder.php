ccec48b78892c096d4f5bdbb9d38b4b4f0c53556
kubakarminski <jlkarminski@gmail.com>
2017-03-14 13:19:25 +0100

    Add slideshow seeder;

--- database/seeds/SlideshowSeeder.php
+++ database/seeds/SlideshowSeeder.php
@@ -1,16 +1,18 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Lib\SlideParser\Parser;
 
 class SlideshowSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+	/**
+	 * Run the database seeds.
+	 *
+	 * @param Parser $parser
+	 */
+    public function run(Parser $parser)
     {
-        //
+        $html = Storage::get('example_slideshow.html');
+		$parser->parse($html);
     }
 }

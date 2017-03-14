3a9f3f5afa7278fef0aa2733b99ddb15b11e8f51
kubakarminski <jlkarminski@gmail.com>
2017-03-14 15:18:38 +0100

    Add migrations;

---
+++ database/migrations/2017_03_14_132239_create_coupons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCouponsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('coupons', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('code');
+			$table->enum('type', ['percentage', 'amount']);
+			$table->float('value');
+			$table->string('slug')->unique()->nullable();
+			$table->timestamp('expires_at');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('coupons');
+	}
+}

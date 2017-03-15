dff75a89d0581f11673301db935217264ef19053
kubakarminski <jlkarminski@gmail.com>
2017-03-15 20:41:37 +0100

    Fix internal error occurring for unknown routes;

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -11,62 +11,65 @@ use App;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
-    ];
+	/**
+	 * A list of the exception types that should not be reported.
+	 *
+	 * @var array
+	 */
+	protected $dontReport = [
+		\Illuminate\Auth\AuthenticationException::class,
+		\Illuminate\Auth\Access\AuthorizationException::class,
+		\Symfony\Component\HttpKernel\Exception\HttpException::class,
+		\Illuminate\Database\Eloquent\ModelNotFoundException::class,
+		\Illuminate\Session\TokenMismatchException::class,
+		\Illuminate\Validation\ValidationException::class,
+	];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
+	/**
+	 * Report or log an exception.
+	 *
+	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+	 *
+	 * @param  \Exception $exception
+	 * @return void
+	 */
+	public function report(Exception $exception)
+	{
+		parent::report($exception);
+	}
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-    	if ($request->route()->getPrefix() === '/payment' && App::environment('production')){
-    		Log::critical('Exception on payment path: ' . $exception->getMessage() . ' in ' . $exception->getFile());
+	/**
+	 * Render an exception into an HTTP response.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \Exception $exception
+	 * @return \Illuminate\Http\Response
+	 */
+	public function render($request, Exception $exception)
+	{
+		if (!is_null($request->route()) &&
+			$request->route()->getPrefix() === '/payment' &&
+			App::environment('production')
+		) {
+			Log::critical('Exception on payment path: ' . $exception->getMessage() . ' in ' . $exception->getFile());
 		}
 
-        return parent::render($request, $exception);
-    }
+		return parent::render($request, $exception);
+	}
 
-    /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
+	/**
+	 * Convert an authentication exception into an unauthenticated response.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \Illuminate\Auth\AuthenticationException $exception
+	 * @return \Illuminate\Http\Response
+	 */
+	protected function unauthenticated($request, AuthenticationException $exception)
+	{
+		if ($request->expectsJson()) {
+			return response()->json(['error' => 'Unauthenticated.'], 401);
+		}
 
-        return redirect()->guest('login');
-    }
+		return redirect()->guest('login');
+	}
 }

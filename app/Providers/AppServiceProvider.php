290f6a5b6d750d7ce029dfb9c465098d6cc893c0
kubakarminski <jlkarminski@gmail.com>
2017-03-20 18:58:44 +0100

    Log fix;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\ServiceProvider;
 use Laravel\Dusk\DuskServiceProvider;
 use Barryvdh\Debugbar\ServiceProvider as DebugBarServiceProvider;
 use Laravel\Tinker\TinkerServiceProvider;
+use Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Order::observe(OrderObserver::class);
+
+		// Send slack notifications when a critical or higher level error occurs
+		$monolog = Log::getMonolog();
+		$token = env('ERROR_REPORTER_SLACK_TOKEN');
+		$chanel = env('ERROR_REPORTER_SLACK_CHANNEL');
+		$level = \Monolog\Logger::CRITICAL;
+		$slackHandler = new \Monolog\Handler\SlackHandler($token, $chanel, 'Error Reporter', true, null, $level);
+		$monolog->pushHandler($slackHandler);
     }
 
     /**

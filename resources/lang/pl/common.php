b4b9dae49c588299739cce18148befb789288bc7
adamkarminski <adamkarminski@gmail.com>
2017-03-26 18:53:06 +0200

    Add more info to checkout footer

--- resources/lang/pl/common.php
+++ resources/lang/pl/common.php
@@ -17,7 +17,11 @@ return [
 	'nav-user-dropdown-title' => 'Gdzie Cię zabrać?',
 	'your-profile-link-content' => 'Twój profil',
 	'mobile-nav-toggle' => 'Przełącz mobilną nawigację',
-	'footer-copy' => '&copy; by bethink',
+	'footer-copy' => '
+		&copy; by bethink sp. z o.o.<br>
+		ul. Henryka Sienkiewicza 8/1<br>
+		60-817, Poznań<br>
+		NIP: 781-194-37-56, KRS: 0000668811',
 	'percent' => ':value%',
 	'currency' => ':valuezł',
 ];

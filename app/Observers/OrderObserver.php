6bf5ea39453f5a21560ed9984e98cefb63719da3
kubakarminski <jlkarminski@gmail.com>
2017-03-27 20:00:13 +0200

    Party parrot order notifications;

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -8,12 +8,14 @@ use App\Jobs\IssueInvoice;
 use App\Jobs\OrderConfirmed;
 use App\Jobs\OrderPaid;
 use App\Models\Order;
+use App\Notifications\OrderCreated;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Notifications\Notifiable;
 
 
 class OrderObserver
 {
-	use DispatchesJobs;
+	use DispatchesJobs, Notifiable;
 
 	public function updated(Order $order)
 	{
@@ -28,6 +30,11 @@ class OrderObserver
 
 	public function created(Order $order)
 	{
+		$this->notify(new OrderCreated($order));
+	}
 
+	public function routeNotificationForSlack()
+	{
+		return 'https://hooks.slack.com/services/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';
 	}
 }

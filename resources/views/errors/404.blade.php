de84a7b3ad3d1e02e181ebd557243fcd4cbe9b8c
adamkarminski <adamkarminski@gmail.com>
2017-03-29 13:03:14 +0200

    Add a 404 error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.guest')
 
 @section('content')
-	Strona o podanym adresie nie istnieje.
-
+	<section class="hero is-medium">
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<p class="title">Ups, wygląda na to, że nie ma strony o takim adresie</p>
+				<p class="subtitle">Gdzie chcesz iść teraz?</p>
+				<p class="margin vertical">
+					<a class="margin horizontal" href="{{ config('app.url') }}">Strona główna platformy</a>
+					<a class="margin horizontal" href="@lang('common.course-website-link')">Strona kursu "Więcej niż LEK"</a>
+				</p>
+				<img src="https://media.giphy.com/media/3o7aTskHEUdgCQAXde/giphy.gif" alt="Vincent Vega szuka">
+				<p class="margin top">Jeżeli sami skierowaliśmy Cię tutaj - prosimy, daj nam znać, żebyśmy mogli to naprawić. Dzięki!</p>
+				<p><small>nerdy@wiecejnizlek.pl</small></p>
+			</div>
+		</div>
+	</section>
 @endsection
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,7 +8,7 @@
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 
-		<title>{{ config('app.name', 'Laravel') }}</title>
+		<title>@lang('common.app-title')</title>
 
 		<!-- Styles -->
 		<link href="/css/app.css" rel="stylesheet">
@@ -44,6 +44,6 @@
 			</footer>
 		</div>
 		<!-- Scripts -->
-		<script src="/js/payment.js"></script>
+		@yield('scripts')
 	</body>
 </html>

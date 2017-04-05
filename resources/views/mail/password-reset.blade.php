f4a9f75ed224edc647646a7ab1516340db8db06e
adamkarminski <adamkarminski@gmail.com>
2017-04-05 13:56:30 +0200

    Make e-mails great again!

--- resources/views/mail/password-reset.blade.php
+++ resources/views/mail/password-reset.blade.php
@@ -1,7 +1,12 @@
 @extends('mail.layout')
 
 @section('content')
-	<p>Otrzymałeś tego maila, ponieważ bla bla bla</p>
-	<a class="button" href="{{ route('password.reset', $token) }}">Resetuj hasło</a>\
-	<p>Jeśli to nie Ty, to bla bla bla</p>
+	<h3>Cześć {{ $user->first_name }}!</h3>
+	<h4>Wysłaliśmy do Ciebie tę wiadomość w odpowiedzi na prośbę o zmianę hasła, przypisanego do tego adresu e-mail.</h4>
+	<p><strong>Jeżeli to nie Ty stoisz za tą prośbą - zignoruj tę wiadomość.</strong></p>
+	<p>Jeżeli jednak faktycznie chcesz zmienić hasło, kliknij na przycisk <strong>Resetuj hasło</strong>, aby przejść dalej.</p>
+	<p class="has-text-centered">
+		<a class="button is-primary" href="{{ route('password.reset', $token) }}">Resetuj hasło</a>
+	</p>
+	<p>Pozdrawiamy!</p>
 @endsection

b127f706044e842252cf1f5fb47383f35d29b971
adamkarminski <adamkarminski@gmail.com>
2017-04-12 00:16:21 +0200

    Add newsletter about Instalments and Streaming

---
+++ resources/views/mail/newsletter/instalments-plain.blade.php
@@ -0,0 +1,35 @@
+Cześć!
+
+Chcemy Wam przede wszystkim bardzo podziękować za zaufanie i wsparcie! Ponad 100 z Was zadeklarowało już udział w kursie! Dostajemy też wiele doskonałych sugestii oraz pytań, które pozwalają nam lepiej dopasować “Więcej niż LEK” do Waszych potrzeb. Dziękujemy!
+
+Dziś mamy dla Was dwa krótkie, lecz ważne ogłoszenia. :)
+
+W czwartek od 19:00 do 21:00 odpowiadamy na Wasze pytania!
+
+Zdajemy sobie sprawę, że na stronie jest co czytać o kursie. :) Wciąż jednak mamy Wam bardzo wiele do powiedzenia! Tym razem jednak, postawiliśmy na bardziej interaktywną formę przekazu.
+
+Dlatego serdecznie zapraszamy Was do wzięcia udziału w streamingu, podczas którego będziemy odpowiadać na wszystkie Wasze pytania. Na pewno dokładnie opowiemy o warsztatach z naszymi wyjątkowymi gośćmi, metodologii nauczania oraz o tym, jak wyobrażamy sobie codzienną interakcję z platformą e-learningową. Nie zabraknie też rozważań o naszych dalszych planach oraz misji "Więcej niż LEK". :)
+
+Zobacz wydarzenie na facebooku - https://www.facebook.com/events/1818711421723449/
+
+Uruchomiliśmy płatność na raty
+
+Jedno z Waszych najczęstszych pytań dotyczyło rozłożenia płatności w czasie. Od dziś jest już taka możliwość!
+
+Niezależnie od wybranego wariantu i całościowej kwoty, płatność rozkładana jest na 3 raty: Pierwsza płatna do 7 dni od złożenia zamówienia Druga płatna do 15 czerwca 2017r. Trzecia płatna do 15 lipca 2017r. Dokładne kwoty znajdziecie na stronie Cennik - https://wiecejnizlek.pl/cennik.
+
+Mamy też odpowiedzi na dodatkowe pytania, które padły w kontekście rat:
+
+Jak można skorzystać z nowego sposobu płatności?
+Przy składaniu zamówienia, w ostatnim kroku Potwierdzenie płatności, należy kliknąć na "Płatność na raty". Po upewnieniu się, że kwoty i terminy odpowiadają zamówieniu, możecie potwierdzić zakup klikając na "Kupuję i płacę na raty".
+
+Czy można zmienić metodę płatności z przelewu bankowego na raty?
+Jak najbardziej! Jeżeli Twoje zamówienie nie zostało jeszcze opłacone, możesz zmienić metodę płatności poprzez stronę Twoje zamówienia.
+
+Zapisz się i zapłać na raty - https://platforma.wiecejnizlek.pl/payment/select-product
+
+Dziękujemy Wam raz jeszcze za wsparcie! Jeśli chcecie podzielić się wątpliwościami, uwagami, czy po prostu pogadać - piszcie śmiało na facebooku (https://facebook.com/wiecejnizlek) lub na info@wiecejnizlek.pl
+
+Pozdrawiamy!
+
+Ekipa Więcej niż LEK

de1f2647265bff98a11943f6d595855693bdeadc
adamkarminski <adamkarminski@gmail.com>
2017-04-12 01:15:44 +0200

    Final adjustments

--- resources/views/mail/newsletter/instalments-plain.blade.php
+++ resources/views/mail/newsletter/instalments-plain.blade.php
@@ -6,30 +6,34 @@ Dziś mamy dla Was dwa krótkie, lecz ważne ogłoszenia. :)
 
 W czwartek od 19:00 do 21:00 odpowiadamy na Wasze pytania!
 
-Zdajemy sobie sprawę, że na stronie jest co czytać o kursie. :) Wciąż jednak mamy Wam bardzo wiele do powiedzenia! Tym razem jednak, postawiliśmy na bardziej interaktywną formę przekazu.
+Zdajemy sobie sprawę, że na stronie jest co czytać o kursie. :)
+
+Wciąż jednak mamy Wam bardzo wiele do powiedzenia! Tym razem jednak, postawiliśmy na bardziej interaktywną formę przekazu.
 
 Dlatego serdecznie zapraszamy Was do wzięcia udziału w streamingu, podczas którego będziemy odpowiadać na wszystkie Wasze pytania. Na pewno dokładnie opowiemy o warsztatach z naszymi wyjątkowymi gośćmi, metodologii nauczania oraz o tym, jak wyobrażamy sobie codzienną interakcję z platformą e-learningową. Nie zabraknie też rozważań o naszych dalszych planach oraz misji "Więcej niż LEK". :)
 
-Zobacz wydarzenie na facebooku - https://www.facebook.com/events/1818711421723449/
+Zobacz wydarzenie na facebooku (https://www.facebook.com/events/1818711421723449/)
 
-Uruchomiliśmy płatność na raty
+Uruchomiliśmy płatność rozłożoną w czasie
 
-Jedno z Waszych najczęstszych pytań dotyczyło rozłożenia płatności w czasie. Od dziś jest już taka możliwość!
+Jedno z Waszych najczęstszych pytań dotyczyło rozłożenia płatności w czasie. Od dziś jest już taka możliwość! Niezależnie od wybranego wariantu i całościowej kwoty, płatność rozkładana jest na 3 części:
 
-Niezależnie od wybranego wariantu i całościowej kwoty, płatność rozkładana jest na 3 raty: Pierwsza płatna do 7 dni od złożenia zamówienia Druga płatna do 15 czerwca 2017r. Trzecia płatna do 15 lipca 2017r. Dokładne kwoty znajdziecie na stronie Cennik - https://wiecejnizlek.pl/cennik.
+1. Pierwsza płatna do 7 dni od złożenia zamówienia
+2. Druga płatna do 15 czerwca 2017r.
+3. Trzecia płatna do 15 lipca 2017r.
 
-Mamy też odpowiedzi na dodatkowe pytania, które padły w kontekście rat:
+Dokładne kwoty znajdziecie na stronie Cennik (https://wiecejnizlek.pl/cennik).
+
+Mamy też odpowiedzi na dodatkowe pytania, które padły w kontekście nowej formy płatności:
 
 Jak można skorzystać z nowego sposobu płatności?
-Przy składaniu zamówienia, w ostatnim kroku Potwierdzenie płatności, należy kliknąć na "Płatność na raty". Po upewnieniu się, że kwoty i terminy odpowiadają zamówieniu, możecie potwierdzić zakup klikając na "Kupuję i płacę na raty".
+Opcja ta dostępna jest w ostatnim kroku - "Potwierdzenie zamówienia" - podczas wyboru metody płatności. Dokładną instrukcję również znajdziecie w Cenniku (https://wiecejnizlek.pl/cennik).
 
-Czy można zmienić metodę płatności z przelewu bankowego na raty?
-Jak najbardziej! Jeżeli Twoje zamówienie nie zostało jeszcze opłacone, możesz zmienić metodę płatności poprzez stronę Twoje zamówienia.
-
-Zapisz się i zapłać na raty - https://platforma.wiecejnizlek.pl/payment/select-product
+Czy można zmienić metodę płatności?
+Jak najbardziej! Jeżeli Twoje zamówienie nie zostało jeszcze opłacone, możesz zmienić metodę płatności poprzez stronę Twoje zamówienia (https://platforma.wiecejnizlek.pl/app/myself/orders).
 
 Dziękujemy Wam raz jeszcze za wsparcie! Jeśli chcecie podzielić się wątpliwościami, uwagami, czy po prostu pogadać - piszcie śmiało na facebooku (https://facebook.com/wiecejnizlek) lub na info@wiecejnizlek.pl
 
 Pozdrawiamy!
 
-Ekipa Więcej niż LEK
+Ekipa Więcej niż LEK

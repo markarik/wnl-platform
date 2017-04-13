8d36519cee35577545c39e79fae0384c7cc2ba71
kubakarminski <jlkarminski@gmail.com>
2017-04-13 16:57:40 +0200

    Quiz api part;

---
+++ app/Http/Controllers/Api/Transformers/QuizAnswerTransformer.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\Transformers;
+
+
+use App\Models\QuizAnswer;
+use League\Fractal\TransformerAbstract;
+
+class QuizAnswerTransformer extends TransformerAbstract
+{
+	protected $parent;
+
+	public function __construct($parent = null)
+	{
+		$this->parent = $parent;
+	}
+
+
+	public function transform(QuizAnswer $quizAnswer)
+	{
+		$data = [
+			'id'         => $quizAnswer->id,
+			'text'       => $quizAnswer->text,
+			'is_correct' => $quizAnswer->is_correct,
+		];
+
+		if ($this->parent) {
+			$data = array_merge($data, $this->parent);
+		}
+
+		return $data;
+	}
+}

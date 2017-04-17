3163df1dd5a32d310af47a989c9db131c1acc185
kubakarminski <jlkarminski@gmail.com>
2017-04-17 17:35:28 +0200

    Update form class;

--- resources/assets/js/classes/forms/Form.js
+++ resources/assets/js/classes/forms/Form.js
@@ -2,45 +2,132 @@ import Errors from './Errors'
 import axios from 'axios'
 
 class Form {
+	/**
+	 * Create a new Form instance.
+	 *
+	 * @param {object} data
+	 */
 	constructor(data) {
-		this.originalData = data
+		this.originalData = data;
 
 		for (let field in data) {
-			this[field] = data[field]
+			this[field] = data[field];
 		}
 
-		this.errors = new Errors()
+		this.errors = new Errors();
 	}
 
+
+	/**
+	 * Fetch all relevant data for the form.
+	 */
 	data() {
-		let data = Object.assign({}, this)
+		let data = {};
 
-		delete(data.originalData)
-		delete(data.errors)
+		for (let property in this.originalData) {
+			data[property] = this[property];
+		}
 
-		return data
+		return data;
 	}
 
+
+	/**
+	 * Reset the form fields.
+	 */
 	reset() {
 		for (let field in this.originalData) {
-			this[field] = null
+			this[field] = '';
 		}
+
+		this.errors.clear();
 	}
 
-	submit(requestType, apiUrl) {
-		axios[requestType](apiUrl, this.data())
-			.then(this.onSuccess.bind(this))
-			.catch(this.onFail.bind(this))
+
+	/**
+	 * Send a POST request to the given URL.
+	 * .
+	 * @param {string} url
+	 */
+	post(url) {
+		return this.submit('post', url);
 	}
 
-	onSuccess(response) {
-		console.log('Success!')
-		console.log(response)
+
+	/**
+	 * Send a PUT request to the given URL.
+	 * .
+	 * @param {string} url
+	 */
+	put(url) {
+		return this.submit('put', url);
 	}
 
-	onFail(error) {
-		this.errors.record(error.response.data)
+
+	/**
+	 * Send a PATCH request to the given URL.
+	 * .
+	 * @param {string} url
+	 */
+	patch(url) {
+		return this.submit('patch', url);
+	}
+
+
+	/**
+	 * Send a DELETE request to the given URL.
+	 * .
+	 * @param {string} url
+	 */
+	delete(url) {
+		return this.submit('delete', url);
+	}
+
+
+	/**
+	 * Submit the form.
+	 *
+	 * @param {string} requestType
+	 * @param {string} url
+	 */
+	submit(requestType, url) {
+		return new Promise((resolve, reject) => {
+			axios[requestType](url, this.data())
+				.then(response => {
+					this.onSuccess(response.data);
+
+					resolve(response.data);
+				})
+				.catch(error => {
+					this.onFail(error.response.data);
+
+					reject(error.response.data);
+				});
+		});
+	}
+
+
+	/**
+	 * Handle a successful form submission.
+	 *
+	 * @param {object} data
+	 */
+	onSuccess(data) {
+		alert(data.message); // temporary
+
+		this.reset();
+	}
+
+
+	/**
+	 * Handle a failed form submission.
+	 *
+	 * @param {object} errors
+	 */
+	onFail(errors) {
+		this.errors.record(errors);
 	}
 }
+
 
 export { Form as default }

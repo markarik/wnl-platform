ff6bdbc3f9ebd892e2a6c6f20c7e010eb65f8806
kubakarminski <jlkarminski@gmail.com>
2017-04-20 17:10:11 +0200

    Add form vue components;

---
+++ resources/assets/js/components/global/form/Input.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+	<div class="field">
+
+		<label :for="name" class="label">
+			<slot></slot>
+		</label>
+
+		<input :type="type"
+			   class="input is-medium"
+			   :name="name"
+			   :value="form[name]"
+			   @input="$emit('input', $event.target.value)"
+			   :placeholder="name"
+			   :id="name">
+
+		<span class="help is-danger" v-if="form.errors.has(name)"
+			  v-text="form.errors.get(name)"></span>
+	</div>
+</template>
+
+<style lang="sass">
+</style>
+
+<script>
+	export default {
+		name: 'wnl-form-input',
+		props: ['type', 'name', 'form'],
+	}
+</script>

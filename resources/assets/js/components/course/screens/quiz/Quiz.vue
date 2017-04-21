2cfa32ed8749f671980d774e5f71784c1cf1b7c8
adamkarminski <adamkarminski@gmail.com>
2017-04-21 12:23:44 +0200

    Add introduction to quiz

--- resources/assets/js/components/course/screens/quiz/Quiz.vue
+++ resources/assets/js/components/course/screens/quiz/Quiz.vue
@@ -1,14 +1,27 @@
 <template>
 	<div>
-		<wnl-quiz-question v-for="question in getUnresolved" v-if="isLoaded"
-			:answers="question.answers"
-			:text="question.text"
-		></wnl-quiz-question>
+		<p class="title is-5">
+			Zanim zakończysz tę lekcję, sprawdź swoją wiedzę z wczorajszej! <wnl-emoji name="thinking_face"></wnl-emoji>
+		</p>
+		<p class="big">
+			Po każdym podejściu, na ekranie pozostaną tylko błędnie rozwiązane pytania. Aby zakończyć test, odpowiadasz do skutku! Żeby nie było zbyt łatwo, kolejność odpowiedzi będzie każdorazowo zmieniana. Powodzenia!
+		</p>
+		<div class="wnl-quiz">
+			<wnl-quiz-question v-for="question in getUnresolved" v-if="isLoaded"
+				:answers="question.answers"
+				:text="question.text"
+			></wnl-quiz-question>
+		</div>
 	</div>
 </template>
 
 <style lang="sass" rel="stylesheet/sass" scoped>
+	@import 'resources/assets/sass/variables'
 
+	.wnl-quiz
+		border-top: $border-light-gray
+		margin: $margin-big 0
+		padding-top: $margin-big
 </style>
 
 <script>

2765fd99dc751554359247ae769bb228ef7e7f6b
kubakarminski <jlkarminski@gmail.com>
2017-05-30 17:57:45 +0200

    Use user_addresses table when exporting shipment details;

--- app/Console/Commands/AddressesExport.php
+++ app/Console/Commands/AddressesExport.php
@@ -39,9 +39,10 @@ class AddressesExport extends Command
 	 */
 	public function handle()
 	{
-		$users = User::wherehas('orders', function ($query) {
-			return $query->where('paid', 1);
-		})->get();
+		$users = User::with(['address'])
+			->wherehas('orders', function ($query) {
+				return $query->where('paid', 1);
+			})->get();
 
 		$schema = $this->getSchema();
 
@@ -58,13 +59,14 @@ class AddressesExport extends Command
 
 	public function printLine($user, $schema)
 	{
+		$address = $user->address()->first();
 		$schema['receiver_name'] = $user->first_name;
 		$schema['receiver_surname'] = $user->last_name;
 		$schema['receiver_email'] = $user->email;
-		$schema['receiver_street'] = $user->address;
-		$schema['receiver_postcode'] = "23-234";
-		$schema['receiver_city'] = $user->city;
-		$schema['receiver_phone'] = str_replace([' ', '+48', '-'], '', $user->phone);
+		$schema['receiver_street'] = $address->street;
+		$schema['receiver_postcode'] = $address->zip;
+		$schema['receiver_city'] = $address->city;
+		$schema['receiver_phone'] = $address->phone;
 		$schema['user_reference_number'] = $user->id;
 
 		$fields = array_map(function ($element) {
@@ -86,23 +88,23 @@ class AddressesExport extends Command
 	protected function getSchema()
 	{
 		return [
-			'service'           => 'inpostkurier',
-			'sender_name'       => 'bethink Sp. z o. o.',
-			'sender_surname'    => '',
-			'sender_company'    => 'bethink sp. z o. o.',
-			'sender_email'      => 'info@bethink.pl',
-			'sender_street'     => 'Henryka Sienkiewicza 8/1',
-			'sender_postcode'   => '60-817',
-			'sender_city'       => 'Poznań',
-			'sender_phone'      => '722100867',
-			'sender_paczkomat'  => '',
-			'sender_placowka'   => '',
-			'receiver_type'     => 'private',
-			'receiver_name'     => '#',
-			'receiver_surname'  => '#',
-			'receiver_email'    => '#',
-			'receiver_street'   => '#',
-			'receiver_postcode' => '#',
+			'service'                => 'inpostkurier',
+			'sender_name'            => 'bethink Sp. z o. o.',
+			'sender_surname'         => '',
+			'sender_company'         => 'bethink sp. z o. o.',
+			'sender_email'           => 'info@bethink.pl',
+			'sender_street'          => 'Henryka Sienkiewicza 8/1',
+			'sender_postcode'        => '60-817',
+			'sender_city'            => 'Poznań',
+			'sender_phone'           => '722100867',
+			'sender_paczkomat'       => '',
+			'sender_placowka'        => '',
+			'receiver_type'          => 'private',
+			'receiver_name'          => '#',
+			'receiver_surname'       => '#',
+			'receiver_email'         => '#',
+			'receiver_street'        => '#',
+			'receiver_postcode'      => '#',
 			'receiver_city'          => '#',
 			'receiver_phone'         => '#',
 			'receiver_paczkomat'     => '',

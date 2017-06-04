f77e88e912b144911cfb453b0c48fc5b02542291
adamkarminski <adamkarminski@gmail.com>
2017-06-04 13:12:48 +0200

    Add automatic font color to Quill

--- resources/assets/js/utils/colors.js
+++ resources/assets/js/utils/colors.js
@@ -1,1 +1,1 @@
-export const fontColors = ['#BD2318', '#CC3E17', '#ECA005', '#389476', '#2F4999', '#35245B']
+export const fontColors = [false, '#BD2318', '#CC3E17', '#ECA005', '#389476', '#2F4999', '#35245B']

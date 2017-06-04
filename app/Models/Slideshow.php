c937ed5cffa7fc436818b92af936f7afc5deda00
kubakarminski <jlkarminski@gmail.com>
2017-06-04 20:05:23 +0200

    Store slideshow backgrounds;

--- app/Models/Slideshow.php
+++ app/Models/Slideshow.php
@@ -2,12 +2,22 @@
 
 namespace App\Models;
 
+use Storage;
 use Illuminate\Database\Eloquent\Model;
 
 class Slideshow extends Model
 {
+	protected $fillable = ['background'];
+
+	protected $appends = ['background_url'];
+
 	public function slides()
 	{
 		return $this->morphToMany('\App\Models\Slide', 'presentable');
+	}
+
+	public function getBackgroundUrlAttribute()
+	{
+		return Storage::url($this->background);
 	}
 }

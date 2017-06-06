1d23dbe284e36bc0edb60d2c1a34c1c188204b3e
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2017-06-06 13:34:14 +0200

    add tests for chat

--- tests/Browser/DataProviders/User.php
+++ tests/Browser/DataProviders/User.php
@@ -5,10 +5,20 @@ namespace Tests\Browser\DataProviders;
 
 class User
 {
+	const USER_EMAIL_1 = 'jlkarminski@gmail.com';
+	const USER_PASSWORD_1 = 'secret';
+	const USER_NAME_1 = 'Kuba';
+	const USER_LAST_NAME_1 = 'Karmiński';
+
+	const USER_EMAIL_2 = 'adamkarminski@gmail.com';
+	const USER_PASSWORD_2 = 'secret';
+	const USER_NAME_2 = 'Adam';
+	const USER_LAST_NAME_2 = 'Karmiński';
+
 	public static function userProvider()
 	{
 		return [
-			['jlkarminski@gmail.com', 'secret', 'Kuba']
+			[self::USER_EMAIL_1, self::USER_PASSWORD_1, self::USER_NAME_1]
 		];
 	}
 }

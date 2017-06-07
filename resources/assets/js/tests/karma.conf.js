f1d51a67feeccf33d7b8e8bbc5adfb417f1f7bf3
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2017-06-07 15:07:08 +0200

    WIP: wait with unit tests for decision about vuex/service

--- resources/assets/js/tests/karma.conf.js
+++ resources/assets/js/tests/karma.conf.js
@@ -5,7 +5,10 @@ module.exports = (config) => {
 		browsers: ['PhantomJS'],
 		frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
 		reporters: ['spec'],
-		files: ['./index.js'],
+		files: [
+			'../../../../node_modules/babel-polyfill/dist/polyfill.js',
+			'./index.js'
+		],
 		preprocessors: {
 			'./index.js': ['webpack', 'sourcemap']
 		},

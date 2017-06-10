9eb292542453295ca283ccc33fbd4e7a6a970ac9
kubakarminski <jlkarminski@gmail.com>
2017-06-10 23:31:29 +0200

    Create Vue implementation of user settings;

--- resources/assets/js/components/global/form/index.js
+++ resources/assets/js/components/global/form/index.js
@@ -2,10 +2,12 @@ import Form from './Form'
 import Text from './Text'
 import Quill from './Quill'
 import Submit from './Submit'
+import Check from './Check'
 
 export {
 	Form,
 	Text,
 	Quill,
 	Submit,
+	Check,
 }

3ec7a1ffe6dce200673e2bf1574b17b040901be6
Bartłomiej Kowalczyk <brt.kowal@gmail.com>
2017-06-15 16:13:50 +0200

    Code Style | Spaces inside curly in currentUser.js

--- resources/assets/js/store/modules/currentUser.js
+++ resources/assets/js/store/modules/currentUser.js
@@ -1,7 +1,7 @@
 import * as types from '../mutations-types'
-import {getApiUrl} from 'js/utils/env'
-import {getCurrentUser, getUserSettings, getDefaultSettings, setUserSettings} from 'js/services/user';
-import {set} from 'vue'
+import { getApiUrl } from 'js/utils/env'
+import { getCurrentUser, getUserSettings, getDefaultSettings, setUserSettings } from 'js/services/user';
+import { set } from 'vue'
 
 // Initial state
 const state = {
@@ -52,7 +52,7 @@ const mutations = {
 
 // Actions
 const actions = {
-	setupCurrentUser({commit, dispatch}) {
+	setupCurrentUser({ commit, dispatch }) {
 		Promise
 			.all([
 				dispatch('fetchCurrentUserProfile'),
@@ -65,7 +65,7 @@ const actions = {
 			})
 	},
 
-	fetchCurrentUserProfile({commit}) {
+	fetchCurrentUserProfile({ commit }) {
 		return new Promise((resolve, reject) => {
 			getCurrentUser().then((response) => {
 				commit(types.USERS_SETUP_CURRENT, response.data)
@@ -78,7 +78,7 @@ const actions = {
 		})
 	},
 
-	fetchUserSettings({commit}) {
+	fetchUserSettings({ commit }) {
 		return new Promise((resolve, reject) => {
 			getUserSettings().then((response) => {
 				commit(types.USERS_SETUP_SETTINGS, response.data)
@@ -91,7 +91,7 @@ const actions = {
 		})
 	},
 
-	changeUserSetting({commit}, payload) {
+	changeUserSetting({ commit }, payload) {
 		commit(types.USERS_CHANGE_SETTING, payload)
 	},
 

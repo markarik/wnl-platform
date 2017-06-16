fc499147c0d97eef08b8df298d76f8c703177179
adamkarminski <adamkarminski@gmail.com>
2017-06-16 20:17:07 +0200

    Add a mail with the last info

---
+++ resources/views/mail/newsletter/last-christmas.blade.php
@@ -0,0 +1,31 @@
+@extends('mail.newsletter.layout')
+
+@section('content')
+
+	<h3>Cześć %recipient_name%!</h3>
+
+	<p>To już jutro! Sobotnimi warsztatami oficjalnie rozpoczniemy pierwszą edycję kursu! Z uwagi na liczne pytania, chcielibyśmy podzielić się z Wami najważniejszymi informacjami na zbliżające się dni. :)</p>
+
+	<h4>SOBOTA</h4>
+
+	<p>O 11:00, w Collegium da Vinci w Poznaniu odbędą się pierwsze warsztaty poświęcone efektywnej nauce, pomagające w przygotowaniu się do kursu. Zajęcia przewidziane są do godziny 19:00. :) Do wszystkich warsztatowiczów prześlemy jeszcze dziś wieczorem maila z dokładnym planem warsztatów, rozpisanymi godzinami oraz tematami poszczególnych bloków.</p>
+
+	<hr style="border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));">
+
+	<h4>NIEDZIELA</h4>
+	<p>Rano, na platformie pod adresem https://platforma.wiecejnizlek.pl otworzy się “Lekcja 0” - Wstęp do kursu. :) W kilkunastu nagraniach wideo wprowadzimy Was w obsługę platformy oraz przedstawimy podstawy efektywnej nauki. Praca z tym materiałem na pewno nie będzie tak obciążająca, jak właściwe dni lekcyjne, ale zachęcamy do zarezerwowania sobie czasu na spokojne zapoznanie się z tymi informacjami. Na pewno pomogą Wam wyciągnąć z kursu maksimum wartości! Oprócz tego liczymy na to, że będziemy już mogli rozpocząć dyskusje w systemie pytań i odpowiedzi oraz na czacie. :)</p>
+	<p>Oczywiście wszystkie lekcje, również ta wstępna, będą dostępne przez cały czas trwania kursu. :)</p>
+
+	<hr style="border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));">
+
+	<h4>PONIEDZIAŁEK</h4>
+	<p>Pisaki w dłoń, albumy na biurka! Pierwsza lekcja z Interny - Kardiologia 1, czyli ruszamy z materiałem do LEK-u! :)</p>
+
+	<hr style="border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));">
+
+	<p>Od dziś, przez cały następny tydzień, będziemy też w podwyższonej gotowości do rozwiązywania wszelkich problemów technicznych, na jakie zdarzy się Wam natrafić. Będziemy robić co w naszej mocy, żeby nic nie przeszkodziło Wam w swobodnej i przyjemnej nauce. :)</p>
+	<p>W razie pytań pozostajemy stale do dyspozycji pod adresem mailowym info@wiecejnizlek.pl, na facebooku oraz pod numerem telefonu podanym w kontakcie. :)</p>
+	<p>Nie możemy się już doczekać!</p>
+	<p>Z pozdrowieniami,</p>
+
+@endsection

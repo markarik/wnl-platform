ae949f0fbacfcd47c8a353d7d1eaf4bb883ebe03
adamkarminski <adamkarminski@gmail.com>
2017-06-16 21:28:00 +0200

    Add email to workshop members

---
+++ resources/views/mail/newsletter/workshops-details-plain.blade.php
@@ -0,0 +1,20 @@
+Cześć %recipient_name%!
+
+Już jutro widzimy się na pierwszych warsztatach! Przypominamy, że zajęcia rozpoczną się o godzinie 11:00 w Collegium da Vinci, przy ul. Tadeusza Kutrzeby 20 w Poznaniu. Spotykamy się w sali N205, do której będą prowadziły Was od wejścia wskazówki umieszczone na monitorach widocznych w korytarzach Collegium. :)
+
+Plan warsztatów wygląda następująco:
+
+11:00-11:45 Część I - Powitanie + wprowadzenie do kursu :)
+11:45-12:00 Przerwa
+12.00-13:30 Część II - Podstawy efektywnej nauki
+13:30-15:00 Przerwa obiadowa
+15:00-16:30 Część III - Mapy myśli
+16:30-16:45 Przerwa
+16:45-18:00 Część IV - Nowe spojrzenie na proces nauki + Przepis na LEK
+18:00-19:00 Pytania i odpowiedzi
+19:00 Zakończenie warsztatów
+
+W razie pytań pozostajemy stale do dyspozycji pod adresem mailowym warsztaty@wiecejnizlek.pl, na facebooku oraz pod numerami telefonu: 722 100 867 (Darek), 663 703 593 (Adam) oraz 536 958 454 (Asia). :)
+Do zobaczenia jutro!
+Z pozdrowieniami,
+Ekipa Więcej niż LEK
---
+++ resources/views/mail/newsletter/workshops-details.blade.php
@@ -0,0 +1,25 @@
+@extends('mail.newsletter.layout')
+
+@section('content')
+
+	<h3>Cześć %recipient_name%!</h3>
+
+	<p>Już jutro widzimy się na pierwszych warsztatach! Przypominamy, że zajęcia rozpoczną się <strong>o godzinie 11:00</strong> w <a href="https://goo.gl/maps/f78fwrAzh322">Collegium da Vinci</a>, przy ul. Tadeusza Kutrzeby 20 w Poznaniu. Spotykamy się w sali <strong>N205</strong>, do której będą prowadziły Was od wejścia wskazówki umieszczone na monitorach widocznych w korytarzach Collegium. :)</p>
+
+	<p>Plan warsztatów wygląda następująco:</p>
+
+	<p><strong>11:00-11:45 Część I - Powitanie + wprowadzenie do kursu :)</strong></p>
+	<p>11:45-12:00 Przerwa</p>
+	<p><strong>12.00-13:30 Część II - Podstawy efektywnej nauki</strong></p>
+	<p>13:30-15:00 Przerwa obiadowa</p>
+	<p><strong>15:00-16:30 Część III - Mapy myśli</strong></p>
+	<p>16:30-16:45 Przerwa</p>
+	<p><strong>16:45-18:00 Część IV - Nowe spojrzenie na proces nauki + Przepis na LEK</strong></p>
+	<p><strong>18:00-19:00 Pytania i odpowiedzi</strong></p>
+	<p>19:00 Zakończenie warsztatów</p>
+
+	<p>W razie pytań pozostajemy stale do dyspozycji pod adresem mailowym warsztaty@wiecejnizlek.pl, na facebooku oraz pod numerami telefonu: 722 100 867 (Darek), 663 703 593 (Adam) oraz 536 958 454 (Asia). :)</p>
+	<p>Do zobaczenia jutro!</p>
+	<p>Z pozdrowieniami,</p>
+
+@endsection

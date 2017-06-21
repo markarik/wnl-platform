b0e344b6378fed5d2aa852b0594d9e7c308a53b2
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2017-06-21 12:11:56 +0200

    changed my mind - use memoryStorage

--- resources/assets/js/services/sessionStore.js
+++ resources/assets/js/services/sessionStore.js
@@ -1,12 +1,12 @@
 import engine from 'store/src/store-engine';
 import sessionStorage from 'store/storages/sessionStorage';
 import localStorage from 'store/storages/localStorage';
-import cookieStorage from 'store/storages/cookieStorage';
+import memoryStorage from 'store/storages/memoryStorage';
 
 const store = engine.createStore([
 	sessionStorage,
 	localStorage,
-	cookieStorage
+	memoryStorage
 ]);
 
 export default store;

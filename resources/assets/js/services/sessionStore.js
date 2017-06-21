dd6a7570ed8460ba156172c668b45e39c659e891
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2017-06-21 12:10:55 +0200

    fallback to cookie storage if local/session not available

--- resources/assets/js/services/sessionStore.js
+++ resources/assets/js/services/sessionStore.js
@@ -1,10 +1,12 @@
 import engine from 'store/src/store-engine';
 import sessionStorage from 'store/storages/sessionStorage';
 import localStorage from 'store/storages/localStorage';
+import cookieStorage from 'store/storages/cookieStorage';
 
 const store = engine.createStore([
 	sessionStorage,
-	localStorage
+	localStorage,
+	cookieStorage
 ]);
 
 export default store;

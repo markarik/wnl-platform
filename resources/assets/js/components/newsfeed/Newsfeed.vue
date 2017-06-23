6780ca77998b89eb9878827d818b188c371a7013
kubakarminski <jlkarminski@gmail.com>
2017-06-23 20:22:28 +0200

    Pick notifications from store, display in loop;

--- resources/assets/js/components/newsfeed/Newsfeed.vue
+++ resources/assets/js/components/newsfeed/Newsfeed.vue
@@ -1,6 +1,15 @@
 <template lang="html">
 	<div class="wnl-newsfeed">
-		siema
+		<div v-if="isLoading">
+			<wnl-text-loader></wnl-text-loader>
+		</div>
+		<div v-else class="container">
+			<wnl-newsfeed-post
+					v-for="(notification, index) in notifications"
+					:notification="notification"
+					:key="index">
+			</wnl-newsfeed-post>
+		</div>
 	</div>
 </template>
 
@@ -8,7 +17,19 @@
 </style>
 
 <script>
+	import {mapGetters} from 'vuex'
+	import Post from 'js/components/newsfeed/Post'
+
 	export default {
 		name: 'wnl-newsfeed',
+		computed: {
+			...mapGetters('notifications', ['notifications', 'isLoading'])
+		},
+		components: {
+			'wnl-newsfeed-post': Post,
+		},
+		mounted() {
+
+		}
 	}
 </script>
---
+++ resources/assets/js/components/newsfeed/Post.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+	<div class="wnl-newsfeed-notification">
+		{{ notification.event }}
+	</div>
+</template>
+
+<style lang="sass">
+</style>
+
+<script>
+	export default {
+		name: 'wnl-newsfeed-notification',
+		props: ['notification'],
+		components: {}
+	}
+</script>

4b73c5765334e5e087472533e9fe3f7b925d6059
adamkarminski <adamkarminski@gmail.com>
2017-06-27 23:31:17 +0200

    Prepare a mail message to people

--- resources/views/mail/final-invoice-notification.blade.php
+++ resources/views/mail/final-invoice-notification.blade.php
@@ -3,5 +3,13 @@
 @section('content')
 	<h3>Cześć {{ $order->user->first_name or '{first_name}' }}!</h3>
 
-	<p>W załączniku znajdziesz fakturę końcową bla bla bla bla</p>
+	<p>W załączniku przesyłamy fakturę końcową, będącą potwierdzeniem dostarczenia i realizacji kursu. :)</p>
+
+	<p>Faktura może Ci się przydać, aby uzyskać refundację z Izby Lekarskiej lub jeżeli prowadzisz działalność gospodarczą.</p>
+
+	<p>Dziękujemy za uczestnictwo i życzymy powodzenia w nauce!</p>
+
+	<p class="text-align: center;">
+		<img src="https://media.giphy.com/media/yUrUb9fYz6x7a/giphy.gif" alt="Tyle papierologii..." style="display: block; margin: 0 auto;">
+	</p>
 @endsection

89d9b616c55f0812c5e93d33f22b3ef2f407076b
kubakarminski <jlkarminski@gmail.com>
2017-06-27 23:56:59 +0200

    Finish final invoice command;

---
+++ app/Jobs/IssueFinalInvoice.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Facades\Lib\Invoice\Invoice;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\FinalInvoice;
+
+
+/**
+ * @property bool mail
+ */
+class IssueFinalInvoice implements ShouldQueue
+{
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+	protected $order;
+	protected $proforma;
+	protected $send;
+
+	/**
+	 * Create a new job instance.
+	 *
+	 * @param Order $order
+	 * @param bool $proforma
+	 * @param bool $send
+	 *
+	 * @internal param bool $mail
+	 */
+	public function __construct(Order $order, $send = true)
+	{
+		$this->order = $order;
+		$this->send = $send;
+	}
+
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		$invoice = Invoice::finalInvoice($this->order);
+		if ($this->send) {
+			Mail::to($this->order->user)->send(new FinalInvoice($this->order, $invoice));
+		}
+	}
+}
--- app/Mail/FinalInvoice.php
+++ app/Mail/FinalInvoice.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class PaymentConfirmation extends Mailable
+class FinalInvoice extends Mailable
 {
 	use Queueable, SerializesModels;
 	public $order;

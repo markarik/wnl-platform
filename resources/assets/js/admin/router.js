922a725996ceff56dae3f3aefb19c29313af90f8
kubakarminski <jlkarminski@gmail.com>
2017-06-29 15:16:58 +0200

    Remove news feed from admin app;

--- resources/assets/js/admin/router.js
+++ resources/assets/js/admin/router.js
@@ -39,9 +39,7 @@ let routes = [
 	},
 	{
 		name: 'dashboard',
-		path: '/admin/app',
-		component: require('js/components/newsfeed/Newsfeed.vue'),
-		props: {type: 'admin'}
+		path: '/admin/app'
 	},
 	{
 		name: 'logout',

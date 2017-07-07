3458eba23e2b2741f6a67cd8b8a37f224d211150
adamkarminski <adamkarminski@gmail.com>
2017-07-07 19:53:42 +0200

    Add a second info about workshops

--- resources/assets/js/components/course/dashboard/DashboardNews.vue
+++ resources/assets/js/components/course/dashboard/DashboardNews.vue
@@ -2,15 +2,20 @@
 	<div class="notification content" v-if="showNews">
 		<button class="delete" @click="seenCurrentNews"></button>
 		<p class="strong">Cześć {{currentUserName}}!</p>
-		<p>Już w tę sobotę odbędą się pierwsze warsztaty ze specjalistą w ramach kursu "Więcej niż LEK"! Pan <strong>dr&nbsp;hab.&nbsp;n.&nbsp;med.&nbsp;Mariusz&nbsp;Puszczewicz</strong> przygotowuje oczywiście własny program warsztatów, natomiast do dziś możesz zgłaszać też zagadnienia, na których omówieniu zależy Ci najbardziej. :)</p>
+		<p>Już w tę sobotę odbędą się pierwsze warsztaty ze specjalistą w ramach kursu "Więcej niż LEK", które poprowadzi Pan <strong>dr&nbsp;hab.&nbsp;n.&nbsp;med.&nbsp;Mariusz&nbsp;Puszczewicz</strong>.</p>
 
-		<p>Zajrzyj na lekcję Warsztaty i w systemie pytań i odpowiedzi na ekranie Interna, zgłoś wybrane przez Ciebie tematy. To wyjątkowa szansa uzyskania wyczerpującej odpowiedzi od wyjątkowego specjalisty! Do zobaczenia! :)</p>
+		<p>Przypominamy, że zajęcia odbywają się w <strong>Collegium da Vinci</strong> przy ul. Gen. Tadeusza Kutrzeby 10 w Poznaniu, <strong>sala N205</strong>.</p>
 
-		<p class="has-text-centered">
-			<a class="button is-primary is-outlined" href="https://platforma.wiecejnizlek.pl/app/courses/1/lessons/17/screens/97">
-				Przejdź do warsztatów z Interny
-			</a>
+		<p>
+			<strong>Plan warsztatów:</strong><br>
+			<strong>11:00 - 12:30</strong> - Wstęp do diagnostyki w Internie, z naciskiem na Reumatologię + Pytania i Odpowiedzi<br>
+			<strong>12:30 - 14:00</strong> - Przerwa obiadowa<br>
+			<strong>14:00 - 15:45</strong> - Przypadki kliniczne + Omówienie<br>
+			<strong>15:45 - 16:00</strong> - Przerwa kawowa<br>
+			<strong>16:00 - 17:00</strong> - Godzina z mentorem (może ulec przedłużeniu)<br>
 		</p>
+
+		<p>Wszystkie powyższe informacje znajdziesz też w lekcji Warsztaty / Interna. :) Do zobaczenia!</p>
 	</div>
 </template>
 
@@ -18,7 +23,7 @@
 	import store from 'js/services/messagesStore'
 	import { mapGetters } from 'vuex'
 
-	const CURRENT_NEWS = 'workshops-deadline'
+	const CURRENT_NEWS = 'workshops-details'
 	const REQUIRED_ROLE = 'workshop-participant'
 
 	export default {

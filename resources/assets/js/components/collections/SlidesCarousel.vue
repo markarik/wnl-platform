23c1b19be2472b883764426c4aaa95598aed0eb8
adamkarminski <adamkarminski@gmail.com>
2017-07-11 22:32:15 +0200

    Slides collection | Style carousel items

--- resources/assets/js/components/collections/SlidesCarousel.vue
+++ resources/assets/js/components/collections/SlidesCarousel.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<h3>Twoja kolekcja slajdów</h3>
+	<div class="wnl-slides-collection">
+		<p class="title is-4">Twoja kolekcja slajdów</p>
 		<wnl-slideshow
 			:screenData="screenData"
 			:presentableId="categoryId"
@@ -8,38 +8,45 @@
 			:preserveRoute="true"
 			:slideOrderNumber="currentSlide.order_number"
 		></wnl-slideshow>
-		<carousel class="wnl-carousel" :navigationEnabled="true" :perPage="4">
+		<carousel class="wnl-carousel" :paginationEnabled="false" :navigationEnabled="true" :perPage="4">
 			<slide class="wnl-slide" v-bind:key="index" v-for="(slide, index) in slides">
 				<div class="slide-thumb" @click="showSlide(index)">
-					<h4>{{slide.header}}</h4>
+					<p class="metadata">{{slide.header}}</p>
 					<div class="slide-snippet" v-if="!slide.media" v-html="slide.snippet"></div>
-					<div class="slide-snippet" v-else>Obrazki</div>
+					<div class="slide-snippet" v-else>Obraz lub film</div>
 				</div>
 			</slide>
 		</carousel>
 	</div>
 </template>
 <style lang="sass" rel="stylesheet/sass" scoped>
+	@import 'resources/assets/sass/variables'
+
+	.wnl-slides-collection
+		margin-top: $margin-base
+
 	.wnl-carousel
-		justify-content: space-around;
-		margin-bottom: 10px;
+		justify-content: space-around
+		margin-bottom: 10px
 
 	.VueCarousel-inner
-		margin: 0 auto;
+		margin: 0 auto
+
 	.slide
-		width: 100%;
-		height: 400px;
-		background-color: grey;
+		width: 100%
+		height: 400px
 
 	.slide-thumb
-		height: 100px;
-		background-color: lime;
-		border: 1px solid grey;
-		cursor: pointer;
+		background-color: white
+		border: $border-light-gray
+		cursor: pointer
+		height: 100px
+		margin-right: $margin-small
+		padding: $margin-small
 </style>
 
 <script>
-	import {mapActions, mapGetters} from 'vuex'
+	import { mapActions, mapGetters } from 'vuex'
 	import { Carousel, Slide } from 'vue-carousel';
 	import Slideshow from 'js/components/course/screens/slideshow/Slideshow.vue'
 

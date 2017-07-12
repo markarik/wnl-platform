7d46e181387715c6ce92b4c304f7dcbb6b0be3ca
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2017-07-12 10:03:51 +0200

    don't insert category if it exists

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -11,16 +12,14 @@ class CategoriesTableSeeder extends Seeder
 	 */
 	public function run()
 	{
-		DB::table('categories')->insert([
-			['name' => 'Interna'],
-			['name' => 'Pediatria'],
-			['name' => 'Chirurgia'],
-			['name' => 'Ginekologia'],
-			['name' => 'Psychiatria'],
-			['name' => 'Medycyna ratunkowa i anestezjologia'],
-			['name' => 'Medycyna rodzinna'],
-			['name' => 'Zdrowie publiczne, Bioetyka, Prawo medyczne'],
-		]);
+		Category::firstOrCreate(['name' => 'Interna']);
+		Category::firstOrCreate(['name' => 'Pediatria']);
+		Category::firstOrCreate(['name' => 'Chirurgia']);
+		Category::firstOrCreate(['name' => 'Ginekologia']);
+		Category::firstOrCreate(['name' => 'Psychiatria']);
+		Category::firstOrCreate(['name' => 'Medycyna ratunkowa i anestezjologia']);
+		Category::firstOrCreate(['name' => 'Medycyna rodzinna']);
+		Category::firstOrCreate(['name' => 'Zdrowie publiczne, Bioetyka, Prawo medyczne']);
 
 		DB::table('categories')->insert([
 			['name' => 'Kardiologia', 'parent_id' => App\Models\Category::where('name', 'Interna')->first()->id],

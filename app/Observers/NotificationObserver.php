cf8da37b8ffb173291557869c3b08a902eebebc6
kubakarminski <jlkarminski@gmail.com>
2017-07-19 20:42:02 +0200

    Synchronizing moderator notifications;

--- app/Observers/NotificationObserver.php
+++ app/Observers/NotificationObserver.php
@@ -12,6 +12,41 @@ class NotificationObserver
 
 	public function updated(Notification $notification)
 	{
+		if (str_is('*moderator*', $notification->channel)) {
+			$this->syncModeratorNotifications($notification);
+		}
 	}
 
+	public function syncModeratorNotifications($notification)
+	{
+		$eventId = $notification->event_id;
+		$modifiedFields = $notification->getDirty();
+
+		$twinNotifications = Notification::select()
+			->where('event_id', $eventId)
+			->where('id', '!=' ,$notification->event_id)
+			->where('channel', 'like', 'private-role.moderator%')
+			->get();
+
+		$dispatcher = $this->eventsOff();
+
+		foreach ($twinNotifications as $twinNotification) {
+			$twinNotification->update($modifiedFields);
+		}
+
+		$this->eventsOn($dispatcher);
+	}
+
+	private function eventsOff()
+	{
+		$dispatcher = Notification::getEventDispatcher();
+		Notification::unsetEventDispatcher();
+
+		return $dispatcher;
+	}
+
+	private function eventsOn($dispatcher)
+	{
+		Notification::setEventDispatcher($dispatcher);
+	}
 }

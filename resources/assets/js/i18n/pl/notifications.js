045913b102ff0a3b74a6ceab605d7a04511f286a
adamkarminski <adamkarminski@gmail.com>
2017-07-25 13:11:13 +0200

    Notifications | Add notifications related i18n

--- resources/assets/js/i18n/pl/notifications.js
+++ resources/assets/js/i18n/pl/notifications.js
@@ -2,17 +2,19 @@ export const notifications = {
 	personal: {
 		heading: 'Powiadomienia',
 		markAllAsRead: 'Oznacz wszystkie jako przeczytane',
+		turnOff: 'Wyłącz',
+		turnOn: 'Włącz',
 	},
-	actions: {
-		commentPosted: 'skomentował/-a',
-		qnaQuestionPosted: 'zadał/-a',
-		qnaAnswerPosted: 'odpowiedział/-a na',
-		upvoteAdded: 'polubił/-a',
+	events: {
+		'comment-posted': '{actor} skomentował/-a {object}',
+		'qna-question-posted': '{actor} zadał/-a pytanie',
+		'qna-answer-posted': '{actor} odpowiedział/-a na {object}',
+		'reaction-added': '{actor} polubił/-a {object}',
 	},
 	objects: {
-		comment: 'komentarz | Twój komentarz',
-		qnaAnswer: 'odpowiedź | Twoją odpowiedź',
-		qnaQuestion: 'pytanie | Twoje pytanie',
-		quizQuestion: 'pytanie kontrolne',
+		'comment': 'komentarz | Twój komentarz',
+		'qna_answer': 'odpowiedź | Twoją odpowiedź',
+		'qna_question': 'pytanie | Twoje pytanie',
+		'quiz_question': 'pytanie kontrolne',
 	},
 }

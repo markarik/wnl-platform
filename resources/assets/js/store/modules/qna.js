17902666999fce03a5e821f7f138e0ce8e775490
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2017-07-26 12:43:15 +0200

    pass query to url

--- resources/assets/js/store/modules/qna.js
+++ resources/assets/js/store/modules/qna.js
@@ -178,7 +178,9 @@ const getters = {
 				return sortByVotes(list)
 		}
 	},
-
+	getQuestionIndex: state => (id, list) => {
+		list.findIndex((question) => question.id === id)
+	},
 	// Resources
 	getQuestion: state => (id) => state.qna_questions[id],
 	answer:      state => (id) => state.qna_answers[id],

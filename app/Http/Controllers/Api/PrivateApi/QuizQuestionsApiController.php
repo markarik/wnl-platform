c845cee4183a8a83048c837277b5613b1c0a44ba
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2017-08-17 16:37:22 +0200

    hardcoded api response

--- app/Http/Controllers/Api/PrivateApi/QuizQuestionsApiController.php
+++ app/Http/Controllers/Api/PrivateApi/QuizQuestionsApiController.php
@@ -13,6 +13,18 @@ class QuizQuestionsApiController extends ApiController
 	}
 
 	public function getFilters() {
+		/***
+			[
+				type: 'tags',
+				items: [
+					['name' => 'Interna', 'value' => 1, 'items': [
+						['name' => 'Kardiologia', 'value' => 64]
+					]],
+					['name' => 'Pediatria', 'value' => 2],
+				]
+			]
+
+		**/
 		$chronoTags = Tag::where('name', 'like', 'lek-%')->get();
 		$subjectsTags = Tag::whereIn('name', [
 			'Kardiologia',
@@ -27,8 +39,74 @@ class QuizQuestionsApiController extends ApiController
 		])->get();
 
 		return $this->respondOk([
-			'chrono' => $chronoTags->toArray(),
-			'subjects' => $subjectsTags->toArray()
+			'exams' => [
+				'type' => 'tags',
+				'items' => [
+					[
+						'name' => Tag::find(145)->name,
+						'value' => Tag::find(145)->id
+					],
+					[
+						'name' => Tag::find(120)->name,
+						'value' => Tag::find(120)->id
+					],
+					[
+						'name' => Tag::find(154)->name,
+						'value' => Tag::find(154)->id
+					],
+					[
+						'name' => Tag::find(110)->name,
+						'value' => Tag::find(110)->id
+					],
+					[
+						'name' => Tag::find(144)->name,
+						'value' => Tag::find(144)->id
+					],
+				]
+			],
+			'subjects' => [
+				'type' => 'tags',
+				'items' => [
+					[
+						'name' => Tag::where('name', 'Interna')->first()->name,
+						'value' => Tag::where('name', 'Interna')->first()->id,
+						'type' => 'tags',
+						'items' => [
+							[
+								'name' => Tag::where('name', 'Kardiologia')->first()->name,
+								'value' => Tag::where('name', 'Kardiologia')->first()->id
+							],
+							[
+								'name' => Tag::where('name', 'Pulmonologia')->first()->name,
+								'value' => Tag::where('name', 'Pulmonologia')->first()->id
+							],
+							[
+								'name' => Tag::where('name', 'Laryngologia')->first()->name,
+								'value' => Tag::where('name', 'Laryngologia')->first()->id
+							]
+						]
+					],
+					[
+						'name' => Tag::where('name', 'Pediatria')->first()->name,
+						'value' => Tag::where('name', 'Pediatria')->first()->id,
+						'type' => 'tags',
+						'items' => [
+							[
+								'name' => Tag::where('name', 'Kardiologia pediatryczna')->first()->name,
+								'value' => Tag::where('name', 'Kardiologia pediatryczna')->first()->id
+							],
+							[
+								'name' => Tag::where('name', 'Pulmonologia pediatryczna')->first()->name,
+								'value' => Tag::where('name', 'Pulmonologia pediatryczna')->first()->id
+							],
+							[
+								'name' => Tag::where('name', 'Hematologia pediatryczna')->first()->name,
+								'value' => Tag::where('name', 'Hematologia pediatryczna')->first()->id
+							]
+						]
+					]
+				]
+			]
 		]);
 	}
 }

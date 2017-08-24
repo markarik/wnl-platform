7b6953c49652b2d8c4fed5bacf5a5b9762017614
adamkarminski <adamkarminski@gmail.com>
2017-08-24 14:44:02 +0200

    Provide layout structure to QuestionsPlanner

--- resources/assets/js/components/questions/QuestionsPlanner.vue
+++ resources/assets/js/components/questions/QuestionsPlanner.vue
@@ -1,20 +1,32 @@
 <template>
-	<div>
-		<label for="startDate">Kiedy zaczynasz?</label>
-		<input name="startDate" v-model="startDate" type="date"/>
-		<label for="endDate">Kiedy kończysz?</label>
-		<input name="endDate" v-model="endDate" type="date"/>
-		<label for="slackDays">Ile dni wolnych?</label>
-		<input name="slackDays" v-model="slackDays" type="number"/>
+	<div class="wnl-app-layout">
+		<wnl-questions-navigation/>
+		<div class="wnl-middle wnl-app-layout-main">
+			<div class="scrollable-main-container">
+				<p class="title is-3">Planner</p>
+				<label for="startDate">Kiedy zaczynasz?</label>
+				<input name="startDate" v-model="startDate" type="date"/>
+				<label for="endDate">Kiedy kończysz?</label>
+				<input name="endDate" v-model="endDate" type="date"/>
+				<label for="slackDays">Ile dni wolnych?</label>
+				<input name="slackDays" v-model="slackDays" type="number"/>
 
-		<p><button @click="createPlan">Ułóż plan!</button></p>
-		<p>Pytania z których kategorii Ciebie interesują?</p>
-		<wnl-questions-filters
-			:activeFilters="activeFilters"
-			:fetchingQuestions="false"
-			:filters="filters"
-			@activeFiltersChanged="onActiveFiltersChanged"
-		/>
+				<p><button @click="createPlan">Ułóż plan!</button></p>
+				<p>Pytania z których kategorii Ciebie interesują?</p>
+				<wnl-questions-filters
+					:activeFilters="activeFilters"
+					:fetchingQuestions="false"
+					:filters="filters"
+					@activeFiltersChanged="onActiveFiltersChanged"
+				/>
+			</div>
+		</div>
+		<wnl-sidenav-slot
+			:isDetached="!isChatMounted"
+			:isVisible="isLargeDesktop"
+			:hasChat="true"
+		>
+		</wnl-sidenav-slot>
 	</div>
 </template>
 
@@ -22,11 +34,15 @@
 	import {mapActions, mapGetters} from 'vuex'
 
 	import QuestionsFilters from 'js/components/questions/QuestionsFilters'
+	import QuestionsNavigation from 'js/components/questions/QuestionsNavigation'
+	import SidenavSlot from 'js/components/global/SidenavSlot'
 
 	export default {
 		name: 'QuestionsPlanner',
 		components: {
-			'wnl-questions-filters': QuestionsFilters
+			'wnl-questions-filters': QuestionsFilters,
+			'wnl-questions-navigation': QuestionsNavigation,
+			'wnl-sidenav-slot': SidenavSlot,
 		},
 		props: [],
 		data() {
@@ -37,6 +53,7 @@
 			}
 		},
 		computed: {
+			...mapGetters(['isChatMounted', 'isLargeDesktop']),
 			...mapGetters('questions', [
 				'activeFilters',
 				'filters',

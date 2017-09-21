53efb56c6e05cf711ae05b661fbda20540be89a5
kubakarminski <jlkarminski@gmail.com>
2017-09-21 16:21:14 +0200

    Add corrective invoice;

---
+++ resources/views/payment/invoices/corrective-layout.blade.php
@@ -0,0 +1,159 @@
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>@yield('title')</title>
+
+	<!-- Styles -->
+	<style>
+		body {
+			color: #242d47;
+			font-family: 'DejaVu Sans', sans-serif;
+			font-size: 8pt;
+		}
+
+		.columns {
+			padding: 20px;
+		}
+
+		.columns:after {
+			content: "";
+			display: table;
+			clear: both;
+		}
+
+		.column {
+			box-sizing: border-box;
+			float: left;
+			padding: 0 15px 50px;
+			width: 50%;
+		}
+
+		.has-text-right {
+			text-align: right;
+		}
+
+		table {
+			border-spacing: 0;
+			border-collapse: collapse;
+			margin: 0 0 30px;
+			width: 100%;
+		}
+
+		th, td {
+			border: 1px solid #efefef;
+			padding: 15px;
+		}
+
+		th {
+			background: #efefef;
+		}
+
+		.no-border {
+			border: none;
+		}
+	</style>
+</head>
+<body>
+	<div class="columns">
+		{{-- Invoice data --}}
+		<div class="column">
+			@yield('invoice-data')
+		</div>
+
+		{{-- Logo --}}
+		<div class="column has-text-right">
+			<img src="{{ asset('/images/wnl-logo@2x.png') }}" alt="Logo Więcej niż LEK" style="width: 400px;">
+		</div>
+	</div>
+
+	<div class="columns">
+		{{-- Seller --}}
+		<div class="column">
+			<strong>Sprzedawca</strong>
+			<address>
+				bethink sp. z o.o.<br>
+				ul. Henryka Sienkiewicza 8/1<br>
+				60-817 Poznań<br>
+				NIP: 781-194-37-56<br>
+				REGON: 366-802-351
+			</address>
+		</div>
+
+		{{-- Buyer --}}
+		<div class="column has-text-right">
+			<strong>Nabywca</strong>
+			<address>
+				@yield('buyer')
+			</address>
+		</div>
+	</div>
+
+	{{-- Orders --}}
+	<div class="columns">
+		<h3>@yield('before-orders-title', '')</h3>
+		<table>
+			<tr>
+				<th>Lp</th>
+				<th>Nazwa</th>
+				<th>Jedn.</th>
+				<th>Ilość</th>
+				<th>Cena brutto</th>
+				<th>Stawka VAT</th>
+				<th>Wartość netto</th>
+				<th>Wartość brutto</th>
+			</tr>
+			@yield('before-orders-list')
+		</table>
+	</div>
+
+	<div class="columns">
+		<h3>@yield('after-orders-title', '')</h3>
+		<table>
+			<tr>
+				<th>Lp</th>
+				<th>Nazwa</th>
+				<th>Jedn.</th>
+				<th>Ilość</th>
+				<th>Cena brutto</th>
+				<th>Stawka VAT</th>
+				<th>Wartość netto</th>
+				<th>Wartość brutto</th>
+			</tr>
+			@yield('after-orders-list')
+		</table>
+	</div>
+
+
+	<table>
+		<tr class="no-border">
+			<td style="width: 50%;" class="no-border">
+				{{-- Orders summary --}}
+				@yield('orders-summary', '')
+			</td>
+			<td style="width: 50%;" class="no-border">
+				<div class="has-text-right">
+				@yield('summary')
+				</div>
+			</td>
+		</tr>
+		{{--<tr class="no-border">
+			<td style="width: 50%; vertical-align: top;" class="no-border">
+				--}}{{-- Advances --}}{{--
+				@yield('advances', '')
+			</td>
+			<td style="width: 50%; vertical-align: top;" class="no-border">
+				--}}{{-- Settlement --}}{{--
+				@yield('settlement', '')
+			</td>
+		</tr>--}}
+	</table>
+	@yield('final', '')
+	<div class="column">
+		<p><strong>Uwagi:</strong></p>
+		@yield('notes', '')
+	</div>
+</body>
+</html>
---
+++ resources/views/payment/invoices/corrective.blade.php
@@ -0,0 +1,193 @@
+@extends('payment.invoices.corrective-layout')
+
+@section('title')
+	Faktura korekta
+@endsection
+
+@section('invoice-data')
+	<table>
+		<tr>
+			<th>Faktura korygująca numer</th>
+			<th>{{ $invoiceData['full_number'] }}</th>
+		</tr>
+		<tr>
+			<td>Numer dokumentu korygowanego:</td>
+			<td>{{ $correctedInvoice['number'] }}</td>
+		</tr>
+		<tr>
+			<td>Data wystawienia dokumentu korygowanego:</td>
+			<td>{{ $invoiceData['date'] }}</td>
+		</tr>
+		<tr>
+			<td>Data wystawienia korekty:</td>
+			<td>{{ $invoiceData['payment_date'] }}</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<strong>Powód korekty: </strong>
+				{{ $reason }}
+			</td>
+		</tr>
+	</table>
+@endsection
+
+@section('buyer')
+	{{ $buyer['name'] }}<br>
+	{{ $buyer['address'] }}<br>
+	{{ $buyer['zip'] }}, {{ $buyer['city'] }}<br>
+	{{ $buyer['country'] }}<br>
+	{{ $buyer['nip'] }}
+@endsection
+
+@section('before-orders-title')
+	Zamówienie przed korektą
+@endsection
+
+@section('before-orders-list')
+	@foreach ($ordersList as $index => $order)
+		<tr>
+			{{-- L.p. --}}
+			<td>{{ $index + 1 }}</td>
+			{{-- Nazwa produktu --}}
+			<td>{{ $order['product_name'] }}</td>
+			{{-- Jednostka --}}
+			<td>{{ $order['unit'] }}</td>
+			{{-- Ilość --}}
+			<td>{{ $order['amount'] }}</td>
+			{{-- Cena brutto --}}
+			<td>{{ $order['priceGross'] }}zł</td>
+			{{-- VAT --}}
+			<td>{{ $order['vat'] }}</td>
+			{{-- Wartość netto --}}
+			<td>{{ $order['priceNet'] }}zł</td>
+			{{-- Wartość brutto --}}
+			<td>{{ $order['priceGross'] }}zł</td>
+		</tr>
+	@endforeach
+@endsection
+
+@section('after-orders-title')
+	Zamówienie po korekcie
+@endsection
+
+@section('after-orders-list')
+	@foreach ($ordersCorrected as $index => $order)
+		<tr>
+			{{-- L.p. --}}
+			<td>{{ $index + 1 }}</td>
+			{{-- Nazwa produktu --}}
+			<td>{{ $order['product_name'] }}</td>
+			{{-- Jednostka --}}
+			<td>{{ $order['unit'] }}</td>
+			{{-- Ilość --}}
+			<td>{{ $order['amount'] }}</td>
+			{{-- Cena brutto --}}
+			<td>{{ $order['priceGross'] }}zł</td>
+			{{-- VAT --}}
+			<td>{{ $order['vat'] }}</td>
+			{{-- Wartość netto --}}
+			<td>{{ $order['priceNet'] }}zł</td>
+			{{-- Wartość brutto --}}
+			<td>{{ $order['priceGross'] }}zł</td>
+		</tr>
+	@endforeach
+@endsection
+
+
+@section('orders-summary')
+	@if($taxDifference['vat'] < 0)
+		<h4>Zmniejszenie podatku należnego</h4>
+	@else
+		<h4>Zwiększenie podatku należnego</h4>
+	@endif
+	<table>
+		<tr>
+			<th>Stawka VAT</th>
+			<th>Wartość netto</th>
+			<th>Kwota VAT</th>
+			<th>Wartość brutto</th>
+		</tr>
+		<tr>
+			<td>{{ $order['vat'] }}</td>
+			<td>{{ $taxDifference['net'] }}zł</td>
+			<td>{{ $taxDifference['vat'] }}zł</td>
+			<td>{{ $taxDifference['gross'] }}zł</td>
+		</tr>
+		<tr>
+			<td><strong>Razem:</strong></td>
+			<td>{{ $taxDifference['net'] }}zł</td>
+			<td>{{ $taxDifference['vat'] }}zł</td>
+			<td>{{ $taxDifference['gross'] }}zł</td>
+		</tr>
+	</table>
+@endsection
+
+{{--@section('settlement')
+	<h4>Rozliczenie wg stawek</h4>
+	<table>
+		<tr>
+			<th>Stawka VAT</th>
+			<th>Wartość netto</th>
+			<th>Kwota VAT</th>
+			<th>Wartość brutto</th>
+		</tr>
+		<tr>
+			<td>{{ $order['vat'] }}</td>
+			<td>{{ $settlement['priceNet'] }}zł</td>
+			<td>{{ $settlement['vatValue'] }}zł</td>
+			<td>{{ $settlement['priceGross'] }}zł</td>
+		</tr>
+		<tr>
+			<td><strong>Razem:</strong></td>
+			<td>{{ $settlement['priceNet'] }}zł</td>
+			<td>{{ $settlement['vatValue'] }}zł</td>
+			<td>{{ $settlement['priceGross'] }}zł</td>
+		</tr>
+	</table>
+@endsection--}}
+
+{{--@section('advances')
+	<h4>Poprzednie zaliczki</h4>
+	<table>
+		<tr>
+			<th>Lp</th>
+			<th>Numer faktury</th>
+			<th>Data</th>
+			<th>Brutto</th>
+		</tr>
+		@foreach($previousAdvances as $index => $invoice)
+			<tr>
+				<td>{{ $index + 1 }}</td>
+				<td>{{ $invoice->full_number }}</td>
+				<td>{{ $invoice->created_at->format('d-m-Y') }}</td>
+				<td>{{ $invoice->amount }}zł</td>
+			</tr>
+		@endforeach
+		<tr>
+			<td class="no-border">&nbsp;</td>
+			<td class="no-border">&nbsp;</td>
+			<td><strong>Razem:</strong></td>
+			<td>{{ $previousAdvances->sum('amount') }}zł</td>
+		</tr>
+	</table>
+@endsection--}}
+
+@section('notes')
+	<ul>
+		@foreach ($notes as $note)
+			<li>{{ $note }}</li>
+		@endforeach
+	</ul>
+@endsection
+
+@section('summary')
+	<p>Metoda płatności: <strong>{{ $invoiceData['payment_method'] }}</strong></p>
+	<p>Wpłacono: <strong>{{ $paid }}zł</strong></p>
+	<p style="font-size: small">
+		@if($difference < 0)
+			<strong>Do zwrotu: {{ $difference * -1 }}zł</strong>
+		@else
+			<strong>Do zapłaty: {{ $difference }}zł</strong>
+		@endif
+	</p>
+@endsection

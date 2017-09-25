7fe23b6b3bc75f7b2067146a53fcfd179c335c93
adamkarminski <adamkarminski@gmail.com>
2017-09-25 19:39:40 +0200

    Add info about 50% discount not including the physical materials

--- resources/lang/pl/payment.php
+++ resources/lang/pl/payment.php
@@ -171,5 +171,5 @@ return [
 
 	'confirm-change-order'          => 'Chcę poprawić dane',
 	'personal-data-invoice-heading' => 'Dane do faktury',
-	'voucher-current-explanation' => 'Pamiętaj, zniżka dotyczy tylko Kursu internetowego i nie zostanie dołączona przy zamówieniu Kursu stacjonarnego!',
+	'voucher-current-explanation' => 'Pamiętaj, zniżka dotyczy tylko Kursu internetowego i nie zostanie dołączona przy zamówieniu Kursu stacjonarnego! Cena nie obejmuje nowych materiałów. Możesz dokupić je osobno, korzystając z linku <a href="https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-album">https://platforma.wiecejnizlek.pl/payment/personal-data/wnl-album</a>',
 ];

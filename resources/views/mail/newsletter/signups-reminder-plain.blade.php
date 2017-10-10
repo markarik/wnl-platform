d3cc3de59890cc6586e70cbd95f10cfc0dea884d
adamkarminski <adamkarminski@gmail.com>
2017-10-10 18:34:34 +0200

    Signups reminder

---
+++ resources/views/mail/newsletter/signups-reminder-plain.blade.php
@@ -0,0 +1,15 @@
+Cześć!
+
+Połowa zapisówa za nami, połowa miejsc zajęta! Bardzo cieszymy się, że wielu z Was skorzystało z promocji Study Buddy! W towarzystwie zawsze raźniej! ;)
+
+Zapisy trwają jeszcze przez 2 tygodnie! W tym czasie możesz jeszcze zaprosić znajomą osobę na kurs - skorzystacie na tym obydwoje! ;)
+
+Myślisz o kursie, ale masz wątpliwości? Napisz do nas na facebooku (https://facebook.com/wiecejnizlek) lub na info@wiecejnizlek.pl - odpowiadamy na wszystkie pytania! ;)
+
+Zapisz się na kurs - https://platforma.wiecejnizlek.pl/payment/select-product
+
+Niezdecydowanym niezmiennie polecamy odwiedziny na demo naszej platformy - https://demo.wiecejnizlek.pl/. :)
+
+Do zobaczenia na kursie!
+
+Z serdecznymi pozdrowieniami,
---
+++ resources/views/mail/newsletter/signups-reminder.blade.php
@@ -0,0 +1,31 @@
+@extends('mail.newsletter.layout')
+
+@section('content')
+
+	<h3>Cześć!</h3>
+
+	<p>
+		Połowa zapisówa za nami, połowa miejsc zajęta! Bardzo cieszymy się, że wielu z Was skorzystało z promocji Study Buddy! W towarzystwie zawsze raźniej! ;)
+	</p>
+	<p>
+		Zapisy trwają jeszcze przez 2 tygodnie! W tym czasie możesz jeszcze zaprosić znajomą osobę na kurs - skorzystacie na tym obydwoje! ;)
+	</p>
+	<p>
+		Myślisz o kursie, ale masz wątpliwości? Napisz do nas na <a href="https://facebook.com/wiecejnizlek" target="_blank">facebooku</a> lub na info@wiecejnizlek.pl - odpowiadamy na wszystkie pytania! ;)
+	</p>
+
+	<div style="text-align: center; margin: 10px auto 20px;">
+		<a class="button" target="_blank" href="https://platforma.wiecejnizlek.pl/payment/select-product">
+			Zapisz się na kurs
+		</a>
+	</div>
+
+	<p>
+		Niezdecydowanym niezmiennie polecamy odwiedziny na <a target="_blank" href="https://demo.wiecejnizlek.pl/">demo naszej platformy</a>. :)
+	</p>
+
+	<p>Do zobaczenia na kursie!</p>
+
+	<p>Z serdecznymi pozdrowieniami,</p>
+
+@endsection

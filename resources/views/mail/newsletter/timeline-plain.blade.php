e483c91a3caf6125767b191083c97131d5f4bac5
adamkarminski <adamkarminski@gmail.com>
2017-10-31 20:25:50 +0100

    Add a course timeline email

---
+++ resources/views/mail/newsletter/timeline-plain.blade.php
@@ -0,0 +1,20 @@
+	Cześć %recipient_name%!
+
+	Na wstępie chcielibyśmy gorąco podziękować Ci za zaufanie i dołączenie do kursu “Więcej niż LEK”! Nie możemy już doczekać się wspólnej nauki! I mamy nadzieję, że Ty również. ;)
+
+	W związku z nadchodzącym startem kursu chcielibyśmy przybliżyć Ci co wydarzy się w najbliższych dniach. :)
+
+	1 - 3 listopada
+	W tych dniach będziemy prowadzić na platformie prace związane z uruchomieniem drugiej edycji. Jeśli jesteś uczestnikiem pierwszej edycji, będziesz mieć w tym czasie dostęp do kursu. Możesz natomiast spotkać się z przerwami w funkcjonowaniu samej platformy oraz poszczególnych lekcji. Prosimy o wyrozumiałość! :)
+	Ze względu na poważne zmiany w materiałach, część slajdów zapisanych w Twoich Kolekcjach może niestety zniknąć. Nie będą one już po prostu występowały w nowych, poprawionych prezentacjach. Postaramy się jednak zachować wszystkie slajdy, których treść nie uległa zmianie. :)
+
+	4 listopada
+	Otwieramy 2. edycję kursu "Więcej niż LEK"! Tego dnia w Poznaniu odbędą się pierwsze warsztaty dla zapisanych na kurs stacjonarny. W tym samym czasie, na platformie pojawi się lekcja "0", czyli Wstęp do kursu. :) Bardzo zachęcamy do poświęcenia jej czasu w weekend, ponieważ zawiera wiele cennych wyjaśnień, które pozwolą Ci lepiej skorzystać z kursu. :)
+	Każdy z poruszanych aspektów będzie posiadał osobny ekran z możliwością dyskusji, do której serdecznie Cię zapraszamy! Uczestnicy pierwszej edycji wiedzą już, jaka potęga drzemie w wymianie wiedzy oraz pytań paruset osób! ;)
+
+	6 listopada
+	Ruszamy z programem kursu! Na pierwszy ogień - Kardiologia, której poświęcimy cały pierwszy tydzień! :) W razie pytań i wątpliwości, przez pierwsze dni będziemy dla Ciebie dostępni niemal całodobowo. ;) Wiemy, że pierwsze kroki w nowym środowisku mogą być jeszcze niepewne, dlatego postaramy się zapewnić Ci maksimum wsparcia!
+
+	Pozostajemy do Twojej dyspozycji na facebooku (https://facebook.com/wiecejnizlek) oraz na info@wiecejnizlek.pl! Przygodę czas zacząć! :)
+
+	Do zobaczenia!
---
+++ resources/views/mail/newsletter/timeline.blade.php
@@ -0,0 +1,29 @@
+@extends('mail.newsletter.layout')
+
+@section('content')
+
+	<h3>Cześć %recipient_name%!</h3>
+
+	<p><strong>Na wstępie chcielibyśmy gorąco podziękować Ci za zaufanie i dołączenie do kursu “Więcej niż LEK”! Nie możemy już doczekać się wspólnej nauki! I mamy nadzieję, że Ty również. ;)</strong></p>
+
+	<p>W związku z nadchodzącym startem kursu chcielibyśmy przybliżyć Ci co wydarzy się w najbliższych dniach. :)</p>
+
+<hr style="border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));">
+
+	<h4>1 - 3 listopada</h4>
+	<p>W tych dniach będziemy prowadzić na platformie prace związane z uruchomieniem drugiej edycji. Jeśli jesteś uczestnikiem pierwszej edycji, będziesz mieć w tym czasie dostęp do kursu. Możesz natomiast spotkać się z przerwami w funkcjonowaniu samej platformy oraz poszczególnych lekcji. Prosimy o wyrozumiałość! :)</p>
+	<p>Ze względu na poważne zmiany w materiałach, część slajdów zapisanych w Twoich Kolekcjach może niestety zniknąć. Nie będą one już po prostu występowały w nowych, poprawionych prezentacjach. Postaramy się jednak zachować wszystkie slajdy, których treść nie uległa zmianie. :)</p>
+
+	<h4>4 listopada</h4>
+	<p>Otwieramy 2. edycję kursu "Więcej niż LEK"! Tego dnia w Poznaniu odbędą się pierwsze warsztaty dla zapisanych na kurs stacjonarny. W tym samym czasie, na platformie pojawi się lekcja "0", czyli Wstęp do kursu. :) Bardzo zachęcamy do poświęcenia jej czasu w weekend, ponieważ zawiera wiele cennych wyjaśnień, które pozwolą Ci lepiej skorzystać z kursu. :)</p>
+	<p>Każdy z poruszanych aspektów będzie posiadał osobny ekran z możliwością dyskusji, do której serdecznie Cię zapraszamy! Uczestnicy pierwszej edycji wiedzą już, jaka potęga drzemie w wymianie wiedzy oraz pytań paruset osób! ;)</p>
+
+	<h4>6 listopada</h4>
+	<p>Ruszamy z programem kursu! Na pierwszy ogień - Kardiologia, której poświęcimy cały pierwszy tydzień! :) W razie pytań i wątpliwości, przez pierwsze dni będziemy dla Ciebie dostępni niemal całodobowo. ;) Wiemy, że pierwsze kroki w nowym środowisku mogą być jeszcze niepewne, dlatego postaramy się zapewnić Ci maksimum wsparcia!</p>
+
+<hr style="border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));">
+
+	<p>Pozostajemy do Twojej dyspozycji na <a href="https://facebook.com/wiecejnizlek">facebooku</a> oraz na info@wiecejnizlek.pl! Przygodę czas zacząć! :)</p>
+
+	<p>Do zobaczenia!</p>
+@endsection

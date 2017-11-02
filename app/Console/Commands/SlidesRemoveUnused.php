903158161caaeca9a4229fcd0b932ad9357d639a
kubakarminski <jlkarminski@gmail.com>
2017-11-02 14:41:10 +0100

    Add command for removing unused slides;

---
+++ app/Console/Commands/SlidesRemoveUnused.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Slide;
+use Illuminate\Console\Command;
+
+class SlidesRemoveUnused extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'slides:removeUnused';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$slides = Slide::whereDoesntHave('slideshow')->get();
+
+		$this->info($slides->count() . ' slides without slideshow found.');
+		$ids = $slides->pluck('id')->toArray();
+
+		\DB::table('taggables')
+			->where('taggable_type', 'App\\Models\\Slide')
+			->whereIn('taggable_id', $ids)
+			->delete();
+
+		\DB::table('reactables')
+			->where('reactable_type', 'App\\Models\\Slide')
+			->whereIn('reactable_id', $ids)
+			->delete();
+
+		Slide::whereDoesntHave('slideshow')->delete();
+
+		return;
+	}
+}

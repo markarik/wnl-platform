70f20af22c019fb175408743c5ac97caf18cc599
kubakarminski <jlkarminski@gmail.com>
2017-11-13 20:45:45 +0100

    (Task assignee notifications) Fire event on task update;

---
+++ app/Events/Tasks/AssignedToTask.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Events\Tasks;
+
+use App\Events\Event;
+use App\Models\Task;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AssignedToTask extends Event
+{
+	use Dispatchable,
+		InteractsWithSockets,
+		SerializesModels;
+
+	public $task;
+
+	public $actor;
+
+	/**
+	 * Create a new event instance.
+	 *
+	 * @param Task $task
+	 */
+	public function __construct(Task $task)
+	{
+		parent::__construct();
+		$this->task = $task;
+		$this->actor = clone \Auth::user();
+	}
+
+	public function transform()
+	{
+		$task = $this->task;
+
+		$this->data = [
+			'event'   => 'assigned-to-task',
+			'objects' => [
+//				'author' => $commentable->user->id ?? null,
+//				'type' => $commentableType,
+//				'id'   => $commentable->id,
+//				'text' => $this->sanitize($commentable->text ?? ''),
+//				'snippet' => $commentable->snippet ?? [],
+			],
+			'subject' => [
+//				'type' => 'comment',
+//				'id'   => $comment->id,
+//				'text' => $this->sanitize($comment->text),
+			],
+			'actors'  => [
+				'id'         => $this->actor->id,
+				'first_name' => $this->actor->profile->first_name,
+				'last_name'  => $this->actor->profile->last_name,
+				'full_name'  => $this->actor->profile->full_name,
+				'avatar'     => $this->actor->profile->avatar_url,
+			],
+			'referer' => $this->referer,
+		];
+	}
+}

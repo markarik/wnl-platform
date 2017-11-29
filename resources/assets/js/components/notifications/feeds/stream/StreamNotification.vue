71d9e8082860181c5d553c8c86537425a6c0fe60
Przemek Ferkaluk <pferkaluk@gmail.com>
2017-11-29 12:58:03 +0100

    change full_name for display_name at all notifications

--- resources/assets/js/components/notifications/feeds/stream/StreamNotification.vue
+++ resources/assets/js/components/notifications/feeds/stream/StreamNotification.vue
@@ -9,7 +9,7 @@
 			</div>
 			<div class="notification-content">
 				<div class="notification-header">
-					<span class="actor">{{ message.actors.full_name }}</span>
+					<span class="actor">{{ nameToDisplay }}</span>
 					<span class="action">{{ action }}</span>
 					<span class="object">{{ object }}</span>
 					<span class="context">{{ contextInfo }}</span>
@@ -193,6 +193,9 @@
 		},
 		computed: {
 			...mapGetters(['currentUserId', 'isMobile', 'isTouchScreen']),
+			nameToDisplay() {
+				return this.message.actors.display_name || this.message.actors.full_name
+			},
 			action() {
 				return this.$t(`notifications.events.${camelCase(this.message.event)}`)
 			},

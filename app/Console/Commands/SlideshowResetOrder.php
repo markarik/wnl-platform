c9fa6b50c5c07f5f3ab68483a6d9d4449e7b990a
kubakarminski <jlkarminski@gmail.com>
2017-12-13 19:00:17 +0100

    Add command for resetting slideshow's order numbers;

---
+++ app/Console/Commands/SlideshowResetOrder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Presentable;
+use Illuminate\Console\Command;
+
+class SlideshowResetOrder extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'slideshow:resetOrder {slideshowId}';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$slideshowId = $this->argument('slideshowId');
+		$presentables = Presentable::select()
+			->where([
+				['presentable_type', '=', 'App\\Models\\Slideshow'],
+				['presentable_id', '=', $slideshowId],
+			])
+			->orderBy('order_number', 'asc')
+			->get();
+
+		foreach ($presentables as $index => $presentable) {
+			$presentable->order_number = $index;
+			$presentable->save();
+		}
+
+		return;
+	}
+}

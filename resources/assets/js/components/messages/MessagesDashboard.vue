57b1ec4e00d7fca8f1aaac22dccb7757b6d25bf7
kubakarminski <jlkarminski@gmail.com>
2018-01-01 17:39:54 +0100

    Create messages page;

---
+++ resources/assets/js/components/messages/MessagesDashboard.vue
@@ -0,0 +1,57 @@
+<template lang="html">
+	<div class="wnl-app-layout wnl-course-layout">
+		<wnl-sidenav-slot
+				:isVisible="isSidenavVisible"
+				:isDetached="!isSidenavMounted"
+		>
+			<wnl-main-nav :isHorizontal="!isSidenavMounted"></wnl-main-nav>
+			<aside class="sidenav-aside help-sidenav">
+				siema
+			</aside>
+		</wnl-sidenav-slot>
+		<div class="wnl-course-content wnl-column">
+			<div class="scrollable-main-container">
+				<wnl-private-chat :users="chatUsers"></wnl-private-chat>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="sass">
+</style>
+
+<script>
+	import {mapActions, mapGetters} from 'vuex'
+
+	import MainNav from 'js/components/MainNav'
+	import PublicChat from 'js/components/chat/PublicChat'
+	import PrivateChat from 'js/components/chat/PrivateChat'
+	import Sidenav from 'js/components/global/Sidenav'
+	import SidenavSlot from 'js/components/global/SidenavSlot'
+	import withChat from 'js/mixins/with-chat'
+
+	export default {
+		name: 'MessagesDashboard',
+		components: {
+			'wnl-main-nav': MainNav,
+			'wnl-public-chat': PublicChat,
+			'wnl-sidenav': Sidenav,
+			'wnl-sidenav-slot': SidenavSlot,
+			'wnl-private-chat': PrivateChat,
+		},
+		computed: {
+			...mapGetters([
+				'isSidenavVisible',
+				'isSidenavMounted',
+				'isChatMounted',
+				'isChatVisible',
+				'isChatToggleVisible',
+				'currentUserName'
+			]),
+			...mapGetters('course', ['ready']),
+			chatUsers() {
+				return [1,2]
+			},
+		}
+	}
+</script>

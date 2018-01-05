c8641ecb28d05e15dd229c6cb0d418ff796edcc4
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-01-05 14:13:28 +0100

    use notifications queue

--- app/Listeners/ContentUpdatesGate.php
+++ app/Listeners/ContentUpdatesGate.php
@@ -8,15 +8,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class ContentUpdatesGate implements ShouldQueue
 {
 	/**
-	 * Create the event listener.
-	 *
-	 */
-	public function __construct()
-	{
-		//
-	}
-
-	/**
 	 * Handle the event.
 	 *
 	 * @param  $event

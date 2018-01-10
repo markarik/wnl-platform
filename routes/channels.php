003a127d9247e730d18da4c5bdfc42ce6d049232
Przemek Ferkaluk <pferkaluk@gmail.com>
2018-01-10 12:51:51 +0100

    set users data on presents channel

--- routes/channels.php
+++ routes/channels.php
@@ -10,7 +10,18 @@ Broadcast::channel('active-users', function ($user) {
 		'email' => $user->email,
 		'avatar' => $user->profile->avatar_url,
 		'fullName' => $user->profile->full_name,
-		'profile' => $user->profile,
+		'profile' => [
+			'avatar' => $user->profile->avatar_url,
+			'city' => $user->profile->city,
+			'display_name' => $user->profile->display_name,
+			'first_name' => $user->profile->first_name,
+			'full_name' => $user->profile->full_name,
+			'help' => $user->profile->help,
+			'id' => $user->profile->id,
+			'last_name' => $user->profile->last_name,
+			'learning_location' => $user->profile->learning_location,
+			'user_id' => $user->profile->user_id,
+		],
 	];
 });
 

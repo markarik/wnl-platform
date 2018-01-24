3eb2f24e1598cbd6281a903007c5c7b65167facb
kubakarminski <jlkarminski@gmail.com>
2018-01-24 17:21:36 +0100

    Add slack notifications for tech help Q&As;

---
+++ app/Notifications/TaskCreated.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Illuminate\Notifications\Notification;
+
+class TaskCreated extends Notification implements ShouldQueue
+{
+	use Queueable;
+	protected $task;
+
+	/**
+	 * Create a new notification instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($task)
+	{
+		$this->task = $task;
+	}
+
+	/**
+	 * Get the notification's delivery channels.
+	 *
+	 * @param  mixed $notifiable
+	 *
+	 * @return array
+	 */
+	public function via($notifiable)
+	{
+		return ['slack'];
+	}
+
+	/**
+	 * Send notification to slack channel
+	 *
+	 * @param $notifiable
+	 *
+	 * @return SlackMessage
+	 */
+	public function toSlack($notifiable)
+	{
+		return (new SlackMessage)
+			->success()
+			->content($this->task->text)
+			->attachment(function ($attachment) {
+				$attachment
+					->title('Jedziesz Szwagier', url('app/help/tech'));
+			});
+	}
+
+	/**
+	 * Get the array representation of the notification.
+	 *
+	 * @param  mixed $notifiable
+	 *
+	 * @return array
+	 */
+	public function toArray($notifiable)
+	{
+		return [
+			//
+		];
+	}
+}

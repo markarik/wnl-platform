cef401844c539545ba6be941f216528ba27098a3
preziu <pferkaluk@gmail.com>
2018-02-01 13:02:48 +0100

    revert changes

--- resources/assets/js/admin/components/slides/SlidePreview.vue
+++ resources/assets/js/admin/components/slides/SlidePreview.vue
@@ -1,49 +1,34 @@
 <template>
-	<div ref="preview-modal" class="modal" :class="{'is-active': showModal}">
-		<div class="modal-background" @click="$emit('closeModal')"></div>
-		<div class="modal-content">
-			<iframe :srcdoc="content"/>
-		</div>
-		<button class="modal-close is-large" aria-label="close" @click="$emit('closeModal')"></button>
-	</div>
+    <div ref="preview-modal" class="modal" :class="{'is-active': showModal}">
+        <div class="modal-background" @click="$emit('closeModal')"></div>
+        <div class="modal-content">
+            <iframe :srcdoc="content"/>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('closeModal')"></button>
+    </div>
 </template>
 
 <style lang="sass" rel="stylesheet/sass" scoped>
-	@import 'resources/assets/sass/variables'
-	.modal
-		z-index: $z-index-alerts
-
 	.modal-content
 		width: 90vw
 		height: 90vh
-
-		frame
+		iframe
 			width: 100%
 			height: 100%
 </style>
 
 <script>
-	export default {
-		name: 'SlidePreview',
-		data() {
-			return {
-				modal: false,
-			}
-		},
-		props: {
-			content: {
-				type: String,
-				default: ''
-			},
-			showModal: {
-				type: Boolean,
-				default: false
-			}
-		},
-		methods: {
-			closeModal() {
-				this.modal = false
-			}
-		}
-	}
+    export default {
+        name: 'SlidePreview',
+        props: {
+            content: {
+                type: String,
+                default: ''
+            },
+            showModal: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
 </script>

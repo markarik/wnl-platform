8a12d3639f90e1db4ae2fc18a1a5ac998c948b58
kubakarminski <jlkarminski@gmail.com>
2018-02-07 12:40:08 +0100

    Messages persisted in databse using queue;

--- app/Console/Commands/ProcessChatQueue.php
+++ app/Console/Commands/ProcessChatQueue.php
@@ -2,45 +2,57 @@
 
 namespace App\Console\Commands;
 
+use App\Models\ChatRoom;
 use Illuminate\Console\Command;
 
 class ProcessChatQueue extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'chat:process-queue';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'chat:process-queue';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Consume messages queue with chat events';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Consume messages queue with chat events';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
 		\Rabbit::consume('chat-events', function ($message, $resolver) {
-			dump($message->body);
+
+			$data = json_decode($message->body);
+			if ($data) {
+				$room = ChatRoom::ofName($data->room)->first();
+				$room->messages()->create([
+					'user_id' => $data->message->user_id,
+					'content' => $data->message->content,
+					'time'    => $data->message->time,
+				]);
+			}
+
+			// TODO: What should be done if message format is invalid ??
 
 			$resolver->acknowledge($message);
 		});
-    }
+	}
 }

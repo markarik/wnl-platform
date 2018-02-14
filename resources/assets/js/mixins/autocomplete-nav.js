c47e3b92446520848ffac77c6a0aa870443f9bc7
kubakarminski <jlkarminski@gmail.com>
2018-02-14 14:28:02 +0100

    Move autocomplete's navigation logic to a mixin;

---
+++ resources/assets/js/mixins/autocomplete-nav.js
@@ -0,0 +1,79 @@
+export default {
+	computed: {
+		hasItems() {
+			return this.items && this.items.length
+		},
+	},
+	methods: {
+		onKeyDown(evt) {
+			switch (evt.keyCode) {
+				case 38:
+					evt.stopPropagation()
+					this.onArrowUp(evt)
+					break
+				case 40:
+					evt.stopPropagation()
+					this.onArrowDown(evt)
+					break
+				case 13:
+					this.onEnter(evt)
+					break
+				case 27:
+					this.$emit('close')
+					break
+			}
+		},
+		onArrowUp() {
+			if (!this.hasItems) return
+
+			const activeIndex = this.getActiveItem();
+			if (activeIndex <= 0) {
+				this.$set(this.items[this.items.length - 1], 'active', true);
+			} else {
+				this.$set(this.items[activeIndex - 1], 'active', true)
+			}
+
+			if (activeIndex >= 0) this.$set(this.items[activeIndex], 'active', false)
+
+			//Something would steal the focus back to the Quill input when if we'd do it synchronously
+			this.$nextTick(() => {
+				this.$el.focus();
+			})
+		},
+		onArrowDown() {
+			if (!this.hasItems) return
+
+			const activeIndex = this.getActiveItem();
+
+			if (activeIndex < 0 || activeIndex === this.items.length - 1) {
+				this.$set(this.items[0], 'active', true)
+			} else {
+				this.$set(this.items[activeIndex + 1], 'active', true)
+			}
+
+			if (activeIndex > -1) this.$set(this.items[activeIndex], 'active', false)
+
+			//Something would steal the focus back to the Quill input when if we'd do it synchronously
+			this.$nextTick(() => {
+				this.$el.focus();
+			})
+		},
+
+		onEnter(evt) {
+			const activeIndex = this.getActiveItem();
+
+			if (activeIndex < 0) return
+
+			this.$set(this.items[activeIndex], 'active', false)
+			this.onItemChosen(this.items[activeIndex])
+
+			evt.preventDefault();
+			evt.stopPropagation();
+			return false
+		},
+
+		getActiveItem() {
+			return this.items.findIndex((item) => item.active)
+		},
+	}
+}

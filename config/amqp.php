f75119482191a218424815ce01c10df934272915
kubakarminski <jlkarminski@gmail.com>
2018-02-21 14:44:23 +0100

    Move message queue config to .env

--- config/amqp.php
+++ config/amqp.php
@@ -2,19 +2,19 @@
 
 return [
 
-	'use' => 'production',
+	'use' => 'common',
 
 	'properties' => [
 
-		'production' => [
-			'host'                => 'localhost',
-			'port'                => 5672,
-			'username'            => 'guest',
-			'password'            => 'guest',
+		'common' => [
+			'host'                => env('AMQP_HOST'),
+			'port'                => env('AMQP_PORT'),
+			'username'            => env('AMQP_USER'),
+			'password'            => env('AMQP_PASS'),
 			'vhost'               => '/',
 			'exchange'            => 'messages',
 			'exchange_type'       => 'direct',
-			'consumer_tag'        => 'consumer',
+//			'consumer_tag'        => 'consumer',
 			'ssl_options'         => [], // See https://secure.php.net/manual/en/context.ssl.php
 			'connect_options'     => [], // See https://github.com/php-amqplib/php-amqplib/blob/master/PhpAmqpLib/Connection/AMQPSSLConnection.php
 			'queue_properties'    => ['x-ha-policy' => ['S', 'all']],

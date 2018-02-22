b334843c76c717c2724f6b946912e7437f5dddf4
adamkarminski <adamkarminski@gmail.com>
2018-02-23 00:21:34 +0100

    Update certificates email

--- resources/views/mail/certificate.blade.php
+++ resources/views/mail/certificate.blade.php
@@ -3,13 +3,11 @@
 @section('content')
 	<h3>Cześć {{ $user->first_name or '{first_name}' }}!</h3>
 
-	<p>Chcieliśmy pogratulować Ci długiej i systematycznej pracy do LEK-u! Jesteśmy pod wielkim wrażeniem motywacji, którą udało Ci się utrzymać!</p>
+	<p>Dziękujemy Ci za udział w drugiej edycji kursu! W załączniku znajdziesz certyfikat jego ukończenia. :) Jeżeli planujesz uzyskać refundację kursu z Izby Lekarskiej, to może okazać się przydatny. ;) Jest on również dowodem przyznania punktów edukacyjnych.</p>
 
-	<p>Dziękujemy Ci za udział w pierwszej edycji kursu, która dobiega już końca. W załączniku znajdziesz symboliczny certyfikat uczestnictwa. :) Jeżeli planujesz uzyskać refundację kursu z Izby Lekarskiej, to na pewno okaże się przydatny. ;)</p>
+	<p>Na koniec mamy też do Ciebie skromną prośbę o podzielenie się swoją oceną kursu na <a href="https://www.facebook.com/pg/wiecejnizlek/reviews/" target="_blank">facebooku</a> oraz <a href="https://goo.gl/forms/sYLtHFS44WKL88Rp2">podzielenie się swoim wynikiem</a>! Rezultaty osób, które rzeczywiście ukończyły kurs są dla nas niezwykle istotne. Rozumiemy, że teraz pewnie Twoje myśli odbiegły już od nauki i kursu, ale może ten mail skłoni Cię do krótkiego powrotu do tego kontekstu. ;) </p>
 
-	<p>Kończąc, mamy do Ciebie skromną prośbę o podzielenie się swoją oceną kursu na <a href="https://www.facebook.com/pg/wiecejnizlek/reviews/" target="_blank">facebooku</a> oraz wypełnienie <a href="https://goo.gl/forms/C1mQ0MUwUzZBJyTO2">ostatniej ankiety ewaluacyjnej</a>. Rozumiemy, że teraz pewnie Twoje myśli krążą wokół nauki, ale może ten mail przypomni Ci o naszej prośbie po egzaminie. ;) </p>
-
-	<p>Jeszcze raz gratulujemy Ci wytrwałości i życzymy powodzenia, zarówno na ostatniej prostej, jak i samym egzaminie!</p>
+	<p>Jeszcze raz gratulujemy Ci wytrwałości i życzymy powodzenia na dalszej drodze edukacyjnej!</p>
 
 	<p>Do zobaczenia!</p>
 @endsection

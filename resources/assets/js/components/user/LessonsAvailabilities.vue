e243349d4a609112d2ae3960e67826972e405463
preziu <pferkaluk@gmail.com>
2018-03-19 13:20:21 +0100

    set view

---
+++ resources/assets/js/components/user/LessonsAvailabilities.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="scrollable-main-container">
+		<div class="level wnl-screen-title">
+			<div class="level-left">
+				<div class="level-item big strong">
+					Dostępność lekcji
+				</div>
+			</div>
+		</div>
+        <div class="wujek">
+            halko
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    
+}
+</script>

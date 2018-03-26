b295853324e3b55e6dfefa8e0b4afe2c94a2bdef
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-03-26 13:32:59 +0200

    clear cache on date change

--- app/Http/Controllers/Api/PrivateApi/UserLessonAvailabilitiesApiController.php
+++ app/Http/Controllers/Api/PrivateApi/UserLessonAvailabilitiesApiController.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ApiController;
 use Carbon\Carbon;
+use Cache;
 use Illuminate\Http\Request;
 use League\Fractal\Resource\Item;
 use App\Http\Requests\Course\UpdateUserLessonAvailability;
@@ -24,13 +25,16 @@ class UserLessonAvailabilitiesApiController extends ApiController
 			'user_id' => $user->id
 		])->first();
 
+
 		if (!$userLessonAvailability) {
 			return $this->respondNotFound();
 		}
 
 		$userLessonAvailability->update([
 			'start_date' => Carbon::parse($request->input('date')),
-		]);
+			]);
+
+		Cache::tags("user-$user->id")->flush();
 
 		return $this->respondOk();
 	}

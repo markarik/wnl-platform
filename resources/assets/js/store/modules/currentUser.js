99b032b5db93dd07163d7ae89a67078113d8a9d5
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-04-23 16:20:39 +0200

    Add initial state for user subscription to avoid race conditions

--- resources/assets/js/store/modules/currentUser.js
+++ resources/assets/js/store/modules/currentUser.js
@@ -18,6 +18,11 @@ const state = {
 		avatar: '',
 		roles: [],
 		user_id: 0,
+		subscription: {
+			dates: {
+				min: 0, max: 0
+			}
+		}
 	},
 	settings: getDefaultSettings(),
 }
@@ -41,7 +46,7 @@ const getters = {
 	isCurrentUserLoading: state => state.loading,
 	currentUserStats: state => state.stats,
 	currentUserSubscriptionDates: state => {
-		console.log(state.profile)
+
 		return state.profile.subscription.dates
 	},
 }
@@ -115,16 +120,6 @@ const actions = {
 		})
 	},
 
-	fetchUserSubscription({commit}) {
-		return _fetchUserSubscription()
-			.then(({data}) => {
-				commit(types.USERS_SET_SUBSCRIPTION, data)
-			})
-			.catch((error) => {
-				$wnl.logger.error(error)
-			})
-	},
-
 	fetchUserSettings({ commit }) {
 		return new Promise((resolve, reject) => {
 			getUserSettings().then((response) => {
@@ -164,10 +159,6 @@ const actions = {
 
 const _fetchUserStats = (userId) => {
 	return axios.get(getApiUrl(`users/${userId}/state/stats`));
-}
-
-const _fetchUserSubscription = () => {
-	return axios.get(getApiUrl('user_subscription/current'));
 }
 
 export default {

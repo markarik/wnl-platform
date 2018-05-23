95b8c76bc0d4e9750e9d8d3410a00d2153b40269
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-05-23 16:10:59 +0200

    youtube iframe styles

--- resources/assets/js/components/user/plan/PlannerGuide.vue
+++ resources/assets/js/components/user/plan/PlannerGuide.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <iframe width="800" height="450" src="https://www.youtube.com/embed/IkPrPRegL_8"
-      frameborder="0" allow="encrypted-media" allowfullscreen></iframe>
+    <iframe
+      class="guide-iframe"
+      src="https://www.youtube.com/embed/IkPrPRegL_8"
+      frameborder="0" allow="encrypted-media" allowfullscreen
+    ></iframe>
   </div>
 </template>
+
+<style lang="sass" scoped>
+  .guide-iframe
+    width: 100%
+    height: 450px
+</style>

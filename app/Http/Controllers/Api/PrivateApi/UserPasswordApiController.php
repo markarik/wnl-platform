e2f4be30bec573f6b7c88f81f1062afbeb90fa80
preziu <pferkaluk@gmail.com>
2018-06-04 12:45:47 +0200

    poc reset password

--- app/Http/Controllers/Api/PrivateApi/UserPasswordApiController.php
+++ app/Http/Controllers/Api/PrivateApi/UserPasswordApiController.php
@@ -1,13 +1,16 @@
 <?php namespace App\Http\Controllers\Api\PrivateApi;
 
-
+use App\Models\User;
 use App\Http\Controllers\Api\ApiController;
 use App\Http\Requests\User\UpdateUserPassword;
+use DB;
 
 class UserPasswordApiController extends ApiController
 {
 	public function put(UpdateUserPassword $request)
 	{
-
+		$user = User::fetch($request->id);
+		$hashedPassword = bcrypt($request->new_password);
+		DB::table('users')->where('id', $user->id)->update(['password' => $hashedPassword]);
 	}
 }

8be21b4c05dcafdc88740c835575954ca8128409
preziu <pferkaluk@gmail.com>
2018-06-06 14:44:12 +0200

    change same passwords translation

--- resources/assets/js/i18n/pl/ui.js
+++ resources/assets/js/i18n/pl/ui.js
@@ -28,7 +28,7 @@ export const ui = {
 	error: {
 		defaultErrorHandle: 'Ups, coś poszło nie tak :(',
 		wrongOldPassword: 'Ups, stare hasło jest nieprawidłowe.',
-		samePasswords: 'Nowe hasło musi być inne niż stare.',
+		samePasswords: 'Nowe hasło różnić się od starego.',
 		notFound: `Ups, zapisywanie nie powiodło się.
 			Najprawdopodobniej treść do której się odwołujesz została usunięta.
 			W przypadku dalszych problemów odśwież stronę.`,

5c15b2b03e831bd291626f565ef7fda2b62cf0da
kubakarminski <jlkarminski@gmail.com>
2018-06-12 16:33:39 +0200

    Make public files public on s3

--- app/Console/Commands/MigrateFilesToS3.php
+++ app/Console/Commands/MigrateFilesToS3.php
@@ -3,68 +3,79 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\FilesystemAdapter;
 
 class MigrateFilesToS3 extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 's3:migrate';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 's3:migrate';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Move all files from local disk to S3';
-    
-    private $local;
-    
-    private $s3;
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Move all files from local disk to S3';
 
 	/**
 	 * Create a new command instance.
 	 *
 	 */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $this->local = \Storage::disk('local');
-        $this->s3 = \Storage::disk('s3');
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$local = \Storage::disk('local');
+		$s3 = \Storage::disk('s3');
 
-        $this->scan('/');
-        
-        return 42;
-    }
+		$this->scan($local, '/', function ($path) use ($s3, $local) {
+			$s3->put($path, $local->get($path));
+			$this->line('Copied ' . $path);
+		});
 
-    private function scan($path) {
-		foreach ($this->local->listContents($path) as $content) {
+		$this->scan($s3, '/public', function ($path) use ($s3) {
+			$s3->setVisibility($path, 'public');
+			$this->line('Made public ' . $path);
+		});
+
+		return 42;
+	}
+
+	/**
+	 * Recursively traverse directory tree on $disk starting from $path
+	 * and execute $callback for each file.
+	 *
+	 * @param FilesystemAdapter $disk
+	 * @param string $path
+	 * @param callable $callback
+	 */
+	private function scan($disk, $path, $callback)
+	{
+		foreach ($disk->listContents($path) as $content) {
 			if ($content['type'] === 'dir') {
-				$this->scan($content['path']); continue;
+				$this->scan($disk, $content['path'], $callback);
+				continue;
 			}
 
-			if($content['type'] === 'file') {
-				$this->move($content['path']); continue;
+			if ($content['type'] === 'file') {
+				$callback($content['path']);
+				continue;
 			}
 
 			$this->warn('Unsupported content type: ' . $content['type']);
 			continue;
 		}
-	}
-
-	private	function move($path) {
-		$this->s3->put($path, $this->local->get($path));
-		$this->line('Moved ' . $path);
 	}
 }

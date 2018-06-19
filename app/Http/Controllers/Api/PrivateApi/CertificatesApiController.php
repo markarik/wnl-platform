8c5031f4df7191f42914ac3de15368adc013db05
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-06-19 13:48:43 +0200

    move font directory

--- app/Http/Controllers/Api/PrivateApi/CertificatesApiController.php
+++ app/Http/Controllers/Api/PrivateApi/CertificatesApiController.php
@@ -48,7 +48,7 @@ class CertificatesApiController extends ApiController
 		$img = Image::make($file);
 
 		$img->text($order->id, 1740, 915, function($font) {
-			$fontFile = base_path('resources/assets/fonts/Roboto_Mono/RobotoMono-Light.ttf');
+			$fontFile = base_path('resources/fonts/Roboto_Mono/RobotoMono-Light.ttf');
 			$font->file($fontFile);
 			$font->size(48);
 		});
@@ -58,20 +58,20 @@ class CertificatesApiController extends ApiController
 				$order->product->course_start->format('j.m.Y'),
 				$order->product->course_end->format('j.m.Y')
 			), 1794, 1372, function($font) {
-			$fontFile = base_path('resources/assets/fonts/Rubik/Rubik-Light.ttf');
+			$fontFile = base_path('resources/fonts/Rubik/Rubik-Light.ttf');
 			$font->file($fontFile);
 			$font->size(54);
 		});
 
 		$img->text($order->user->profile->fullName, 1754, 1150, function($font) {
-			$fontFile = base_path('resources/assets/fonts/Rubik/Rubik-Medium.ttf');
+			$fontFile = base_path('resources/fonts/Rubik/Rubik-Medium.ttf');
 			$font->file($fontFile);
 			$font->size(86);
 			$font->align('center');
 		});
 
 		$img->text($order->product->course_start->format('j / m / Y') . ' r., Poznań', 2365, 1650, function($font) {
-			$fontFile = base_path('resources/assets/fonts/Rubik/Rubik-Medium.ttf');
+			$fontFile = base_path('resources/fonts/Rubik/Rubik-Medium.ttf');
 			$font->file($fontFile);
 			$font->size(61);
 			$font->align('center');

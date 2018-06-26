faee5eefac2a1aa5d5989ee38d7309bea6337a67
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-06-26 12:19:18 +0200

    constaint error

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -86,10 +86,7 @@ class UsersTableSeeder extends Seeder
 
 		DB::table('user_subscription')->insert([
 			['user_id' => 1, 'access_start' => Carbon::now()->subDays(5), 'access_end' => Carbon::now()->addDays(5)],
-			['user_id' => 1, 'access_start' => Carbon::now()->subDays(5), 'access_end' => Carbon::now()->addDays(5)],
 			['user_id' => 2, 'access_start' => Carbon::now()->subDays(5), 'access_end' => Carbon::now()->addDays(5)],
-			['user_id' => 2, 'access_start' => Carbon::now()->subDays(5), 'access_end' => Carbon::now()->addDays(5)],
-			['user_id' => 3, 'access_start' => Carbon::now()->subDays(5), 'access_end' => Carbon::now()->addDays(5)],
 			['user_id' => 3, 'access_start' => Carbon::now()->subDays(5), 'access_end' => Carbon::now()->addDays(5)],
 		]);
 	}

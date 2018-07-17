856ded0cb1455c750a382ff6ebc42a2b304a9f56
Preziu <pferkaluk@gmail.com>
2018-07-17 11:21:57 +0200

    change variables' ;

--- resources/assets/js/components/user/MySettings.vue
+++ resources/assets/js/components/user/MySettings.vue
@@ -3,7 +3,7 @@
 		<div class="level wnl-screen-title">
 			<div class="level-left">
 				<div class="level-item big strong">
-					Ustawienia
+					Ustawienia funkcjonalne
 				</div>
 			</div>
 		</div>
@@ -20,8 +20,34 @@
 			<wnl-form-check name="skip_functional_slides">Pomijaj slajdy funkcjonalne</wnl-form-check>
 			<wnl-form-check name="notify_live">Powiadomienia w przeglądarce</wnl-form-check>
 		</wnl-form>
+
+		<div class="level wnl-screen-title">
+			<div class="level-left">
+				<div class="level-item big strong">
+					Ustawienia wyglądu
+				</div>
+			</div>
+		</div>
+
+		<div class="scrollbar">
+			<label>Szerokość scrollbar'a</label>
+			<input class="scrollbar__range" type="range" value="0" min="2" max="20" @change="handleInput">
+			<div class="scrollbar__snippet">
+		</div>
+
+		</div>
 	</div>
 </template>
+
+<style lang="sass" rel="stylesheet/sass" scoped>
+	@import 'resources/assets/sass/variables'
+
+	.scrollbar__snippet
+		height: 30px
+		width: $scrollbar-tiny
+		background-color: black
+
+</style>
 
 <script>
 	import { mapGetters, mapActions } from 'vuex'
@@ -41,6 +67,10 @@
 		},
 		methods: {
 			...mapActions(['changeUserSetting']),
+			handleInput(e) {
+				console.log(e.target.value);
+
+			},
 			onSubmitSuccess(response, newData) {
 				Object.keys(newData).forEach(setting => {
 					let value = newData[setting]

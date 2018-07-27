4cb021f2301ce7ca35f2917243d4129397582f81
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-07-27 12:46:32 +0200

    lighter response from presence channel

--- routes/channels.php
+++ routes/channels.php
@@ -7,19 +7,12 @@ Broadcast::channel('active-users', function ($user) {
 	// TODO AUTH ME
 	return [
 		'id' => $user->id,
-		'email' => $user->email,
-		'avatar' => $user->profile->avatar_url,
-		'fullName' => $user->profile->full_name,
 		'profile' => [
 			'avatar' => $user->profile->avatar_url,
 			'city' => $user->profile->city,
 			'display_name' => $user->profile->display_name,
-			'first_name' => $user->profile->first_name,
 			'full_name' => $user->profile->full_name,
 			'help' => $user->profile->help,
-			'id' => $user->profile->id,
-			'last_name' => $user->profile->last_name,
-			'learning_location' => $user->profile->learning_location,
 			'user_id' => $user->profile->user_id,
 		],
 	];
@@ -28,19 +21,12 @@ Broadcast::channel('active-users', function ($user) {
 Broadcast::channel('lesson.{lessonId}', function ($user, $lessonId) {
 	return [
 		'id' => $user->id,
-		'email' => $user->email,
-		'avatar' => $user->profile->avatar_url,
-		'fullName' => $user->profile->full_name,
 		'profile' => [
 			'avatar' => $user->profile->avatar_url,
 			'city' => $user->profile->city,
 			'display_name' => $user->profile->display_name,
-			'first_name' => $user->profile->first_name,
 			'full_name' => $user->profile->full_name,
 			'help' => $user->profile->help,
-			'id' => $user->profile->id,
-			'last_name' => $user->profile->last_name,
-			'learning_location' => $user->profile->learning_location,
 			'user_id' => $user->profile->user_id,
 		],
 	];

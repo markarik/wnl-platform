d0210226e9710e7e6fc5dda96e9c274b0d358e3f
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-07-27 12:24:32 +0200

    change reconnection settings for Echo client

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -30,5 +30,8 @@ import {envValue as env} from 'js/utils/env'
 
 window.Echo = new Echo({
 	broadcaster: 'socket.io',
-	host: `${env('ECHO_HOST')}:${env('ECHO_PORT')}`
+	host: `${env('ECHO_HOST')}:${env('ECHO_PORT')}`,
+	reconnectionDelay: 60000,
+	randomizationFactor: 0,
+	reconnectionDelayMax: 60000
 });

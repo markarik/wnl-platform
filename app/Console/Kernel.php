3fbfe00fd79f536eeb0d84177e3322986ab61dc1
adamkarminski <adamkarminski@gmail.com>
2018-08-03 17:43:25 +0200

    Handle unpaid orders again

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -61,9 +61,9 @@ class Kernel extends ConsoleKernel
 			->command('quiz:slackDaysDecrement')
 			->dailyAt('02:30');
 
-		// $schedule
-		// 	->command('orders:handleUnpaid')
-		// 	->twiceDaily(8, 20);
+		$schedule
+			->command('orders:handleUnpaid')
+			->twiceDaily(8, 20);
 
 		$schedule
 			->command('notifications:cleanup-old --force')

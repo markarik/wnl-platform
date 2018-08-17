cbfd34f6663dd98320c94fa9a4f90333c6120ca2
Preziu <pferkaluk@gmail.com>
2018-08-17 14:10:16 +0200

    identity card front validation

--- resources/assets/js/components/user/IdentityNumber.vue
+++ resources/assets/js/components/user/IdentityNumber.vue
@@ -150,7 +150,7 @@
                 if (this.identity.identity_type === 'personal_identity_number') {
                     let reg = /^[0-9]{11}$/
                     if (reg.test(this.identity.personal_identity_number) == false) {
-                        this.errors.push('PESEL powinien składać się tylko z 11 cyfr')
+                        this.errors.push('PESEL powinien składać się tylko z 11 cyfr.')
                         return false
                     } else {
                         let weight = [1,3,7,9,1,3,7,9,1,3,1]
@@ -162,10 +162,52 @@
                         if (sum % 10 === 0) {
                             return true
                         } else {
-                            this.errors.push('PESEL jest niepoprawny')
+                            this.errors.push('PESEL jest niepoprawny.')
                             return false
                         }
                     }
+                } else if (this.identity.identity_type === 'identity_card') {
+                    console.log('walidacja dowodu osobistego');
+                    if (this.identity.personal_identity_number.length != 9) {
+                        this.errors.push('Numer dowodu osobistego powinien być złożony z dziewięciu znaków.')
+                        return false
+                    }
+
+                    this.identity.personal_identity_number = this.identity.personal_identity_number.toUpperCase()
+                    let letterValues = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+                    function getLetterValue(letter){
+                        return letterValues.indexOf(letter)
+                    }
+
+                    for (i = 0; i < 3; i++) {
+                        if (getLetterValue(this.identity.personal_identity_number[i]) < 10 || this.identity.personal_identity_number[i] == 'O' || this.identity.personal_identity_number == 'Q') {
+                            this.errors.push('Seria dodowodu osobistego jest niepoprawna.')
+                            return false
+                        }
+                    }
+
+                    for (i = 3; i < 9; i++) {
+                        if (getLetterValue(this.identity.personal_identity_number[i]) < 0 || getLetterValue(this.identity.personal_identity_number[i]) > 9) {
+                            this.errors.push('Numer dowodu osobistego jest niepoprawny.')
+                            return false
+                        }
+                    }
+                    let sum = 7 * getLetterValue(this.identity.personal_identity_number[0]) +
+                        3 * getLetterValue(this.identity.personal_identity_number[1]) +
+                        1 * getLetterValue(this.identity.personal_identity_number[2]) +
+                        7 * getLetterValue(this.identity.personal_identity_number[4]) +
+                        3 * getLetterValue(this.identity.personal_identity_number[5]) +
+                        1 * getLetterValue(this.identity.personal_identity_number[6]) +
+                        7 * getLetterValue(this.identity.personal_identity_number[7]) +
+                        3 * getLetterValue(this.identity.personal_identity_number[8]);
+                    sum %= 10;
+                    if (sum != getLetterValue(this.identity.personal_identity_number[3])) {
+                        this.errors.push('Numer dowodu osobistego jest nieprawidłowy')
+                        return false;
+                    }
+                } else if (this.identity.identity_type === 'passport') {
+                    console.log('walidacja paszportu');
                 }
             }
 		},
@@ -186,11 +228,6 @@
                         $wnl.logger.capture(error)
                         this.addAutoDismissableAlert(this.alertError)
                     }
-                } else if (this.identity.identity_type !== 'personal_identity_number') {
-                    this.addAutoDismissableAlert(this.alertSuccess)
-                    this.setUserIdentity(this.identity)
-                } else {
-                    this.addAutoDismissableAlert(this.wrongIdNumber)
                 }
             },
             changeIdentityType() {

0b0eb95a5dc1f83de538310226e57071a7f343f4
Kuba Karmiński <jlkarminski@gmail.com>
2018-09-04 10:57:02 +0200

    Group discount boilerplate

---
+++ resources/views/mail/voucher.blade.php
@@ -0,0 +1,17 @@
+@extends('mail.layout')
+
+@section('content')
+	<p class="text-align: center;">
+		<img src="https://media.giphy.com/media/QbkL9WuorOlgI/giphy.gif" alt="Jak dobrze, że jesteś!" style="display: block; margin: 0 auto;">
+	</p>
+
+	<h3>Cześć {{ $user->first_name or '{first_name}' }}!</h3>
+
+	<p style="font-size: 1.25em;">Twój Study Buddy dołączył właśnie do kursu!</p>
+
+	<p>Spieszymy z informacją, że Twój unikalny kod promocyjny został wykorzystany! Dzięki temu obdarowana przez Ciebie osoba i Ty, otrzymujecie 100zł zniżki na kurs! Dziękujemy Ci za podzielenie się informacją o Więcej niż LEK! :)</p>
+
+	<p>W razie pytań pozostajemy do dyspozycji!</p>
+
+	<p>Z pozdrowieniami,</p>
+@endsection

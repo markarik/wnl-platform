5287b30e0ffec516a06df525ae279b1220e74383
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-10-09 12:41:06 +0200

    fix typo

--- resources/assets/js/components/user/Order.vue
+++ resources/assets/js/components/user/Order.vue
@@ -218,7 +218,7 @@
 						<a class=""
 							title="Dodaj lub zmień kod rabatowy"
 							@click="toggleCouponInput"
-							v-if="order.status !== 'clogised'">
+							v-if="order.status !== 'closed'">
 							<span class="icon is-small margin right"><i class="fa fa-plus"></i></span>
 							<span>Dodaj lub zmień kod rabatowy</span>
 						</a>

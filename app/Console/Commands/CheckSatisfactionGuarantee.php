e994164e402e3299bb50ddca3901c2cb49eba3d9
Preziu <pferkaluk@gmail.com>
2018-10-10 16:23:43 +0200

    fully functional command

--- app/Console/Commands/CheckSatisfactionGuarantee.php
+++ app/Console/Commands/CheckSatisfactionGuarantee.php
@@ -5,7 +5,10 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\UserTime;
 use App\Models\User;
-use Carbon\Carbon;
+use App\Models\Lesson;
+use App\Models\QuizQuestion;
+use App\Models\UserCourseProgress;
+use App\Models\UserQuizResults;
 
 class CheckSatisfactionGuarantee extends Command
 {
@@ -14,7 +17,7 @@ class CheckSatisfactionGuarantee extends Command
      *
      * @var string
      */
-    protected $signature = 'check:satisfactionGuarantee {maxDate} {userId}';
+    protected $signature = 'check:satisfactionGuarantee {maxDate} {userId} {productIds*}';
 	//maxDate should be formatted like: 2018-09-22
 
     /**
@@ -44,17 +47,52 @@ class CheckSatisfactionGuarantee extends Command
 		$userId = $this->argument('userId');
 		$dateString = strval($this->argument('maxDate')).' 00:00:00';
 		$user = User::find($userId);
-		$profileId = $user->profile()->id;
-		dd($profileId);
+		$profileId = $user->profile->id;
+		$productIds = $this->argument('productIds');
 
-		$maxUserTimeId = $user->userTime()
+		$maxUserTime = $user->userTime()
 			->whereDate('created_at','<=', $dateString)
 			->orderBy('id', 'desc')
 			->first();
 
-		print 'User time in minutes '.$maxUserTimeId->time."\n";
-		print 'User time in hours '.($maxUserTimeId->time/60)."\n";
+		if ($maxUserTime) {
+			$actualTime = $maxUserTime->time;
+		} else {
+			$actualTime = 'No user time';
+		}
 
+		print PHP_EOL;
+		print $user->getFullNameAttribute()."\n";
+		print 'Profile id '.$profileId."\n";
+		print 'Id '.$userId."\n";
+		print 'User time in minutes '.$actualTime."\n";
+		print 'User time in hours '.round($actualTime/60)."\n";
 
+		$allLessons = Lesson::whereDate('created_at', '<=', $dateString)->count();
+		$allQuestions = QuizQuestion::whereDate('created_at', '<=', $dateString)->count();
+ 		$users = User::whereHas('orders', function($query) use ($productIds) {
+			$query->whereIn('product_id', $productIds)
+				->where('paid', 1);
+		})->get();
+
+		$userCourseProgress = UserCourseProgress::where('user_id', $profileId)
+			->whereDate('created_at', '<=', $dateString)
+			->whereNull('section_id')
+			->whereNull('screen_id')
+			->where('status', 'complete')
+			->count();
+
+		print 'User course progress percentage '.round(($userCourseProgress/$allLessons)*100)."\n";
+
+		$userQuizQuestionsSolved = UserQuizResults::where('user_id', $userId)
+			->whereDate('created_at', '<=', $dateString)
+			->groupBy('quiz_question_id')
+			->get(['quiz_question_id'])
+			->count();
+
+		print 'User quiz questions solved percentage '.round(($userQuizQuestionsSolved/$allQuestions)*100)."\n";
+		print PHP_EOL;
+
+ 		return;
     }
 }

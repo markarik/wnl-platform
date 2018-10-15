23447625e21107b8ddee93d16412a0677b46b244
Kuba Karmiński <jlkarminski@gmail.com>
2018-10-15 12:47:06 +0200

    User stats export | Adjust user groups criteria

--- app/Console/Commands/ExportUserStatistics.php
+++ app/Console/Commands/ExportUserStatistics.php
@@ -12,55 +12,57 @@ use Storage;
 
 class ExportUserStatistics extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'userStatistics:export {maxDate} {products*}';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'userStatistics:export {maxDate} {products*}';
 	// example date format: 2018-09-22
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Export user statistics';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Export user statistics';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
 		$productIds = $this->argument('products');
 		$maxDate = $this->argument('maxDate');
-		$dateString = strval($maxDate).' 00:00:00';
 
-		$name = 'Statystyki użytkowników dla daty: '.$maxDate.' i produktów: '.$productIds[0].' '.$productIds[1];
-
-		$headers = ['Id', 'Imię', 'Nazwisko', 'Czas spędzony na platformie', 'Procent ukończonych lekcji', 'Procent rozwiązanych pytań'];
+		$headers = ['Id', 'Imię', 'Nazwisko', 'Czas spędzony na platformie', 'Procent ukończonych lekcji',
+			'Procent rozwiązanych pytań'];
 
 		$firstGroup = collect([$headers]);
 		$secondGroup = collect([$headers]);
 		$thirdGroup = collect([$headers]);
 
-		$allLessons = Lesson::whereDate('created_at', '<=', $dateString)->count();
-		$allQuestions = QuizQuestion::whereDate('created_at', '<=', $dateString)->count();
+		$allLessons = Lesson::select(['id'])
+			->whereDate('created_at', '<=', $maxDate)
+			->where('is_required', 1)
+			->count();
+		$allQuestions = QuizQuestion::whereDate('created_at', '<=', $maxDate)->count();
 
-		$users = User::whereHas('orders', function($query) use ($productIds) {
-			$query->whereIn('product_id', $productIds)
+		$users = User::whereHas('orders', function ($query) use ($productIds) {
+			$query
+				->whereIn('product_id', $productIds)
 				->where('paid', 1);
 		})->get();
 
@@ -75,73 +77,65 @@ class ExportUserStatistics extends Command
 			$userId = $user->id;
 
 			$maxUserTime = $user->userTime()
-				->whereDate('created_at','<=', $dateString)
+				->whereDate('created_at', '<=', $maxDate)
 				->orderBy('id', 'desc')
 				->first();
 
-			if($maxUserTime) {
-				$userTime = round($maxUserTime->time/60);
-			} else {
-				$userTime = 'brak';
+			$userTime = 0;
+			if ($maxUserTime) {
+				$userTime = (int)round($maxUserTime->time / 60);
 			}
 
 			$userCourseProgress = UserCourseProgress::where('user_id', $profileId)
-				->whereDate('created_at', '<=', $dateString)
-				->whereNull('section_id')
-				->whereNull('screen_id')
-				->where('status', 'complete')
+				->whereDate('user_course_progress.created_at', '<=', $maxDate)
+				->join('lessons', 'lessons.id', '=', 'lesson_id')
+				->where('lessons.is_required', 1)
+				->whereNull('user_course_progress.section_id')
+				->whereNull('user_course_progress.screen_id')
+				->where('user_course_progress.status', 'complete')
 				->count();
 
-			$userCourseProgressPrecentage = round(($userCourseProgress/$allLessons)*100);
+			$userCourseProgressPrecentage = (int)round(($userCourseProgress / $allLessons) * 100);
 
 			$userQuizQuestionsSolved = UserQuizResults::where('user_id', $userId)
-				->whereDate('created_at', '<=', $dateString)
 				->groupBy('quiz_question_id')
 				->get(['quiz_question_id'])
 				->count();
 
-			$userQuizQuestionsSolvedPercentage = round(($userQuizQuestionsSolved/$allQuestions)*100);
+			$userQuizQuestionsSolvedPercentage = (int)round(($userQuizQuestionsSolved / $allQuestions) * 100);
 
-			if (
+			$userRecord = [
+				$userId,
+				$user->first_name,
+				$user->last_name,
+				$userTime,
+				$userCourseProgressPrecentage,
+				$userQuizQuestionsSolvedPercentage,
+			];
+
+			$firstGroupCriteria =
 				$userCourseProgressPrecentage >= 80 &&
 				$userQuizQuestionsSolvedPercentage >= 60 &&
-				$userTime >= 100
-			) {
-				$firstGroup->push([
-					$userId,
-					$user->first_name,
-					$user->last_name,
-					$userTime,
-					$userCourseProgressPrecentage,
-					$userQuizQuestionsSolvedPercentage,
-				]);
-			} else if (
-				($userCourseProgressPrecentage > 30 && $userCourseProgressPrecentage < 80) &&
-				$userQuizQuestionsSolvedPercentage < 60 &&
-				($userTime >= 100 && $userTime < 300)
-			) {
-				$secondGroup->push([
-					$userId,
-					$user->first_name,
-					$user->last_name,
-					$userTime,
-					$userCourseProgressPrecentage,
-					$userQuizQuestionsSolvedPercentage,
-				]);
+				$userTime >= 300;
+
+			$secondGroupCriteria =
+				$userCourseProgressPrecentage >= 30 &&
+				$userQuizQuestionsSolvedPercentage >= 30 &&
+				$userTime >= 100;
+
+			if ($firstGroupCriteria) {
+				$firstGroup->push($userRecord);
+			} else if ($secondGroupCriteria) {
+				$secondGroup->push($userRecord);
 			} else {
-				$thirdGroup->push([
-					$userId,
-					$user->first_name,
-					$user->last_name,
-					$userTime,
-					$userCourseProgressPrecentage,
-					$userQuizQuestionsSolvedPercentage,
-				]);
+				$thirdGroup->push($userRecord);
 			}
 
 			$bar->advance();
 		}
 		$bar->finish();
+
+		print PHP_EOL;
 
 		$groups = [
 			$firstGroup,
@@ -149,13 +143,21 @@ class ExportUserStatistics extends Command
 			$thirdGroup
 		];
 
-		foreach($groups as $key => $group) {
+		$total = 0;
+
+		foreach ($groups as $key => $group) {
+			$recordsCount = $group->count();
+			$total += $recordsCount;
+			$this->info("Group {$key} has {$recordsCount} records.");
 			$group = $group->map(function ($row) {
 				return implode("\t", $row);
 			});
 			$contents = $group->implode("\n");
-			Storage::put('exports/' . $key . '.tsv', $contents);
+			Storage::put('exports/user-stats/' . $key . '.tsv', $contents);
 		}
+
+		$this->info("Total: {$total}");
+
 		return;
-    }
+	}
 }

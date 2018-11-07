344c72ad1afc78647b9bc93b473496c542b5953b
Preziu <pferkaluk@gmail.com>
2018-11-07 15:31:40 +0100

    include quill in users editor view

--- resources/assets/js/admin/components/users/Users.vue
+++ resources/assets/js/admin/components/users/Users.vue
@@ -1,9 +1,17 @@
 <template>
 	<div>
-		<autocomplete
-			:items="autocompleteItems"
-			:itemComponent="'wnl-user-autocomplete-item'"
-		></autocomplete>
+		<wnl-input>
+			<wnl-quill
+				ref="editor"
+				name="text"
+				id="elo-elo"
+				:options="quillOptions"
+				:keyboard="keyboard"
+				:toolbar="toolbar"
+				:allowMentions="true"
+				@input="onInput"
+			></wnl-quill>
+		</wnl-input>
 	</div>
 </template>
 
@@ -11,12 +19,52 @@
 </style>
 
 <script>
-	import Autocomplete from 'js/components/global/Autocomplete'
+	import Input from 'js/admin/components/forms/Input'
+	import Quill from 'js/components/global/form/Quill'
 
 	export default {
 		name: 'Users',
 		components: {
-			'autocomplete': Autocomplete,
+			'wnl-input': Input,
+			'wnl-quill': Quill
 		},
+		data() {
+			return {
+				keyboard: {
+					bindings: {
+						tab: false,
+						handleEnter: {
+							key: 13,
+							handler: (event) => {
+								console.log('halko');
+								return false;
+							}
+						}
+					}
+				},
+			}
+		},
+		computed: {
+			quillOptions() {
+				return {
+					theme: 'bubble',
+					placeholder: 'Twoja wiadomość...',
+					formats: ['bold', 'italic', 'underline', 'link', 'mention'],
+					bounds: `#${this.formId}`,
+				}
+			},
+			toolbar() {
+				return [
+					['bold', 'italic', 'underline', 'link'],
+					[{ color: fontColors }],
+					['clean'],
+				]
+			},
+		},
+		methods: {
+			onInput() {
+				console.log('onInput');
+			}
+		}
 	}
 </script>

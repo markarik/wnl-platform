0dd373c3857afad7f8963bd47162e76dfec55305
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2018-11-19 11:12:05 +0100

    PLAT-620 | Center text button icon

--- resources/assets/js/components/global/TextButton.vue
+++ resources/assets/js/components/global/TextButton.vue
@@ -13,14 +13,18 @@
 	@import 'resources/assets/sass/variables'
 
 	.wnl-text-button
+		align-items: center
 		background: none
 		border: 0
 		color: $color-ocean-blue
 		cursor: pointer
+		display: inline-flex
 		font-weight: bold
 		outline: none
 		padding: 0
 
+	.icon
+		height: auto
 </style>
 
 <script>

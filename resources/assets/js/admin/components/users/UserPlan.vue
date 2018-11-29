f36ded96e34ae9d9135ea6cde33dab8ee088ef70
Bogna "bognix" Knychala <bogna.ka@gmail.com>
2018-11-29 13:22:50 +0100

    add user lesson

--- resources/assets/js/admin/components/users/UserPlan.vue
+++ resources/assets/js/admin/components/users/UserPlan.vue
@@ -31,13 +31,19 @@
 			Ten użytkownik nie ma żadynch lekcji
 		</p>
 		<div>
-			<div class="control">
-				<label class="label">Wybierz pytanie</label>
-				<input class="input" placeholder="Id lub treść aby wyszukać" v-model="lessonInput"/>
+			<div class="level wnl-screen-title">
+				<div class="level-left">
+					<div class="level-item big strong">
+						Dodaj Lekcje
+					</div>
+				</div>
 			</div>
 			<div class="control">
+				<label class="label">Wyszukaj Lekcję</label>
+				<input class="input" placeholder="nazwa aby wyszukać" v-model="lessonInput"/>
+			</div>
+			<div class="control margin bottom big">
 				<wnl-autocomplete
-					:isDown="true"
 					:items="autocompleteLessonsItems"
 					:onItemChosen="addLesson"
 				>
@@ -46,22 +52,61 @@
 					</template>
 				</wnl-autocomplete>
 			</div>
+			<div  v-if="selectedLessons.length">
+				<table class="table user-plan__add-lesson">
+					<thead>
+						<tr>
+							<th>Lekcja</th>
+							<th>Data Startu</th>
+							<th>Akcje</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="lesson in selectedLessons" :key="lesson.id">
+							<td>{{lesson.name}}</td>
+							<td>
+								<wnl-datepicker
+									:value="lesson.startDate"
+									@onChange="(payload) => onStartDateChange(payload, lesson)"
+								/>
+							</td>
+							<td>
+								<button
+										class="button is-danger"
+										type="button"
+										@click="unselectLesson(lesson)"
+								>
+									<span class="icon"><i class="fa fa-trash"></i></span>
+								</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<a class="button button is-primary is-outlined is-big" @click="addLessons">Zapisz</a>
+			</div>
 		</div>
 	</div>
 </template>
+
+<style lang="sass">
+	.user-plan__add-lesson .datepicker
+		text-align: left
+</style>
 
 <script>
 	import moment from 'moment'
 	import {nextTick} from 'vue'
 	import {mapActions} from 'vuex'
-	import {getApiUrl} from 'js/utils/env'
+	import momentTimezone from 'moment-timezone'
 
+	import {getApiUrl} from 'js/utils/env'
 	import WnlAutocomplete from 'js/components/global/Autocomplete';
+	import WnlDatepicker from 'js/components/global/Datepicker'
 
 	export default {
 		name: "UserPlan",
 		components: {
-			WnlAutocomplete
+			WnlAutocomplete, WnlDatepicker
 		},
 		props: {
 			user: {
@@ -75,7 +120,8 @@
 				filterPhrase: '',
 				loading: false,
 				lessons: [],
-				selectedLessons: []
+				selectedLessons: [],
+				lessonInput: ''
 			}
 		},
 		computed: {
@@ -83,12 +129,58 @@
 				if (!this.filterPhrase) return this.userLessons;
 
 				return this.userLessons.filter(({lesson}) => lesson.toLowerCase().startsWith(this.filterPhrase.toLowerCase()));
+			},
+			autocompleteLessonsItems() {
+				if (this.lessonInput === '') {
+					return [];
+				}
+
+				return this.lessons
+					.filter(lesson => !this.userLessons.find(({lesson_id}) => lesson_id === lesson.id) &&
+						(
+							lesson.name.toLowerCase().includes(this.lessonInput.toLowerCase())
+						)
+					)
+					.slice(0, 10)
 			}
 		},
 		methods: {
 			...mapActions(['addAutoDismissableAlert']),
 			addLesson(lesson) {
-				this.selectedLessons.push(lesson)
+				if (!this.selectedLessons.includes(lesson)) {
+					this.selectedLessons.push({
+						...lesson,
+						startDate: new Date()
+					})
+				}
+
+				this.lessonInput = '';
+			},
+			onStartDateChange(payload, lesson) {
+				this.selectedLessons = this.selectedLessons.map(selectedLesson => {
+					if (lesson.id === selectedLesson.id) {
+						return selectedLesson
+					}
+					return {
+						...selectedLesson,
+						startDate: payload[0]
+					}
+				})
+			},
+			unselectLesson(lesson) {
+				const index = this.selectedLessons.findIndex(selectedLesson => selectedLesson.id === lesson.id)
+				this.selectedLessons.splice(index, 1)
+			},
+			async addLessons() {
+				await axios.put(getApiUrl(`user_lesson/${this.user.id}/batch`), {
+					manual_start_dates: this.selectedLessons.map(lesson => {
+						return {
+							lessonId: lesson.id,
+							startDate: lesson.startDate
+						}
+					}),
+					timezone: momentTimezone.tz.guess(),
+				})
 			}
 		},
 		async mounted() {

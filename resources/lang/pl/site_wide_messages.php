8afea763295bc7545a240b547116044420fc7c9f
rogatty <igor.rogatty@bethink.pl>
2018-12-05 16:23:42 +0100

    PLAT-759 | display due date in the unpaid instalment reminder

--- resources/lang/pl/site_wide_messages.php
+++ resources/lang/pl/site_wide_messages.php
@@ -2,7 +2,7 @@
 
 return [
 
-	'unpaid-instalment-reminder' => "<p>Zbliża się termin płatności raty za zamówienie #:orderId. Nie czekaj do ostatniej chwili i opłać zamówienie w zakładce <a href='/app/myself/orders'>KONTO > Twoje zamówienia</a>.</p>",
+	'unpaid-instalment-reminder' => "<p><strong>:dueDate</strong> upływa termin płatności raty za zamówienie #:orderId. Opłać ratę już teraz w zakładce <a href='/app/myself/orders'>KONTO > Twoje zamówienia</a>.</p>",
 	'unpaid-order-reminder' => "<p>Zbliża się termin płatności za zamówienie #:orderId.</p><p>W przypadku braku wpłaty zamówienie zostanie automatycznie anulowane po 7 dniach.</p><p>Możesz samodzielnie anulować zamówienie w zakładce <a href='/app/myself/orders'>KONTO > Twoje zamówienia</a>.</p>"
 
 ];

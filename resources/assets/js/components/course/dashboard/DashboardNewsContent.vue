77eeb49a118cb778ffa02022562697cac882aad6
rogatty <igor.rogatty@bethink.pl>
2018-12-06 12:23:21 +0100

    PLAT-794 | inject arguments into dashboard news message

--- resources/assets/js/components/course/dashboard/DashboardNewsContent.vue
+++ resources/assets/js/components/course/dashboard/DashboardNewsContent.vue
@@ -2,11 +2,14 @@
 	<div class="notification content">
 		<button class="delete" @click="$emit('onClose')"></button>
 		<p class="has-text-centered"><strong v-html="slug"></strong></p>
-		<span v-html="message"></span>
+		<span v-html="parsedMessage"></span>
 	</div>
 </template>
 
 <script>
+	import {mapGetters} from 'vuex';
+	import injectArguments from 'js/utils/injectArguments';
+
 	export default {
 		name: 'DashboardNewsContent',
 		props: {
@@ -17,6 +20,17 @@
 			message: {
 				type: String,
 				default: ''
+			},
+		},
+		computed: {
+			...mapGetters(['currentUserName']),
+			arguments: function () {
+				return {
+					currentUserName: this.currentUserName
+				};
+			},
+			parsedMessage: function () {
+				return injectArguments(this.message, this.arguments);
 			},
 		},
 	}
---
+++ resources/assets/js/utils/injectArguments.js
@@ -0,0 +1,23 @@
+const PLACEHOLDER_RGX = /{{(.*)}}/g;
+
+export default function injectArguments(content, args) {
+    const matches = content.match(PLACEHOLDER_RGX);
+    let missing = [];
+
+    if (!matches) return content;
+
+    matches.forEach(match => {
+        const argName = match.replace(/{{|}}/g, '');
+        const value = args[argName] || '';
+        if (!value) missing.push(argName);
+        content = content.replace(match, value);
+    });
+
+    if (missing.length > 0) {
+        $wnl.logger.warning(
+            'Missing template arguments: ' + missing.join(',')
+        );
+    }
+
+    return content;
+}

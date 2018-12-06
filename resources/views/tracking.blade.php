b15758f3db5c52e824dc875f3a1adb9488bed37d
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2018-12-06 09:16:14 +0100

    PLAT-795 | Start using GA profile ID from ENV

--- resources/views/tracking.blade.php
+++ resources/views/tracking.blade.php
@@ -1,21 +1,23 @@
-@if (app()->environment('production'))
+@if (env('GA_PROFILE_ID'))
 	<script>
 		(function (i, s, o, g, r, a, m) {
 			i['GoogleAnalyticsObject'] = r;
 			i[r] = i[r] || function () {
-						(i[r].q = i[r].q || []).push(arguments)
-					}, i[r].l = 1 * new Date();
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
 			a = s.createElement(o),
-					m = s.getElementsByTagName(o)[0];
+				m = s.getElementsByTagName(o)[0];
 			a.async = 1;
 			a.src   = g;
 			m.parentNode.insertBefore(a, m)
 		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-		ga('create', 'UA-91640583-2', 'auto');
+		ga('create', '{{env('GA_PROFILE_ID')}}', 'auto');
 		ga('send', 'pageview');
 	</script>
+@endif
 
+@if (app()->environment('production'))
 	<!-- Facebook Pixel Code -->
 	<script>
 		!function(f,b,e,v,n,t,s)
@@ -32,42 +34,4 @@
 	               src={{ "https://www.facebook.com/tr?id=" . env('FACEBOOK_PIXEL_APP_ID') . "&ev=PageView&noscript=1" }}
 		/></noscript>
 	<!-- End Facebook Pixel Code -->
-@endif
-@if (app()->environment('demo'))
-	<script>
-		(function (i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function () {
-						(i[r].q = i[r].q || []).push(arguments)
-					}, i[r].l = 1 * new Date();
-			a = s.createElement(o),
-					m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src   = g;
-			m.parentNode.insertBefore(a, m)
-		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-		ga('create', 'UA-91640583-3', 'auto');
-		ga('send', 'pageview');
-
-	</script>
-@endif
-@if (app()->environment('staging'))
-	<script>
-		(function (i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function () {
-						(i[r].q = i[r].q || []).push(arguments)
-					}, i[r].l = 1 * new Date();
-			a = s.createElement(o),
-					m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src   = g;
-			m.parentNode.insertBefore(a, m)
-		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-		ga('create', 'UA-91640583-4', 'auto');
-		ga('send', 'pageview');
-
-	</script>
 @endif

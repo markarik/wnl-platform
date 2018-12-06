4f5dd3ce7ff4666db9764e57d1b7a775037d3501
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2018-12-06 10:24:45 +0100

    PLAT-795 | Don't track first pageview in Vue

--- resources/assets/js/utils/tracking.js
+++ resources/assets/js/utils/tracking.js
@@ -1,12 +1,15 @@
+// First pageview is tracked from blade, don't duplicate this event
+let isFirstPageview = true;
+
 export function gaEvent(category, action, label) {
-	console.log('ga', category, action, label)
 	if (typeof ga === 'function') {
 		ga('send', 'event', category, action, label);
 	}
 }
 
 export function gaPageView() {
-	if (typeof ga === 'function') {
+	if (typeof ga === 'function' && !isFirstPageview) {
 		ga('send', 'pageview');
 	}
+	isFirstPageview = false;
 }

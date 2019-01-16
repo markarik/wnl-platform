b83cabb39b7c5a4d75a9e013b6e4316186a51b65
rogatty <igor.rogatty@bethink.pl>
2019-01-16 15:44:42 +0100

    PLAT-885 | reuse code for add and edit forms

--- resources/assets/js/admin/components/taxonomies/TaxonomyTermEditorAdd.vue
+++ resources/assets/js/admin/components/taxonomies/TaxonomyTermEditorAdd.vue
@@ -1,126 +1,68 @@
 <template>
-	<div>
-		<h5 class="title is-5 is-uppercase is-marginless"><strong>Nadrzędne pojęcie</strong></h5>
-		<span class="info small">Pozostaw puste, aby dodać pojęcie na 1. poziomie taksonomii.</span>
-		<wnl-term-autocomplete
-			@change="onSelectParent"
-			:selected="parent"
-		/>
-
-		<h5 class="title is-5 is-uppercase is-marginless"><strong>Tag źródłowy</strong></h5>
-		<span class="info">Wybierz tag, na podstawie którego chcesz utworzyć pojęcie, lub utwórz nowy.</span>
-		<wnl-tag-autocomplete
-			@change="onSelectTag"
-			:selected="tag"
-		/>
-
-		<h5 class="title is-5 is-uppercase is-marginless"><strong>Notatka</strong></h5>
-		<span class="info">(Opcjonalnie) Dodaj notatkę niewidoczną dla użytkowników.</span>
-		<textarea class="textarea margin bottom" v-model="description" placeholder="Wpisz tekst" />
-		<div class="has-text-centered">
-			<button class="button" @click="onSave" :disabled="submitDisabled">Dodaj pojęcie</button>
-		</div>
-	</div>
+	<wnl-taxonomy-term-editor-form
+		submit-label="Dodaj pojęcie"
+		:on-save="onSave"
+		:tags="tags"
+		:taxonomy-id="taxonomyId"
+		:term="term"
+		:terms="terms"
+	/>
 </template>
-
-<style lang="sass" rel="stylesheet/sass" scoped>
-	@import 'resources/assets/sass/variables'
-
-	.info
-		color: #aaa
-
-	.autocomplete-selected
-		display: flex
-		justify-content: space-between
-
-	.autocomplete-parent-term
-		color: $color-inactive-gray
-
-</style>
 
 <script>
 import {mapActions, mapState, mapGetters} from 'vuex';
-import {uniqBy} from 'lodash';
 
-import WnlAutocomplete from 'js/components/global/Autocomplete';
-import WnlTermAutocomplete from 'js/admin/components/taxonomies/TaxonomyTermEditorTermAutocomplete';
-import WnlTagAutocomplete from 'js/admin/components/taxonomies/TaxonomyTermEditorTagAutocomplete';
+import WnlTaxonomyTermEditorForm from 'js/admin/components/taxonomies/TaxonomyTermEditorForm';
 
 export default {
 	props: {
 		taxonomyId: {
 			type: String|Number,
 			required: true,
-		}
+		},
 	},
 	data() {
 		return {
-			description: '',
-			tag: null,
-			parent: null,
+			term: {
+				description: '',
+				id: null,
+				tag: null,
+				parent: null,
+			}
 		};
 	},
 	computed: {
 		...mapGetters('taxonomyTerms', ['termById']),
-		...mapState('taxonomyTerms', ['selectedTerms']),
-		submitDisabled() {
-			return this.tag === null;
-		}
+		...mapState('taxonomyTerms', ['selectedTerms', 'terms']),
+		...mapState('tags', ['tags']),
 	},
 	components: {
-		WnlAutocomplete,
-		WnlTermAutocomplete,
-		WnlTagAutocomplete
+		WnlTaxonomyTermEditorForm
 	},
 	methods: {
-		...mapActions(['addAutoDismissableAlert']),
 		...mapActions('taxonomyTerms', {
 			'createTerm': 'create',
 		}),
-		async onSave() {
-			try {
-				await this.createTerm({
-					parent_id: this.parent ? this.parent.id : null,
-					tag_id: this.tag.id,
-					taxonomy_id: this.taxonomyId,
-					description: this.description,
-				});
-
-				this.tag = null;
-				this.parent = null;
-
-				this.addAutoDismissableAlert({
-					text: 'Zapisano!',
-					type: 'success'
-				});
-			} catch (error) {
-				$wnl.logger.capture(error);
-
-				this.addAutoDismissableAlert({
-					text: 'Ups, coś poszło nie tak, spróbuj ponownie.',
-					type: 'error',
-				});
-			}
-		},
-		onSelectParent(term) {
-			this.parent = term;
-		},
-		onSelectTag(tag) {
-			this.tag = tag;
+		...mapActions('tags', {
+			fetchAllTags: 'fetchAll'
+		}),
+		async onSave(term) {
+			await this.createTerm(term);
 		},
 		initializeParent(selectedTerms) {
 			if (selectedTerms.length) {
-				this.parent = this.termById(selectedTerms[0]);
+				this.term = Object.assign({}, this.term, {parent: this.termById(selectedTerms[0])});
 			}
-		}
+		},
 	},
 	mounted() {
+		this.fetchAllTags();
 		this.initializeParent(this.selectedTerms);
 	},
 	watch: {
 		selectedTerms(selectedTerms) {
 			this.initializeParent(selectedTerms);
-		}
-	}
+		},
+	},
 };
 </script>
--- resources/assets/js/admin/components/taxonomies/TaxonomyTermEditorEdit.vue
+++ resources/assets/js/admin/components/taxonomies/TaxonomyTermEditorEdit.vue
@@ -1,78 +1,18 @@
 <template>
-	<div>
-		<h5 class="title is-5 is-uppercase is-marginless"><strong>Nadrzędne pojęcie</strong></h5>
-		<span class="info small">Pozostaw puste, aby dodać pojęcie na 1. poziomie taksonomii.</span>
-		<div class="margin bottom">
-			<div v-if="parent" class="autocomplete-selected">
-					<span>
-						{{parent.ancestors.map(ancestor => ancestor.tag.name).join(' > ')}} >
-						{{parent.tag.name}}
-					</span>
-				<span class="icon is-small clickable" @click="parent=null"><i class="fa fa-close" aria-hidden="true"></i></span>
-			</div>
-			<div class="control" v-else>
-				<input class="input" v-model="parentSearch" placeholder="Wpisz nazwę nadrzędnego pojęcia" />
-				<wnl-autocomplete
-					:items="autocompleteTerms"
-					:onItemChosen="onSelectParent"
-				>
-					<template slot-scope="slotProps">
-						<div>
-							<div class="autocomplete-parent-term">{{slotProps.item.ancestors.map(ancestor => ancestor.tag.name).join(' > ')}}</div>
-							<div>{{slotProps.item.tag.name}}</div>
-						</div>
-					</template>
-				</wnl-autocomplete>
-			</div>
-		</div>
-
-		<h5 class="title is-5 is-uppercase is-marginless"><strong>Tag źródłowy</strong></h5>
-		<span class="info">Wybierz tag, na podstawie którego chcesz utworzyć pojęcie, lub utwórz nowy.</span>
-		<div class="margin bottom">
-			<div v-if="tag" class="autocomplete-selected">
-				{{tag.name}}
-				<span class="icon is-small clickable" @click="tag=null"><i class="fa fa-close" aria-hidden="true"></i></span>
-			</div>
-			<div class="control" v-else>
-				<input class="input" v-model="tagSearch" placeholder="Wpisz nazwę tagu, który chcesz dołączyć lub utworzyć" />
-				<wnl-autocomplete
-					:items="autocompleteTags"
-					:onItemChosen="onSelectTag"
-				>
-					<template slot-scope="slotProps">
-						<div>
-							{{slotProps.item.name}}
-						</div>
-					</template>
-				</wnl-autocomplete>
-			</div>
-		</div>
-		<h5 class="title is-5 is-uppercase is-marginless"><strong>Notatka</strong></h5>
-		<span class="info">(Opcjonalnie) Dodaj notatkę niewidoczną dla użytkowników.</span>
-		<textarea class="textarea margin bottom" v-model="description" placeholder="Wpisz tekst" />
-		<button class="button" @click="onSave">Zapisz</button>
-	</div>
+	<wnl-taxonomy-term-editor-form
+		submit-label="Zapisz"
+		:on-save="onSave"
+		:tags="tags"
+		:taxonomy-id="taxonomyId"
+		:term="term"
+		:terms="terms"
+	/>
 </template>
-
-<style lang="sass" rel="stylesheet/sass" scoped>
-	@import 'resources/assets/sass/variables'
-
-	.info
-		color: #aaa
-
-	.autocomplete-selected
-		display: flex
-		justify-content: space-between
-
-	.autocomplete-parent-term
-		color: $color-inactive-gray
-
-</style>
 
 <script>
 import {mapActions, mapGetters, mapState} from 'vuex';
 
-import WnlAutocomplete from 'js/components/global/Autocomplete';
+import WnlTaxonomyTermEditorForm from 'js/admin/components/taxonomies/TaxonomyTermEditorForm';
 
 export default {
 	props: {
@@ -83,81 +23,37 @@ export default {
 	},
 	data() {
 		return {
-			description: '',
-			id: null,
-			tag: null,
-			tagSearch: '',
-			parent: null,
-			parentSearch: '',
+			term: {}
 		};
 	},
 	computed: {
 		...mapGetters('taxonomyTerms', ['termById']),
 		...mapState('taxonomyTerms', ['selectedTerms', 'terms']),
 		...mapState('tags', ['tags']),
-		autocompleteTerms() {
-			if (!this.parentSearch) {
-				return [];
-			}
-			return this.terms.filter(term => term.tag.name.toLocaleLowerCase().includes(this.parentSearch.toLocaleLowerCase())).slice(0, 10);
-		},
-		autocompleteTags() {
-			if (!this.tagSearch) {
-				return [];
-			}
-			return this.tags.filter(tag => tag.name.toLocaleLowerCase().includes(this.tagSearch.toLocaleLowerCase())).slice(0, 10);
-			// TODO add tag
-		}
 	},
 	components: {
-		WnlAutocomplete
+		WnlTaxonomyTermEditorForm
 	},
 	methods: {
-		...mapActions(['addAutoDismissableAlert']),
 		...mapActions('taxonomyTerms', {
 			'updateTerm': 'update',
 		}),
 		...mapActions('tags', {
 			fetchAllTags: 'fetchAll'
 		}),
-		async onSave() {
-			try {
-				await this.updateTerm({
-					id: this.id,
-					parent_id: this.parent ? this.parent.id : null,
-					tag_id: this.tag.id,
-					description: this.description,
-					taxonomy_id: this.taxonomyId
-				});
-
-				this.addAutoDismissableAlert({
-					text: 'Zapisano!',
-					type: 'success'
-				});
-			} catch (error) {
-				$wnl.logger.capture(error);
-
-				this.addAutoDismissableAlert({
-					text: 'Ups, coś poszło nie tak, spróbuj ponownie.',
-					type: 'error',
-				});
-			}
-		},
-		onSelectParent(term) {
-			this.parent = term;
-			this.parentSearch = '';
-		},
-		onSelectTag(tag) {
-			this.tag = tag;
-			this.tagSearch = '';
+		async onSave(term) {
+			return await this.updateTerm(term);
 		},
 		onSelectedTermsChange() {
 			// TODO figure out multiple terms selected
 			const term = this.termById(this.selectedTerms[0]);
-			this.description = term.description;
-			this.id = term.id;
-			this.parent = term.ancestors.slice(-1)[0];
-			this.tag = term.tag;
+
+			this.term = {
+				description: term.description,
+				id: term.id,
+				parent: term.ancestors.slice(-1)[0],
+				tag: term.tag
+			};
 		}
 	},
 	watch: {

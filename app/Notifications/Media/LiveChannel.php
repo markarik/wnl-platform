e13663dd8de3a26ff9921f29cf47dd614068d980
Bogna 'bognix' Knychala <bogna.ka@gmail.com>
2019-01-24 14:24:27 +0100

    check if actor set on event

--- app/Notifications/Media/LiveChannel.php
+++ app/Notifications/Media/LiveChannel.php
@@ -22,6 +22,13 @@ class LiveChannel
 			$notifiable, $notification, is_array($message) ? $message : $message->data
 		);
 
+		// If actor is not set on event we don't want to send live notification to users
+		// Instead, let's log such situation and fix - because it shouldn't happen
+		if (empty($notification->event->data['actors']['id'])) {
+			\Log::error('Event is missing an actor', $notification->event);
+			return;
+		}
+
 		// We don't want to use broadcast()->toOthers because X-Socket-ID header is not always set
 		// Instead, we can relay on actors->id field which is added to all events
 		// and is already used in DatabaseChannel.

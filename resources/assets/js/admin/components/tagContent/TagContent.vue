87eb2c7c7a50669aa7758bdb9ddbe9daa88c641f
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-01-24 10:46:06 +0100

    PLAT-936 | Fix linter

--- resources/assets/js/admin/components/tagContent/TagContent.vue
+++ resources/assets/js/admin/components/tagContent/TagContent.vue
@@ -25,79 +25,79 @@
 </style>
 
 <script>
-	import {getApiUrl} from 'js/utils/env';
+import {getApiUrl} from 'js/utils/env';
 
-	export default {
-		data() {
-			const taggableTypes = {
-				lessons: {
-					resourceName: 'lessons/.filter',
-					name: 'Lekcje',
-				},
-				pages: {
-					resourceName: 'pages/.filter',
-					name: 'Strony',
-				},
-				annotations: {
-					resourceName: 'annotations/.filter',
-					name: 'Przypisy',
-				},
-				screens: {
-					resourceName: 'screens/.filter',
-					name: 'Ekrany',
-				},
-				quizQuestions: {
-					resourceName: 'quiz_questions/.filter',
-					name: 'Pytania z bazy pytań',
-				},
-				slides: {
-					resourceName: 'slides/.filter',
-					name: 'Slajdy',
-				},
-			};
+export default {
+	data() {
+		const taggableTypes = {
+			lessons: {
+				resourceName: 'lessons/.filter',
+				name: 'Lekcje',
+			},
+			pages: {
+				resourceName: 'pages/.filter',
+				name: 'Strony',
+			},
+			annotations: {
+				resourceName: 'annotations/.filter',
+				name: 'Przypisy',
+			},
+			screens: {
+				resourceName: 'screens/.filter',
+				name: 'Ekrany',
+			},
+			quizQuestions: {
+				resourceName: 'quiz_questions/.filter',
+				name: 'Pytania z bazy pytań',
+			},
+			slides: {
+				resourceName: 'slides/.filter',
+				name: 'Slajdy',
+			},
+		};
 
-			const filters = Object.keys(taggableTypes).reduce(
-				(collector, taggableType) => {
-					collector[taggableType] = '';
-					return collector;
-				},
-				{}
-			);
+		const filters = Object.keys(taggableTypes).reduce(
+			(collector, taggableType) => {
+				collector[taggableType] = '';
+				return collector;
+			},
+			{}
+		);
 
-			const filtered = Object.keys(taggableTypes).reduce(
-				(collector, taggableType) => {
-					collector[taggableType] = [];
-					return collector;
-				},
-				{}
-			);
+		const filtered = Object.keys(taggableTypes).reduce(
+			(collector, taggableType) => {
+				collector[taggableType] = [];
+				return collector;
+			},
+			{}
+		);
 
-			return {
-				taggableTypes,
-				filters,
-				filtered,
-			}
-		},
-		methods: {
-			async onSearch() {
-				Object.entries(this.taggableTypes).forEach(
-					async ([taggableType, taggableMeta]) => {
-						if (this.filters[taggableType] === '') {
-							return;
-						}
-
-						const {data: {data}} = await axios.post(getApiUrl(taggableMeta.resourceName), {
-							filters: [
-								{
-									by_ids: {ids: this.filters[taggableType].split(',')},
-								},
-							],
-						});
-
-						this.filtered[taggableType] = data;
+		return {
+			taggableTypes,
+			filters,
+			filtered,
+		};
+	},
+	methods: {
+		async onSearch() {
+			Object.entries(this.taggableTypes).forEach(
+				async ([taggableType, taggableMeta]) => {
+					if (this.filters[taggableType] === '') {
+						return;
 					}
-				);
-			}
+
+					const {data: {data}} = await axios.post(getApiUrl(taggableMeta.resourceName), {
+						filters: [
+							{
+								by_ids: {ids: this.filters[taggableType].split(',')},
+							},
+						],
+					});
+
+					this.filtered[taggableType] = data;
+				}
+			);
 		}
-	};
+	}
+};
 </script>

108f2ab3e2854ba51b5ca11e38c65f4caea41afd
rogatty <igor.rogatty@bethink.pl>
2019-02-01 16:36:55 +0100

    Enable vue/strongly-recommended eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	extends: [
 		// TODO use recommended rules
 		// 'plugin:vue/recommended'
-		'plugin:vue/essential'
+		'plugin:vue/strongly-recommended'
 	],
 	env: {
 		browser: true,
@@ -31,8 +31,63 @@ module.exports = {
 			'error',
 			'always'
 		],
-		// TODO enable this rule
+		// TODO enable essential rules below
 		'vue/no-use-v-if-with-v-for': [
+			'off'
+		],
+		// TODO enable strongly-recommended rules below
+		'vue/html-indent': [
+			'off'
+		],
+		'vue/require-prop-types': [
+			'off'
+		],
+		'vue/component-name-in-template-casing': [
+			'off'
+		],
+		'vue/singleline-html-element-content-newline': [
+			'off'
+		],
+		'vue/mustache-interpolation-spacing': [
+			'off'
+		],
+		'vue/attribute-hyphenation': [
+			'off'
+		],
+		'vue/max-attributes-per-line': [
+			'off'
+		],
+		'vue/html-self-closing': [
+			'off'
+		],
+		'vue/html-closing-bracket-newline': [
+			'off'
+		],
+		'vue/html-closing-bracket-spacing': [
+			'off'
+		],
+		'vue/multiline-html-element-content-newline': [
+			'off'
+		],
+		'vue/require-default-prop': [
+			'off'
+		],
+		'vue/no-template-shadow': [
+			'off'
+		],
+		'vue/name-property-casing': [
+			'off'
+		],
+		'vue/html-quotes': [
+			'off'
+		],
+		'vue/v-bind-style': [
+			'off'
+		],
+		'vue/no-multi-spaces': [
+			'off'
+		],
+		'vue/html-end-tags': [
 			'off'
 		],
 	},

3f308a1174693ca7b4ca12e2df2402c454de65dd
rogatty <igor.rogatty@bethink.pl>
2019-02-04 17:40:12 +0100

    PLAT-911 | fix infinite update loop by using an object copy

--- resources/assets/js/utils/contentClassifier.js
+++ resources/assets/js/utils/contentClassifier.js
@@ -4,14 +4,14 @@ export function parseTaxonomyTermsFromIncludes(taxonomyTermIds, included) {
 	}
 
 	return taxonomyTermIds.map(taxonomyTermId => {
-		const taxonomyTerm = included.taxonomy_terms[taxonomyTermId];
+		const taxonomyTerm = {...included.taxonomy_terms[taxonomyTermId]};
 		taxonomyTerm.tag = included.tags[taxonomyTerm.tags[0]];
 		taxonomyTerm.taxonomy = included.taxonomies[taxonomyTerm.taxonomies[0]];
 		taxonomyTerm.ancestors = [];
 
 		let currentTerm = taxonomyTerm;
 		while (currentTerm.parent_id) {
-			const parentTerm = included.ancestors[currentTerm.parent_id];
+			const parentTerm = {...included.ancestors[currentTerm.parent_id]};
 			parentTerm.tag = included.tags[parentTerm.tags[0]];
 			taxonomyTerm.ancestors.unshift(parentTerm);
 

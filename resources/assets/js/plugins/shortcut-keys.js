25cc8946b540e813dfd3da1e2e796263f0d8e7ab
rogatty <igor.rogatty@bethink.pl>
2019-02-06 17:24:29 +0100

    PLAT-958 | disable shortcuts when an editable element is focused

--- resources/assets/js/plugins/shortcut-keys.js
+++ resources/assets/js/plugins/shortcut-keys.js
@@ -1,7 +1,18 @@
+const isEditable = (element) => {
+	return element.tagName === 'INPUT' ||
+		element.tagName === 'SELECT' ||
+		element.tagName === 'TEXTAREA' ||
+		element.isContentEditable;
+};
+
 export default (Vue, {store}) => {
 	const module = 'activateWithShortcutKey';
 
 	document.addEventListener('keydown', (event) => {
+		if (isEditable(event.target)) {
+			return;
+		}
+
 		switch (event.key) {
 		case 't':
 			store.dispatch(`${module}/setFirstInstanceAsActive`);

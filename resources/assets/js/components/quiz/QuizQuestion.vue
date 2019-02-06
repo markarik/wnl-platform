c2fb6ada1ff95ab15e5ab6e3f7730a25ba1973df
Bogna 'bognix' Knychala <bogna.ka@gmail.com>
2019-02-06 13:27:19 +0100

    fix related slides on quiz question

--- resources/assets/js/components/quiz/QuizQuestion.vue
+++ resources/assets/js/components/quiz/QuizQuestion.vue
@@ -266,7 +266,7 @@
 
 </style>
 <script>
-import { isNumber, trim } from 'lodash';
+import { isNumber, trim, get } from 'lodash';
 import { mapGetters, mapActions } from 'vuex';
 import { getApiUrl } from 'js/utils/env';
 
@@ -387,7 +387,7 @@ export default {
 				linkText += this.getLesson(slide.context.lesson.id).name;
 
 				if (_.get(slide, 'context.section.id')) {
-					linkText += ` / ${this.getSection(slide.context.section.id).name}`;
+					linkText += ` / ${get(slide, 'context.section.name')}`;
 				}
 			}
 			return linkText || this.$t('quiz.annotations.slides.defaultLink');

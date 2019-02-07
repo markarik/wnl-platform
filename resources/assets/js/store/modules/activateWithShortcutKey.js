cc18e27742893b71d18b6c65699740cc4c0a01ca
rogatty <igor.rogatty@bethink.pl>
2019-02-07 14:19:59 +0100

    PLAT-958 | stop using array index as it's broken during unregister

--- resources/assets/js/store/modules/activateWithShortcutKey.js
+++ resources/assets/js/store/modules/activateWithShortcutKey.js
@@ -3,13 +3,13 @@ import * as types from 'js/store/mutations-types';
 
 // Initial state
 const state = {
-	activeInstanceIndex: -1,
+	activeInstanceUid: null,
 	instances: [],
 };
 
 // Getters
 const getters = {
-	activeInstance: state => state.instances[state.activeInstanceIndex] || null,
+	activeInstance: state => state.activeInstanceUid,
 };
 
 // Mutations
@@ -20,14 +20,14 @@ export const mutations = {
 	[types.ACTIVATE_WITH_SHORTCUT_KEY_UNREGISTER_INSTANCE] (state, uid) {
 		const index = state.instances.indexOf(uid);
 
-		state.instances.splice(index, 1);
-
-		if (state.activeInstanceIndex === index) {
-			set(state, 'activeInstanceIndex', -1);
+		if (state.activeInstanceUid === uid) {
+			set(state, 'activeInstanceUid', null);
 		}
+
+		state.instances.splice(index, 1);
 	},
-	[types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE] (state, index) {
-		set(state, 'activeInstanceIndex', index);
+	[types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE] (state, uid) {
+		set(state, 'activeInstanceUid', uid);
 	},
 };
 
@@ -40,33 +40,35 @@ export const actions = {
 		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_UNREGISTER_INSTANCE, uid);
 	},
 	setActiveInstance({commit}, uid) {
-		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, state.instances.indexOf(uid));
+		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, uid);
 	},
 	setFirstInstanceAsActive({commit}) {
-		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, 0);
+		if (state.instances.length > 0) {
+			commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, state.instances[0]);
+		}
 	},
 	setNextInstanceAsActive({commit, state}) {
 		const length = state.instances.length;
-		let index = state.activeInstanceIndex + 1;
+		let index = state.instances.indexOf(state.activeInstanceUid) + 1;
 
 		if (index >= length) {
 			index = 0;
 		}
 
-		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, index);
+		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, state.instances[index]);
 	},
 	setPreviousInstanceAsActive({commit, state}) {
 		const length = state.instances.length;
-		let index = state.activeInstanceIndex - 1;
+		let index = state.instances.indexOf(state.activeInstanceUid) - 1;
 
 		if (index < 0) {
 			index = length - 1;
 		}
 
-		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, index);
+		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, state.instances[index]);
 	},
 	resetActiveInstance({commit}) {
-		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, -1);
+		commit(types.ACTIVATE_WITH_SHORTCUT_KEY_SET_ACTIVE_INSTANCE, null);
 	},
 };
 

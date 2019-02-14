cad5898692ba62a6281449b7caf1fb6080f311e0
Bogna 'bognix' Knychala <bogna.ka@gmail.com>
2019-02-14 14:29:59 +0100

    simplify the command because edition is almost over

--- app/Console/Commands/MigrateRedisKeys.php
+++ app/Console/Commands/MigrateRedisKeys.php
@@ -2,11 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Controllers\Api\Concerns\ProvidesApiFiltering;
-use App\Models\Discussion;
-use App\Models\Page;
-use App\Models\QnaQuestion;
-use App\Models\Screen;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
 
@@ -24,7 +19,7 @@ class MigrateRedisKeys extends Command
 	 *
 	 * @var string
 	 */
-	protected $description = "PLAT-984 - Migrate redis active filters.";
+	protected $description = "PLAT-984 - Clean up redis active filters.";
 
 	/**
 	 * Execute the console command.
@@ -36,32 +31,7 @@ class MigrateRedisKeys extends Command
 		$keys = Redis::keys("active-filters-user-*-resource-quiz_questions");
 
 		foreach ($keys as $key) {
-			$value = Redis::get($key);
-
-			list($filters, $active) = json_decode($value, true);
-
-			$cleanedUpFilters = [];
-
-			foreach ($filters as $filter) {
-				$result = array_filter($filter, function($f) {
-					$list = array_filter($f['list'], function($v) {
-						return !empty($v);
-					});
-
-					return !empty($list);
-				});
-
-				if (!empty($result)) $cleanedUpFilters[] = $result;
-			}
-
-			$cleanedUpActive = [];
-			foreach ($active as $ac) {
-				$match = preg_match("/\[\d+\]/i", $ac);
-				if (empty($match)) $cleanedUpActive[] = $ac;
-			}
-
-			$data = json_encode([$cleanedUpFilters, $cleanedUpActive]);
-			Redis::set($key, $data);
+			Redis::del($key);
 		}
 	}
 }

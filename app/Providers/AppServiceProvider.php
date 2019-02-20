992822face90be62d84a1fdb949d671012ab6e1a
rogatty <igor.rogatty@bethink.pl>
2019-02-20 17:01:46 +0100

    PLAT-939 | escape dash in regex patterns
    Fix for:
    ```
    Regex pattern is invalid: Compilation failed: invalid range in character class at offset 9 in pattern: /^[\pL\s\d-,.:;()""]+$/u
    ```

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -111,12 +111,12 @@ class AppServiceProvider extends ServiceProvider
 	{
 		Validator::extend('alpha_spaces', function ($attribute, $value) {
 			// Useful for names and surnames - accept letters, spaces and hyphens
-			return preg_match('/^[\pL\s-]+$/u', $value);
+			return preg_match('/^[\pL\s\-]+$/u', $value);
 		});
 
 		Validator::extend('alpha_comas', function ($attribute, $value) {
 			// Useful for textareas - accepts letters, comas, dots, spaces and hyphens
-			return preg_match('/^[\pL\s\d-,.:;()""]+$/u', $value);
+			return preg_match('/^[\pL\s\d\-,.:;()""]+$/u', $value);
 		});
 
 		Validator::extend('morph_exists', function ($attribute, $value, $parameters, $validator) {

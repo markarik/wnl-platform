86a87bd81632e090904f2ca00ae45d2bdc8eaf3a
rogatty <igor.rogatty@bethink.pl>
2019-02-26 12:36:35 +0100

    PLAT-1046 | remove unfinished code

--- tests/Browser/Tests/Course/Lesson/NavigateUsingSideNavTest.php
+++ tests/Browser/Tests/Course/Lesson/NavigateUsingSideNavTest.php
@@ -2,10 +2,7 @@
 
 namespace Tests\Browser;
 
-use App\Models\Order;
 use App\Models\User;
-use App\Models\UserSubscription;
-use Carbon\Carbon;
 use Tests\Browser\Pages\Course\Lesson;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
@@ -19,18 +16,6 @@ class NavigateUsingSideNavTest extends DuskTestCase
 	{
 		parent::setUp();
 		$this->user = factory(User::class)->create();
-		// TODO figure out how to make it cleaner
-		UserSubscription::create([
-			'user_id' => $this->user->id,
-			'access_start' => Carbon::now(),
-			'access_end' => Carbon::now()->addDays(20),
-		]);
-		Order::create([
-			'user_id' => $this->user->id,
-			'product_id' => 12,
-			'paid' => true,
-			'method' => 'online'
-		]);
 	}
 
 	public function testNavigateUsingSideNav()

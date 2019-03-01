2e2342b270b81cbd22d5fcbde60c34acb03abd7c
rogatty <igor.rogatty@bethink.pl>
2019-03-01 14:19:46 +0100

    PLAT-1056 | use ngrok to expose p24 payment status url

---
+++ ngrok-disable.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker stop ngrok
+rm ./.env.ngrok
---
+++ ngrok-enable.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+docker run --rm -d --name=ngrok --network=container:nginx wernight/ngrok ngrok http --region=eu nginx:80
+
+# It takes some time for ngrok to set up public URL
+sleep 2
+
+NGROK_URL=$(docker run --rm --name=jq --network=container:nginx endeveit/docker-jq sh -c 'curl -s http://nginx:4040/api/tunnels/command_line | jq -r .public_url')
+
+if [[ "$NGROK_URL" == "null" ]]
+then
+    echo "ngrok failed to return public url"
+    exit 1
+fi
+
+echo "P24_STATUS_URL=$NGROK_URL/payment/status" > .env.ngrok
+
+docker-compose up -d

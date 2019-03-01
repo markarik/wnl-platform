b0dc63c4ec233b279d9067f4574cc99fc64d3301
rogatty <igor.rogatty@bethink.pl>
2019-03-01 17:30:25 +0100

    PLAT-1056 | display vnc url

--- tests/payment-tests.sh
+++ tests/payment-tests.sh
@@ -9,6 +9,11 @@ SESSION_DOMAIN=nginx
 " > .env.selenium
 
 ./ngrok-enable.sh
+
+printf "=======================================================\n"
+printf "To see what Selenium is doing open vnc://127.0.0.1:5900\n"
+printf "=======================================================\n"
+
 docker exec -it php /bin/sh -c 'php artisan dusk tests/Browser/Tests/Payment/PaymentTest.php'
 ./ngrok-disable.sh
 

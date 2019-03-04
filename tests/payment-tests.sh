ec5f5496017f6c0c17cb142985982af1db31799d
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-03-04 22:50:42 +0100

    PLAT-1056 | Disable debug_bar

--- tests/payment-tests.sh
+++ tests/payment-tests.sh
@@ -10,11 +10,12 @@ if [[ $? -eq 1 ]]; then
     exit 1
 fi
 
-P24_STATUS_URL="$NGROK_URL/payment/status" APP_URL="http://nginx" SESSION_DOMAIN="nginx" docker-compose up -d php
+P24_STATUS_URL="$NGROK_URL/payment/status" APP_URL="http://nginx" SESSION_DOMAIN="nginx" DEBUG_BAR="false" docker-compose up -d php
 
 printf "=======================================================\n"
 printf "To see what Selenium is doing open vnc://127.0.0.1:5900\n"
-printf "               The password is `secret`                \n"
+printf "              The password is \`secret\`               \n"
+printf "              Don't user \`yarn run hot\`              \n"
 printf "=======================================================\n"
 
 docker exec -it php /bin/sh -c 'php artisan dusk tests/Browser/Tests/Payment/PaymentTest.php'

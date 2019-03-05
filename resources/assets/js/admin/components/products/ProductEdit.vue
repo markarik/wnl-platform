5d09d8d38ce0a25acf35ad6ecb7adba3f0619ccb
Kuba Karmiński <jlkarminski@gmail.com>
2019-03-05 11:00:02 +0100

    [PLAT-997] in-place fields explanations

--- resources/assets/js/admin/components/products/ProductEdit.vue
+++ resources/assets/js/admin/components/products/ProductEdit.vue
@@ -15,55 +15,51 @@
 			<div class="columns">
 				<div class="column left">
 					<wnl-text name="name">Nazwa</wnl-text>
-					<wnl-text name="slug">Slug</wnl-text>
+					<wnl-text name="slug">
+						Slug<br>
+						<small>Unikatowy identyfikator kursu, na jego podstawie produkt wyświetla
+						się na stronie sprzedażowej.</small>
+					</wnl-text>
 					<div class="field is-grouped">
 						<wnl-text name="price">Cena brutto</wnl-text>
 						<wnl-select name="vat_rate" :options="vatRates">Stawka VAT</wnl-select>
 					</div>
-					<wnl-text name="vat_note">Opis VAT</wnl-text>
+					<wnl-text name="vat_note">
+						Opis VAT<br>
+					<small>Notatka dotycząca podstawy zwolnienia z podatku pojawiająca się
+						na fakturze. Zbędna w przypadku stawki 23%.</small>
+					</wnl-text>
 					<wnl-textarea name="invoice_name">Opis na fakturze</wnl-textarea>
 
 					<wnl-text name="quantity">Liczba (obecnie)</wnl-text>
 					<wnl-text name="initial">Liczba (początkowa)</wnl-text>
 				</div>
 
-				<div class="column">
+				<div class="column dates">
 					<wnl-datepicker name="course_start" :config="datepickerConfig">Start kursu</wnl-datepicker>
 					<wnl-datepicker name="course_end" :config="datepickerConfig">Koniec kursu</wnl-datepicker>
-
-					<hr>
 
 					<wnl-datepicker name="access_start" :config="datepickerConfig">Początek dostępu</wnl-datepicker>
 					<wnl-datepicker name="access_end" :config="datepickerConfig">Koniec dostępu</wnl-datepicker>
 
-					<hr>
-
 					<wnl-datepicker name="signups_start" :config="datepickerConfig">Start zapisów</wnl-datepicker>
-					<wnl-datepicker name="signups_end" :config="datepickerConfig">Koniec zapisów</wnl-datepicker>
-					<wnl-datepicker name="signups_close" :config="datepickerConfig">Zamknięcie zapisów</wnl-datepicker>
+					<wnl-datepicker name="signups_end" :config="datepickerConfig">
+						Koniec zapisów<br>
+						<small>Po tej dacie wciąż można zapisać się na kurs, ale wyświetlane jest
+						ostrzeżenie o tym, że kurs już się rozpoczął.</small>
+					</wnl-datepicker>
+					<wnl-datepicker name="signups_close" :config="datepickerConfig">
+						Zamknięcie zapisów<br>
+						<small>Po tej dacie nie można już zapisać się na kurs.</small>
+					</wnl-datepicker>
 
-					<hr>
-
-					<wnl-datepicker name="delivery_date" :config="datepickerConfig">Data dostawy</wnl-datepicker>
+					<wnl-datepicker name="delivery_date" :config="datepickerConfig">
+						Data dostawy<br>
+						<small>Data realizacji usługi. Zazwyczaj pokrywa się ze startem kursu.</small>
+					</wnl-datepicker>
 				</div>
 			</div>
 		</wnl-form>
-
-		<article class="message is-primary">
-			<div class="message-header">
-				<p>Legenda</p>
-			</div>
-			<div class="message-body plan-details">
-				<strong>Slug</strong> - unikatowy identyfikator kursu, na jego podstawie produkt wyświetla
-				się na stronie sprzedażowej.<br>
-				<strong>Opis VAT</strong> - notatka dotycząca podstawy zwolnienia z podatku pojawiająca się
-				na fakturze. Zbędna w przypadku stawki 23%.<br>
-				<strong>Koniec zapisów</strong> - po tej dacie wciąż można zapisać się na kurs, ale wyświetlane jest
-				ostrzeżenie o tym, że kurs już się rozpoczął.<br>
-				<strong>Zamknięcie zapisów</strong> - po tej dacie nie można już zapisać się na kurs.<br>
-				<strong>Data dostawy</strong> - data realizacji usługi, zazwyczaj pokrywa się ze startem kursu.<br>
-			</div>
-		</article>
 	</div>
 
 </template>
@@ -77,6 +73,10 @@
 				margin-right: $margin-medium
 		.column.left
 			border-right: $border-light-gray
+
+		.dates
+			.field
+				border-bottom: $border-light-gray
 </style>
 
 <script>

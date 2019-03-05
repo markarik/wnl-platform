5487409c38ee47fd056d0857edc0ee2c0ea75877
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-03-05 14:21:17 +0100

    PLAT-1055 | Remove questions

--- database/migrations/2019_03_05_083521_delete-workshops-tag.php
+++ database/migrations/2019_03_05_083521_delete-workshops-tag.php
@@ -14,6 +14,7 @@ class DeleteWorkshopsTag extends Migration
     {
 			$tag = Tag::where(['name' => 'Warsztaty'])->first();
 			if ($tag) {
+				$tag->questions()->delete();
 				$tag->delete();
 			}
     }

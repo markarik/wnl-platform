d116aef4b43f5474be07bc6bc3b6e58d8fbe79fe
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-03-06 13:13:04 +0100

    PLAT-1048 | Render tree in manual plan

--- resources/assets/js/components/user/plan/ManualPlan.vue
+++ resources/assets/js/components/user/plan/ManualPlan.vue
@@ -43,44 +43,11 @@
 					{{ $t('lessonsAvailability.viewsDropdownInfo') }}
 				</div>
 			</div>
-			<ul class="groups-list">
-				<li class="group" v-for="(item, index) in rootNodes"
-						:key="index">
-					<span class="item-toggle" @click="toggleItem(item)">
-						<span class="icon is-small">
-							<i class="toggle fa fa-angle-down"
-								 :class="{'fa-rotate-180': isOpen(item)}"></i>
-						</span>
-						<span class="item-name">{{item.model.name}}</span>
-						<span class="subitems-count" v-if="isGroup(item)">
-							({{getChildrenNodes(item.id).length}})
-						</span>
-					</span>
-					<ul class="subitems" v-if="isOpen(item)">
-						<li class="subitem" v-for="(subitem, index) in getChildrenNodes(item.id)"
-								:class="{'isEven': isEven(index)}"
-								:key="index">
-							<span class="subitem-name label"
-										:class="{'is-grayed-out': !subitem.model.isAccessible}"
-							>{{subitem.model.name}}</span>
-							<div class="subitem-left-side">
-								<div class="not-accesible" v-if="!subitem.model.isAccessible">
-									{{ $t('lessonsAvailability.lessonNotAvilable') }}
-								</div>
-								<div class="datepicker" v-else>
-									<wnl-datepicker
-										:class="{'hasColorBackground': isEven(index)}"
-										:value="getStartDate(subitem.model)"
-										:subitem-id="subitem.model.id"
-										:config="startDateConfig"
-										@onChange="(payload) => onStartDateChange(payload, subitem.model)"
-									/>
-								</div>
-							</div>
-						</li>
-					</ul>
-				</li>
-			</ul>
+			<wnl-manual-plan-nodes-list
+				:nodes="rootNodes"
+				@change="onStartDateChange"
+			/>
+
 			<div class="manual-start-dates" v-show="manualStartDates.length > 0">
 				<div class="level-left">
 					<div class="level-item">
@@ -138,52 +105,6 @@
 			.level-item
 				width: 100%
 
-	.groups-list
-		.group
-			margin-bottom: $margin-base
-			.item-toggle
-				color: $color-sky-blue
-				cursor: pointer
-				text-align: center
-				text-transform: uppercase
-				margin-bottom: $margin-small
-				width: 100%
-				.icon
-					color: $color-darkest-gray
-				.subitems-count
-					color: $color-background-gray
-					font-size: $font-size-minus-2
-
-			.subitems
-				display: flex
-				flex-direction: column
-				margin-bottom: $margin-small
-				.subitem
-					display: flex
-					flex-direction: row-reverse
-					justify-content: space-between
-					padding-bottom: $margin-small
-					padding-top: $margin-small
-					min-height: 35px
-					&.isEven
-						background-color: $color-background-lightest-gray
-					.subitem-name
-						align-self: flex-end
-						color: $color-darkest-gray
-						width: 65%
-						&.is-grayed-out
-							color: $color-gray
-					.subitem-left-side
-						align-items: center
-						display: flex
-						margin-right: $margin-small
-						.not-accesible
-							color: $color-gray
-							font-size: $font-size-plus-1
-							text-align: center
-							cursor: not-allowed
-							min-width: 260px
-
 	.accept-plan
 		display: flex
 		justify-content: space-around
@@ -192,8 +113,7 @@
 </style>
 
 <script>
-import TextOverlay from 'js/components/global/TextOverlay.vue';
-import Datepicker from 'js/components/global/Datepicker';
+import WnlTextOverlay from 'js/components/global/TextOverlay.vue';
 import { mapGetters, mapActions } from 'vuex';
 import moment from 'moment';
 import { getApiUrl } from 'js/utils/env';
@@ -201,19 +121,17 @@ import momentTimezone from 'moment-timezone';
 import { isEmpty } from 'lodash';
 import emits_events from 'js/mixins/emits-events';
 import features from 'js/consts/events_map/features.json';
-import {resources} from 'js/utils/constants';
-import {getModelByResource} from 'js/utils/config';
+import WnlManualPlanNodesList from 'js/components/user/plan/ManualPlanNodesList';
 
 export default {
 	name: 'ManualPlan',
 	components: {
-		'wnl-text-overlay': TextOverlay,
-		'wnl-datepicker': Datepicker,
+		WnlTextOverlay,
+		WnlManualPlanNodesList,
 	},
 	mixins: [emits_events],
 	data() {
 		return {
-			openGroups: [],
 			manualStartDates: [],
 			isLoading: false,
 			alertSuccess: {
@@ -232,16 +150,9 @@ export default {
 	},
 	computed: {
 		...mapGetters('course', [
-			'groups',
-			'userLessons',
 			'getChildrenNodes'
 		]),
 		...mapGetters(['currentUserId']),
-		startDateConfig() {
-			return {
-				...this.defaultDateConfig
-			};
-		},
 		rootNodes() {
 			return this.getChildrenNodes(null);
 		},
@@ -258,33 +169,13 @@ export default {
 	methods: {
 		...mapActions(['addAutoDismissableAlert']),
 		...mapActions('course', ['setStructure']),
-		isEven(index) {
-			return index % 2 === 0;
-		},
-		isGroup(item) {
-			return getModelByResource(resources.groups) === item.structurable_type;
-		},
-		getStartDate(item) {
-			return item.startDate ? new Date (item.startDate*1000) : new Date();
-		},
-		toggleItem(item) {
-			const index = this.openGroups.indexOf(item.id);
-			if (index > -1) {
-				this.openGroups.splice(index, 1);
-			} else {
-				this.openGroups.push(item.id);
-			}
-		},
-		isOpen(item) {
-			return this.openGroups.includes(item.id);
-		},
-		onStartDateChange(newStartDate, subitem) {
+		onStartDateChange({newStartDate, lesson}) {
 			if (!newStartDate[0]) return;
 
 			const lessonWithStartDate = {
-				lessonId: subitem.id,
-				lessonName: subitem.name,
-				oldDate: moment(subitem.startDate * 1000).format('LL'),
+				lessonId: lesson.id,
+				lessonName: lesson.name,
+				oldDate: moment(lesson.startDate * 1000).format('LL'),
 				startDate: newStartDate[0],
 				formatedStartDate: moment(newStartDate[0]).format('LL'),
 			};
---
+++ resources/assets/js/components/user/plan/ManualPlanNodesList.vue
@@ -0,0 +1,27 @@
+<template>
+	<ul>
+		<wnl-manual-plan-node-item
+			v-for="(item, index) in nodes"
+			:key="item.id"
+			:node="item"
+			:is-even="index % 2 === 0"
+			@change="$emit('change', $event)"
+		/>
+	</ul>
+</template>
+
+<script>
+import WnlManualPlanNodeItem from 'js/components/user/plan/ManualPlanNodeItem';
+
+export default {
+	components: {
+		WnlManualPlanNodeItem,
+	},
+	props: {
+		nodes: {
+			type: Array,
+			required: true
+		}
+	},
+};
+</script>

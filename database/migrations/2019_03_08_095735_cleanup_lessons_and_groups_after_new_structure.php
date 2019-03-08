ee148027fc7975e5584b7f627d3d4b1b5d9fa30c
Kuba Karmiński <jlkarminski@gmail.com>
2019-03-08 13:53:34 +0100

    [PLAT-1090] Fix indentation

--- database/migrations/2019_03_08_095735_cleanup_lessons_and_groups_after_new_structure.php
+++ database/migrations/2019_03_08_095735_cleanup_lessons_and_groups_after_new_structure.php
@@ -6,29 +6,29 @@ use Illuminate\Database\Migrations\Migration;
 
 class CleanupLessonsAndGroupsAfterNewStructure extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-			Schema::table('lessons', function (Blueprint $table) {
-				$table->dropColumn('order_number');
-				$table->dropColumn('group_id');
-			});
-			Schema::table('groups', function (Blueprint $table) {
-				$table->dropColumn('order_number');
-			});
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('lessons', function (Blueprint $table) {
+			$table->dropColumn('order_number');
+			$table->dropColumn('group_id');
+		});
+		Schema::table('groups', function (Blueprint $table) {
+			$table->dropColumn('order_number');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        // This migration is irreversible
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// This migration is irreversible
+	}
 }

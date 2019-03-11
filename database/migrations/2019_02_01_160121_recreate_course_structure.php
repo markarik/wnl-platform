babe1bccdcb9a94c34a5c022e5d7500e4603d545
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-03-11 12:02:33 +0100

    PLAT-1090 | Bring back empty migration to keep migrate:refresh working

---
+++ database/migrations/2019_02_01_160121_recreate_course_structure.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class RecreateCourseStructure
+ */
+class RecreateCourseStructure extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// This data migration is not valid anymore
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+}

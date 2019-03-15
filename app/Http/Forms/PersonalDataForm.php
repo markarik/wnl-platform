41ec8cc71639e99f0247f786719dc3698995984b
Igor Rogatty <igor.rogatty@bethink.pl>
2019-03-15 14:12:43 +0100

    Update app/Http/Forms/PersonalDataForm.php

--- app/Http/Forms/PersonalDataForm.php
+++ app/Http/Forms/PersonalDataForm.php
@@ -159,6 +159,8 @@ class PersonalDataForm extends Form
 			'personal_identity_number' => new ValidatePersonalIdentityNumber,
 		];
 
+		// If someone sends invalid `identity_number_type` then disable `identity_number` validator
+		// Request will fail anyway
 		if (!array_key_exists($identityNumberType, $validators)) {
 			return false;
 		}

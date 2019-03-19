4e98a1bd2080b722262be148fe1f65487d89b4e4
Bogna 'bognix' Knychala <bogna.ka@gmail.com>
2019-03-19 20:44:51 +0100

    fix the styling of account-continue on desktop

--- resources/views/payment/account-continue.blade.php
+++ resources/views/payment/account-continue.blade.php
@@ -1,22 +1,26 @@
 @extends('payment.layout')
 
 @section('content')
-	@include('payment.cart', [
-		'productName' => $product->name,
-		'productPrice' => $product->price,
-		'productAccessEnd' => $product->access_end,
-		'productPriceWithCoupon' => $productPriceWithCoupon,
-		'coupon' => $coupon,
-	])
-	<div class="payment-content t-account">
+	<div class="t-checkout">
+		@include('payment.cart', [
+			'productName' => $product->name,
+			'productPrice' => $product->price,
+			'productAccessEnd' => $product->access_end,
+			'productPriceWithCoupon' => $productPriceWithCoupon,
+			'coupon' => $coupon,
+		])
+		<div class="payment-content t-checkout__content t-account">
 		<span class="-mischka t-account__row -x-large a-icon -largeSpace">
 			<i class="fa fa-shopping-cart"></i>
 		</span>
-		<h2 class="t-account__row -textPlus2 -largeSpace">@lang('payment.account-continue-heading')</h2>
-		<a class="a-button -big t-account__row" href="{{route('payment-personal-data')}}" data-button="account-continue">
-			@lang('payment.account-continue-submit')
-		</a>
-		<a href="https://wiecejnizlek.pl" class="t-account__row">@lang('payment.account-continue-back-link')</a>
+			<h2 class="t-account__row -textPlus2 -largeSpace">@lang('payment.account-continue-heading')</h2>
+			<div class="m-buttonWithNote t-account__row">
+				<a class="a-button -big" href="{{route('payment-personal-data')}}" data-button="account-continue">
+					@lang('payment.account-continue-submit')
+				</a>
+				<a href="https://wiecejnizlek.pl" class="m-buttonWithNote__note -stormGray">@lang('payment.account-continue-back-link')</a>
+			</div>
+		</div>
 	</div>
 
 @endsection

92ba04564341a7d802b5eb061ed5325bd2902ca0
rogatty <igor.rogatty@bethink.pl>
2019-03-22 11:43:45 +0100

    PLAT-1126 | use code generated in FB admin panel

--- resources/views/payment/facebook-messenger.blade.php
+++ resources/views/payment/facebook-messenger.blade.php
@@ -1,8 +1,7 @@
+<div id="fb-root"></div>
 <script>
 	window.fbAsyncInit = function () {
 		FB.init({
-			appId: '{{env('FACEBOOK_PIXEL_APP_ID')}}',
-			autoLogAppEvents: true,
 			xfbml: true,
 			version: 'v3.2'
 		});
@@ -13,8 +12,15 @@
 		if (d.getElementById(id)) return;
 		js = d.createElement(s);
 		js.id = id;
-		js.src = "https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js";
+		js.src = 'https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js';
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 </script>
-<div class="fb-customerchat" page_id="1107802549311729" theme_color="#0D9EA0"></div>
+
+<div class="fb-customerchat"
+		 attribution=setup_tool
+		 page_id="1107802549311729"
+		 theme_color="#0D9EA0"
+		 logged_in_greeting="Cześ! Chętnie odpowiemy na Twoje pytania."
+		 logged_out_greeting="Cześ! Chętnie odpowiemy na Twoje pytania.">
+</div>

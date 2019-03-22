93f3123d920cd008f08f05260419c8d6c4d2909a
rogatty <igor.rogatty@bethink.pl>
2019-03-22 12:58:05 +0100

    PLAT-998 | add component for the first onboarding screen

---
+++ resources/assets/js/components/onboarding/OnboardingScreenWelcome.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<img src="{{imageUrl}}">
+		<h2>Cześć {{currentUserName}} 👋</h2>
+		<p>Zanim rozpoczniesz naukę, poznasz najważniejsze funkcje platformy i kursu ⭐</p>
+		<div>
+			<div>
+				<span>⏰</span>
+				<p>Wprowadzenie zajmie Tobie około 10min.</p>
+			</div>
+			<div>
+				<span>🔁</span>
+				<p>Wprowadzenie możesz zamknąc w dowolnym momencie. Bedzie na Ciebie czekać.</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="sass" rel="stylesheet/sass">
+	@import 'resources/assets/sass/variables'
+
+</style>
+
+<script>
+import {getImageUrl} from 'js/utils/env';
+
+export default {
+	components: {
+
+	},
+	computed: {
+		...mapGetters([
+			'currentUserName'
+		]),
+		imageUrl() {
+			return getImageUrl('onboarding-screen-welcome.png');
+		}
+	},
+};
+</script>

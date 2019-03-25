6d14e938f959d0cffb4a5b25c7629896eccb693c
Bogna 'bognix' Knychala <bogna.ka@gmail.com>
2019-03-25 15:29:16 +0100

    center buttons in modals and disable hover state on disabled checkboxes

--- resources/views/payment/personal-identity-number-modal.blade.php
+++ resources/views/payment/personal-identity-number-modal.blade.php
@@ -2,6 +2,8 @@
 	@lang('payment.personal-identity-number-modal-header') <i class="a-icon fa-info-circle -cadetBlue -medium"></i>
 </h3>
 <div class="-textMinus1 margin bottom">@lang('payment.personal-identity-number-modal-body')</div>
-<button id="personal-identity-number-modal-closer" class="a-button -big margin top">
-	@lang('payment.personal-identity-number-modal-button')
-</button>
+<div class="-textCenter">
+    <button id="personal-identity-number-modal-closer" class="a-button -big margin top">
+        @lang('payment.personal-identity-number-modal-button')
+    </button>
+</div>

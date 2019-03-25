c191b1984ab9a93d95704693d30ef9a0ecb63bb1
rogatty <igor.rogatty@bethink.pl>
2019-03-25 16:51:56 +0100

    PLAT-998 | hide plan editor until link is clicked

--- resources/assets/js/components/onboarding/OnboardingScreenUserPlan.vue
+++ resources/assets/js/components/onboarding/OnboardingScreenUserPlan.vue
@@ -5,14 +5,20 @@
 		<h3>Domyślny plan</h3>
 		<div>
 			<p>Proponowany przez nas plan pracy trwa od {{defaultPlanStartDate}}, zakłada pracę 5 dni w tygodniu przez 14 tygodni.</p>
-			<p>Możesz zmienić zakres dni, w których chcesz pracować, a my dostosujemy do nich Twój plan pracy – Edytuj plan</p>
+			<p>
+				Możesz zmienić zakres dni, w których chcesz pracować, a my dostosujemy do nich Twój plan pracy –
+				<a @click="openEditor">Edytuj plan</a>
+			</p>
 		</div>
 		<div>
 			<p>Wskazówka:</p>
 			<p>Plan zakłada optymalną kolejność przerabiania przedmiotów. Jeśli chcesz stworzyć indywidualny plan lub go edytować, możesz to zrobić w zakładce KONTO > PLAN PRACY.</p>
 		</div>
 
-		<wnl-automatic-plan :show-annotation="false" />
+		<wnl-automatic-plan
+			v-if="isEditorVisible"
+			:show-annotation="false"
+		/>
 	</div>
 </template>
 
@@ -36,12 +42,18 @@ export default {
 	data() {
 		return {
 			defaultPlanStartDate: null,
+			isEditorVisible: false,
 		};
+	},
+	methods: {
+		openEditor() {
+			this.isEditorVisible = true;
+		}
 	},
 	async mounted() {
 		const {data: {course_start}} = await axios.get(getApiUrl('products/current/paidCourse'));
 
 		this.defaultPlanStartDate = moment(course_start * 1000).format('LL');
-	}
+	},
 };
 </script>

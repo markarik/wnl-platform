f6287b6feadb112eb7494e041d558eb4cb2c9cb9
rogatty <igor.rogatty@bethink.pl>
2019-03-25 21:55:40 +0100

    PLAT-998 | change text on the user plan step for returning users

--- resources/assets/js/components/onboarding/OnboardingScreenUserPlan.vue
+++ resources/assets/js/components/onboarding/OnboardingScreenUserPlan.vue
@@ -1,24 +1,39 @@
 <template>
 	<div>
-		<h2>Plan pracy</h2>
+		<h2>Plan pracy 🗓</h2>
 		<p>Lekcje będą się otwierały zgodnie z ustalonymi przez Ciebie datami. Przed datą otwarcia lekcje pozostaną zamknięte.</p>
 		<h3>Domyślny plan</h3>
-		<div>
-			<p>Proponowany przez nas plan pracy trwa od {{defaultPlanStartDate}}, zakłada pracę 5 dni w tygodniu przez 14 tygodni.</p>
-			<p>
-				Możesz zmienić zakres dni, w których chcesz pracować, a my dostosujemy do nich Twój plan pracy –
-				<a @click="openEditor">Edytuj plan</a>
-			</p>
-		</div>
-		<div>
-			<p>Wskazówka:</p>
-			<p>Plan zakłada optymalną kolejność przerabiania przedmiotów. Jeśli chcesz stworzyć indywidualny plan lub go edytować, możesz to zrobić w zakładce KONTO > PLAN PRACY.</p>
-		</div>
 
-		<wnl-automatic-plan
-			v-if="isEditorVisible"
-			:show-annotation="false"
-		/>
+		<template v-if="isReturningUser">
+			<div>
+				<div>
+					<span>ℹ️</span>
+					<p>Na najbliższą edycję szykujemy nową Chirurgię (na 10 czerwca) i Medycynę ratunkową (na 19 sierpnia). Możesz jednak spokojnie zaplanować naukę!</p>
+					<p>Zakres materiału oraz struktura lekcji pozostaną takie same, jak obecnie. Nawet jeżeli zrealizujesz te lekcje przed ich aktualizacją, możesz bez przeszkód kontynuować kurs i mieć pewność pokrycia całości materiału.</p>
+				</div>
+				<div>
+					<p>Wskazówka:</p>
+					<p>Plan możesz zawsze zmienić w zakłade KONTO > Plan pracy.</p>
+				</div>
+			</div>
+		</template>
+		<template v-else>
+			<div>
+				<p>Proponowany przez nas plan pracy trwa od <strong>{{defaultPlanStartDate}}</strong>, zakłada pracę <strong>5 dni w tygodniu przez 14 tygodni</strong>.</p>
+				<p>
+					Możesz zmienić zakres dni, w których chcesz pracować, a my dostosujemy do nich Twój plan pracy –
+					<a @click="openEditor">Edytuj plan</a>
+				</p>
+			</div>
+			<div>
+				<p>Wskazówka:</p>
+				<p>Plan zakłada optymalną kolejność przerabiania przedmiotów. Jeśli chcesz stworzyć indywidualny plan lub go edytować, możesz to zrobić w zakładce KONTO > PLAN PRACY.</p>
+			</div>
+			<wnl-automatic-plan
+				v-if="isEditorVisible"
+				:show-annotation="false"
+			/>
+		</template>
 	</div>
 </template>
 
@@ -43,6 +58,7 @@ export default {
 		return {
 			defaultPlanStartDate: null,
 			isEditorVisible: false,
+			isReturningUser: false,
 		};
 	},
 	methods: {

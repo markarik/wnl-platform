99e4113c79c9514a7ccd205d9f39326d6757e016
Kuba Karmiński <jlkarminski@gmail.com>
2019-03-27 11:27:58 +0100

    [PLAT-685] Linter fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,7 @@ class LoginController extends Controller
 	/**
 	 * Show the application's login form.
 	 *
-	 * @return \Illuminate\Http\Response
+	 * @return \Illuminate\View\View
 	 */
 	public function showLoginForm()
 	{

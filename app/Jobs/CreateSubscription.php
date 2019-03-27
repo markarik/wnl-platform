7deb13aa7ade32b5e715faff0ab5909de0e8c07e
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-03-27 13:11:31 +0100

    PLAT-1148 | Fix whitespaces

--- app/Jobs/CreateSubscription.php
+++ app/Jobs/CreateSubscription.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 
 class CreateSubscription
 {
-    use Dispatchable, InteractsWithQueue, SerializesModels;
+	use Dispatchable, InteractsWithQueue, SerializesModels;
 
 	protected $order;
 
@@ -25,34 +25,34 @@ class CreateSubscription
 		$this->order = $order;
 	}
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle()
-    {
-	    \Log::notice("CreateSubscription called for order #{$this->order->id}");
-	    $product = $this->order->product;
-	    $user = $this->order->user;
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		\Log::notice("CreateSubscription called for order #{$this->order->id}");
+		$product = $this->order->product;
+		$user = $this->order->user;
 
-	    if (empty($product->access_start) && empty($product->access_end)) {
-		    return;
-	    }
+		if (empty($product->access_start) && empty($product->access_end)) {
+			return;
+		}
 
-	    $subscriptionAccessStart = $user->subscription->access_start ?? null;
-	    $subscriptionAccessEnd = $user->subscription->access_end ?? null;
+		$subscriptionAccessStart = $user->subscription->access_start ?? null;
+		$subscriptionAccessEnd = $user->subscription->access_end ?? null;
 
-	    $accessStart = $subscriptionAccessStart
-		    ? min([$subscriptionAccessStart, $product->access_start])
-		    : $product->access_start;
-	    $accessEnd = max([$subscriptionAccessEnd, $product->access_end]);
+		$accessStart = $subscriptionAccessStart
+			? min([$subscriptionAccessStart, $product->access_start])
+			: $product->access_start;
+		$accessEnd = max([$subscriptionAccessEnd, $product->access_end]);
 
-	    UserSubscription::updateOrCreate(
-		    ['user_id' => $user->id],
-		    ['access_start' => $accessStart, 'access_end' => $accessEnd]
-	    );
+		UserSubscription::updateOrCreate(
+			['user_id' => $user->id],
+			['access_start' => $accessStart, 'access_end' => $accessEnd]
+		);
 
-	    \Cache::forget(User::getSubscriptionKey($this->order->user->id));
-    }
+		\Cache::forget(User::getSubscriptionKey($this->order->user->id));
+	}
 }

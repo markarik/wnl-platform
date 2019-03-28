3f532cbaef4f6618e016a33f04f807c8180c3e12
Igor Rogatty <igor.rogatty@bethink.pl>
2019-03-28 10:08:03 +0100

    PLAT-1149 | fix interpunction
    Approved by Ola

--- resources/assets/js/i18n/pl/ui.js
+++ resources/assets/js/i18n/pl/ui.js
@@ -40,7 +40,7 @@ export const ui = {
 	accountDeleted: 'Konto usunięte',
 	satisfactionGuarantee: {
 		headline: '⚠️ Ta akcja może wpłynąć na Twoją Gwarancję Satysfakcji.',
-		body: 'Jeśli planujesz rozpoczęcie nauki od nowa pamiętaj, że należy spełnić konkretne kryteria, aby Gwarancja Satysfakcji była przyznana.',
+		body: 'Jeśli planujesz rozpoczęcie nauki od nowa, pamiętaj, że należy spełnić konkretne kryteria, aby Gwarancja Satysfakcji była przyznana.',
 		note: 'Wszystkie informacje na temat Gwarancji Satysfakcji znajdziesz w <a href="{url}" target="_blank">POMOC > Gwarancja Satysfakcji.</a>',
 		accept: 'Rozumiem',
 		close: 'Zamknij'

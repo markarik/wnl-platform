7964188901a7c12599c04a45fa554c6d1be93f2a
rogatty <igor.rogatty@bethink.pl>
2019-03-29 18:30:02 +0100

    Fix voucher page selectors in browser tests

--- tests/Browser/Pages/Payment/VoucherPage.php
+++ tests/Browser/Pages/Payment/VoucherPage.php
@@ -7,37 +7,37 @@ use Laravel\Dusk\Page as BasePage;
 
 class VoucherPage extends BasePage
 {
-    /**
-     * Get the URL for the page.
-     *
-     * @return string
-     */
-    public function url()
-    {
+	/**
+	 * Get the URL for the page.
+	 *
+	 * @return string
+	 */
+	public function url()
+	{
 		return '/payment/voucher';
-    }
+	}
 
-    /**
-     * Assert that the browser is on the page.
-     *
-     * @param  Browser  $browser
-     * @return void
-     */
-    public function assert(Browser $browser)
-    {
-        $browser->assertPathIs($this->url());
-    }
+	/**
+	 * Assert that the browser is on the page.
+	 *
+	 * @param Browser $browser
+	 * @return void
+	 */
+	public function assert(Browser $browser)
+	{
+		$browser->assertPathIs($this->url());
+	}
 
-    /**
-     * Get the element shortcuts for the page.
-     *
-     * @return array
-     */
-    public function elements()
-    {
-        return [
-            '@skip' => '.voucher-skip a',
-			'@use' => '.button'
-        ];
-    }
+	/**
+	 * Get the element shortcuts for the page.
+	 *
+	 * @return array
+	 */
+	public function elements()
+	{
+		return [
+			'@skip' => '[data-link="voucher-skip"]',
+			'@use' => '[data-button="voucher-submit"]'
+		];
+	}
 }

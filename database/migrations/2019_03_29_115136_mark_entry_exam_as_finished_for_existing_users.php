2a8a8ed1cd2eb7f1dcbf072dd0258aa65c10da8b
rogatty <7030884+rogatty@users.noreply.github.com>
2019-04-01 20:19:10 +0200

    PLAT-1139 | mark entry exam as finished for those who solved it recently

--- database/migrations/2019_03_29_115136_mark_entry_exam_as_finished_for_existing_users.php
+++ database/migrations/2019_03_29_115136_mark_entry_exam_as_finished_for_existing_users.php
@@ -11,7 +11,8 @@ class MarkEntryExamAsFinishedForExistingUsers extends Migration
 	 */
 	public function up()
 	{
-		DB::statement("UPDATE users SET has_finished_entry_exam = 1 WHERE created_at < '2019-03-30 00:00:00'");
+		// FIXME use exam_tag_id
+		DB::statement("UPDATE users SET has_finished_entry_exam = 1 WHERE created_at < '2019-03-30 00:00:00' OR id IN (SELECT exams_results.user_id FROM exams_results JOIN orders ON orders.user_id = exams_results.user_id WHERE orders.paid = 1 AND orders.product_id = 13 AND exams_results.created_at > '2019-04-01')");
 	}
 
 	/**
@@ -21,6 +22,6 @@ class MarkEntryExamAsFinishedForExistingUsers extends Migration
 	 */
 	public function down()
 	{
-		DB::statement("UPDATE users SET has_finished_entry_exam = 0 WHERE created_at < '2019-03-30 00:00:00'");
+		DB::statement("UPDATE users SET has_finished_entry_exam = 0;");
 	}
 }

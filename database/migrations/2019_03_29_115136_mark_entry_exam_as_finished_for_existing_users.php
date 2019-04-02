80548bc22cdcdc13ea08b1c8bc3b331e651ea471
rogatty <7030884+rogatty@users.noreply.github.com>
2019-04-02 12:34:51 +0200

    PLAT-1139 | use exam_tag_id to check only the entry exam

--- database/migrations/2019_03_29_115136_mark_entry_exam_as_finished_for_existing_users.php
+++ database/migrations/2019_03_29_115136_mark_entry_exam_as_finished_for_existing_users.php
@@ -11,8 +11,7 @@ class MarkEntryExamAsFinishedForExistingUsers extends Migration
 	 */
 	public function up()
 	{
-		// FIXME use exam_tag_id
-		DB::statement("UPDATE users SET has_finished_entry_exam = 1 WHERE created_at < '2019-03-30 00:00:00' OR id IN (SELECT exams_results.user_id FROM exams_results JOIN orders ON orders.user_id = exams_results.user_id WHERE orders.paid = 1 AND orders.product_id = 13 AND exams_results.created_at > '2019-04-01')");
+		DB::statement("UPDATE users SET has_finished_entry_exam = 1 WHERE created_at < '2019-03-30 00:00:00' OR id IN (SELECT exams_results.user_id FROM exams_results JOIN orders ON orders.user_id = exams_results.user_id WHERE orders.paid = 1 AND orders.product_id = 13 AND exams_results.created_at > '2019-04-01' AND exams_results.exam_tag_id = 552)");
 	}
 
 	/**

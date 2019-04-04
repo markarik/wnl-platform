ad4994e92b3d41040e713571b1b883be70b5b900
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-04-04 10:46:21 +0200

    PLAT-1173 | Separate OrderNotPaid spalshScreen

---
+++ resources/assets/js/components/global/splashscreens/OrderNotPaid.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="splash-screen scrollable-main-container">
+		<img class="splash-screen-image" :src="countdownImageUrl" alt="Logo kursu">
+		<div class="has-text-centered">
+			<p class="title is-4">Twoje zamówienie oczekuje na płatność</p>
+			<p class="margin vertical">
+							Na płatność masz <strong>7 dni</strong> od złożenia zamówienia.
+			</p>
+			<p class="margin vertical text-dimmed" v-if="currentProductAccessStartDateIsPast">Dostęp do kursu otrzymasz od razu po dokonaniu płatności.</p>
+			<p class="margin vertical text-dimmed" v-else>Dostęp do kursu otrzymasz od {{currentProductAccessStartDate}}.</p>
+			<p class="margin vertical">
+				<a :href="paymentUrl" class="button is-primary">
+					Opłać zamówienie
+				</a>
+			</p>
+		</div>
+	</div>
+</template>
+
+<style lang="sass" rel="stylesheet/sass" scoped>
+	@import 'resources/assets/sass/variables'
+
+	.splash-screen
+		align-items: center
+		display: flex
+		flex-direction: column
+		height: 100%
+		justify-content: center
+
+	.splash-screen-image
+		max-width: 400px
+		padding: 0 20px
+</style>
+
+<script>
+import moment from 'moment';
+import { getUrl } from 'js/utils/env';
+import { mapGetters } from 'vuex';
+
+require('moment-duration-format');
+
+export default {
+	computed: {
+		...mapGetters('products', ['getCurrentCourse']),
+		countdownImageUrl() {
+			return window.$wnl.course.productLogoBig;
+		},
+		paymentUrl() {
+			return getUrl('payment/account');
+		},
+		currentProductAccessStartDateIsPast() {
+			return this.getCurrentCourse && moment(this.getCurrentCourse.access_start * 1000).isBefore();
+		},
+		currentProductAccessStartDate() {
+			return this.getCurrentCourse && moment(this.getCurrentCourse.access_start * 1000).format('LL');
+		},
+	},
+};
+</script>
--- resources/assets/js/components/global/splashscreens/SplashScreen.vue
+++ resources/assets/js/components/global/splashscreens/SplashScreen.vue
@@ -1,46 +1,51 @@
 <template>
-    <div class="splash-screen__container">
-        <div class="splash-screen__content">
-            <wnl-upcoming-edition v-if="$upcomingEditionParticipant.isAllowed('access')"/>
-            <wnl-no-access v-else/>
-        </div>
-        <footer class="splash-screen__footer text-dimmed">
-            <p class="splash-screen__footer__text">
-                W razie problemów napisz do nas na Messengerze lub wyślij maila na adres: <a href="mailto:info@wiecejnizlek.pl">info@wiecejnizlek.pl</a>
-            </p>
-        </footer>
-    </div>
+	<div class="splash-screen__container">
+		<div class="splash-screen__content">
+					<wnl-upcoming-edition v-if="$upcomingEditionParticipant.isAllowed('access')"/>
+					<wnl-no-access v-else-if="currentUserAccountSuspended"/>
+					<wnl-order-not-paid v-else />
+		</div>
+		<footer class="splash-screen__footer text-dimmed">
+			<p class="splash-screen__footer__text">
+				W razie problemów napisz do nas na Messengerze lub wyślij maila na adres: <a href="mailto:info@wiecejnizlek.pl">info@wiecejnizlek.pl</a>
+			</p>
+		</footer>
+	</div>
 </template>
 
 <style lang="sass" scoped>
-    @import 'resources/assets/sass/variables'
+	@import 'resources/assets/sass/variables'
 
-    .splash-screen__container
-        display: flex
-        flex-direction: column
-        flex: 1 0 auto
-        height: 100%
-        width: 100%
+	.splash-screen__container
+		display: flex
+		flex-direction: column
+		flex: 1 0 auto
+		height: 100%
+		width: 100%
 
-    .splash-screen__content
-        flex: 1 0 auto
+	.splash-screen__content
+		flex: 1 0 auto
 
-    .splash-screen__footer
-        border-top: $border-light-gray
-        padding: $margin-big
+	.splash-screen__footer
+		border-top: $border-light-gray
+		padding: $margin-big
 
-        &__text
-            max-width: 320px
+		&__text
+			max-width: 320px
 </style>
 
 <script>
-import { set } from 'vue';
+import { mapGetters } from 'vuex';
 import upcomingEditionParticipant from 'js/perimeters/upcomingEditionParticipant';
 import WnlNoAccess from 'js/components/global/splashscreens/NoAccess';
 import WnlUpcomingEdition from 'js/components/global/splashscreens/UpcomingEdition';
+import WnlOrderNotPaid from 'js/components/global/splashscreens/OrderNotPaid';
 
 export default {
-	components: { WnlNoAccess, WnlUpcomingEdition },
+	components: { WnlNoAccess, WnlUpcomingEdition, WnlOrderNotPaid },
 	perimeters: [upcomingEditionParticipant],
+	computed: {
+		...mapGetters(['currentUserAccountSuspended']),
+	}
 };
 </script>

d0002ff389fb8917e4c6e7499e6912ca127d1e19
Bogna 'bognix' Knychala <bogna.ka@gmail.com>
2019-04-08 15:03:59 +0200

    PLAT-1208 | create splashscreens where account is suspended or order is canceled

---
+++ resources/assets/js/components/global/splashscreens/AccountSuspended.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<img class="splash-screen-image" :src="logoImageUrl" alt="Logo kursu">
+		<div class="has-text-centered">
+			<p class="title is-4">Twoje konto zostało zablokowane... 🙁</p>
+			<p v-if="instalmentsNotPaid" class="text-dimmed">
+				Zaległe raty możesz opłacić w zakładce <router-link :to="{name: 'my-orders'}">KONTO > Twoje zamówienia.</router-link>
+			</p>
+			<p v-else class="text-dimmed">
+				Aby uzyskać więcej informacji napisz do nas na Messengerze
+				lub wyślij maila na adres: <a href="mailto:info@wiecejnizlek.pl.">info@wiecejnizlek.pl.</a>
+			</p>
+		</div>
+	</div>
+</template>
+
+<style lang="sass" rel="stylesheet/sass" scoped>
+	@import 'resources/assets/sass/variables'
+
+	.splash-screen-image
+		max-width: 400px
+		padding: 0 20px
+</style>
+
+<script>
+export default {
+	props: {
+		instalmentsNotPaid: {
+			type: Boolean,
+			default: false
+		}
+	},
+	computed: {
+		logoImageUrl() {
+			return window.$wnl.course.productLogoWithStudents;
+		},
+	},
+};
+</script>
--- resources/assets/js/components/global/splashscreens/SplashScreen.vue
+++ resources/assets/js/components/global/splashscreens/SplashScreen.vue
@@ -3,7 +3,8 @@
 		<div class="splash-screen__content scrollable-main-container">
 			<wnl-text-loader v-if="isLoading"></wnl-text-loader>
 			<template v-else>
-				<wnl-no-access v-if="currentUserAccountSuspended"/>
+				<wnl-account-suspended v-if="currentUserAccountSuspended" :instalments-not-paid="instalmentsNotPaid"/>
+				<wnl-order-canceled v-else-if="orderCanceled"/>
 				<wnl-upcoming-edition v-else-if="$upcomingEditionParticipant.isAllowed('access')"/>
 				<wnl-order-not-paid v-else-if="latestCourseWaitingForPayment" />
 				<wnl-default-splash-screen v-else />
@@ -46,8 +47,10 @@
 <script>
 import axios from 'axios';
 import {mapGetters} from 'vuex';
+import moment from 'moment';
 
-import WnlNoAccess from 'js/components/global/splashscreens/NoAccess';
+import WnlAccountSuspended from 'js/components/global/splashscreens/AccountSuspended';
+import WnlOrderCanceled from 'js/components/global/splashscreens/OrderCanceled';
 import WnlUpcomingEdition from 'js/components/global/splashscreens/UpcomingEdition';
 import WnlOrderNotPaid from 'js/components/global/splashscreens/OrderNotPaid';
 import WnlDefaultSplashScreen from 'js/components/global/splashscreens/Default';
@@ -59,33 +62,43 @@ import {PRODUCTS_SLUGS} from 'js/consts/products';
 export default {
 	data() {
 		return {
-			latestCourseWaitingForPayment: false,
 			isLoading: true,
+			orders: []
 		};
 	},
-	components: { WnlNoAccess, WnlUpcomingEdition, WnlOrderNotPaid, WnlDefaultSplashScreen },
+	components: { WnlAccountSuspended, WnlUpcomingEdition, WnlOrderNotPaid, WnlDefaultSplashScreen, WnlOrderCanceled },
 	perimeters: [upcomingEditionParticipant],
 	computed: {
 		...mapGetters(['currentUserAccountSuspended']),
+		latestCourseOrders() {
+			return this.orders.filter(order => order.product.slug === PRODUCTS_SLUGS.SLUG_ONLINE);
+		},
+		latestCourseWaitingForPayment() {
+			return !this.latestCourseOrders.some(order => order.paid && !order.canceled);
+		},
+		orderCanceled() {
+			return !this.latestCourseOrders.some(order => order.paid)
+					&& this.latestCourseOrders.some(order => order.canceled);
+		},
+		instalmentsNotPaid() {
+			return this.orders.filter(order => {
+				return order.method === 'instalments' && order.paid;
+			}).some((order) => {
+				return order.instalments.instalments.some(instalment => {
+					return instalment.amount > instalment.paid_amount && moment(instalment.due_date).isBefore(new Date(), 'day');
+				});
+			});
+		}
 	},
 	methods: {
-		async getIsLatestCourseWaitingForPayment() {
+		async fetchOrders() {
 			const {data: orders} = await axios.get(getApiUrl('orders/all'));
-			const courseOrders = orders.filter(order => order.product.slug === PRODUCTS_SLUGS.SLUG_ONLINE);
-
-			if (courseOrders.length === 0) {
-				return false;
-			}
-
-			const maxCourseProductId = courseOrders.reduce((max, currentOrder) => currentOrder.product.id > max ? currentOrder.product.id : max, null);
-
-			return !courseOrders.filter(order => order.product.id === maxCourseProductId && !order.canceled)
-				.some(order => order.paid);
+			this.orders = orders;
 		},
 	},
 	async mounted() {
 		try {
-			this.latestCourseWaitingForPayment = await this.getIsLatestCourseWaitingForPayment();
+			await this.fetchOrders();
 		} catch (e) {
 			$wnl.logger.capture(e);
 		} finally {

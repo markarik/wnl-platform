13c7ea82e7f2923bef7076d62f03755dd7cbe0c7
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-04-09 09:44:13 +0200

    PLAT-1113 | Remove unuased constructor

--- app/Http/Controllers/Api/PublicApi/ProductsApiController.php
+++ app/Http/Controllers/Api/PublicApi/ProductsApiController.php
@@ -7,21 +7,10 @@ namespace App\Http\Controllers\Api\PublicApi;
 use App\Http\Controllers\Api\ApiController;
 use App\Http\Controllers\Api\PublicTransformers\ProductTransformer;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use League\Fractal\Resource\Collection;
 
 class ProductsApiController extends ApiController
 {
-	/**
-	 * ProductsApiController constructor.
-	 * @param Request $request
-	 */
-	public function __construct(Request $request)
-	{
-		parent::__construct($request);
-		$this->resourceName = config('papi.resources.products');
-	}
-
 	/**
 	 * @return \Illuminate\Http\JsonResponse
 	 */

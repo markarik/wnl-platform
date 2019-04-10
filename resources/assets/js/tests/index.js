566fff6a445019dd8f9c1f689e3e3c5c995f5077
rogatty <7030884+rogatty@users.noreply.github.com>
2019-04-10 18:46:19 +0200

    PLAT-1217 | add tests for SplashScreen.vue

--- resources/assets/js/tests/index.js
+++ resources/assets/js/tests/index.js
@@ -4,7 +4,11 @@ const Vue = require('vue');
 Vue.config.productionTip = false;
 
 $wnl = {
+	env: {
+		appUrl: 'http://test',
+	},
 	logger: {
+		capture: console.log,
 		warning: console.log
 	}
 };

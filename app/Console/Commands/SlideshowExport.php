e30bf159dd15dadbc4d175e4982d983bdaa22495
Kuba Karmiński <jlkarminski@gmail.com>
2019-04-12 18:29:43 +0200

    [PLAT-1228] Refactor - remove unwanted fields from export

--- app/Console/Commands/SlideshowExport.php
+++ app/Console/Commands/SlideshowExport.php
@@ -7,44 +7,38 @@ use App\Models\Slideshow;
 use Illuminate\Console\Command;
 use App\Models\Screen;
 use App\Models\Subsection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use DB;
+use phpDocumentor\Reflection\Types\Mixed_;
 use Storage;
 
 class SlideshowExport extends Command
 {
-	const IMAGE_PATTERN = '/<img.*src="(.*?)".*>/';
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'slideshow:export {screenId}';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'slideshow:export {screenId}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command that exports slideshow with all theirs connections including sides, presentables, sections subsections';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = '
+		Command that exports slideshow with all theirs connections including sides, 
+		presentables, sections subsections
+		';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
 		$screenId = $this->argument('screenId');
 		$screen = Screen::find($screenId);
 
@@ -58,49 +52,79 @@ class SlideshowExport extends Command
 		$sections = $screen->sections;
 		$subsections = $this->getSubsections($sections);
 		$presentables = collect()
-			->merge($this->getPresentables($slideshow->id, Slideshow::class))
-			->merge($this->getPresentables($sections->pluck('id'), Section::class))
-			->merge($this->getPresentables($subsections->pluck('id'), Subsection::class));
+			->merge($this->getPresentables([$slideshow->id], Slideshow::class))
+			->merge($this->getPresentables($sections->pluck('id')->toArray(), Section::class))
+			->merge($this->getPresentables($subsections->pluck('id')->toArray(), Subsection::class));
 
 		$data = [
-			'screen' => $screen->toArray(),
-			'slideshow' => $slideshow->toArray(),
-			'slides' => $slides->toArray(),
-			'sections' => $sections->toArray(),
-			'subsections' => $subsections->toArray(),
-			'presentables' => $presentables->toArray(),
+			'screen' => $this->removeUnwantedFieldsFromScreen($screen),
+			'slideshow' => $this->removeId($slideshow),
+			'slides' => $this->removeId($slides),
+			'sections' => $this->removeId($sections),
+			'subsections' => $this->removeId($subsections),
+			'presentables' => $this->removeId($presentables),
 		];
 
-		//delete unwanted keys for screen
-		unset($data['screen']['id']);
-		unset($data['screen']['discussion_id']);
-		unset($data['screen']['is_discussable']);
-		unset($data['screen']['sections']);
-
-		//delete unwanted keys for slides
-		foreach ($data['slides'] as $key => $slide) {
-			unset($slide['id']);
-			$slides[$key] = $slide;
-		}
-
-//		dd($data['slides']);
 
 		// TODO: Generate sensible filename or take if from argument.
 		Storage::put('exports/slideshow_export.json', json_encode($data));
 
 		return 0;
-    }
+	}
 
-	private function getSubsections(Collection $sections)
+	/**
+	 * @param $data
+	 * @return Collection|Model
+	 */
+	private function removeId($data)
+	{
+		if ($data instanceof Collection) {
+			return $data->except(['id'])->toArray();
+		}
+
+		if ($data instanceof Model) {
+			$data->toArray();
+			unset($data['id']);
+			return $data;
+		}
+	}
+
+	/**
+	 * @param Screen $screen
+	 * @return array
+	 */
+	private function removeUnwantedFieldsFromScreen(Screen $screen):array
+	{
+		$screenData = $screen->toArray();
+
+		unset($screenData['id']);
+		unset($screenData['discussion_id']);
+		unset($screenData['is_discussable']);
+		unset($screenData['sections']);
+
+		return $screenData;
+	}
+
+	/**
+	 * @param Collection $sections
+	 * @return Collection
+	 */
+	private function getSubsections(Collection $sections):Collection
 	{
 		return Subsection::whereIn('section_id', $sections->pluck('id'))->get();
 	}
 
-	private function getPresentables($ids, $modelName)
+	/**
+	 * @param $ids
+	 * @param $modelName
+	 * @return Collection
+	 */
+	private function getPresentables(array $ids, string $modelName):Collection
 	{
 		return DB::table('presentables')
-	            ->where('presentable_type', 'App\\Models\\' . $modelName)
-	            ->whereIn('presentable_id', (array) $ids)
-	            ->get();
+			->where('presentable_type', 'App\\Models\\' . $modelName)
+			->whereIn('presentable_id', $ids)
+			->get()
+			->except(['id']);
 	}
 }

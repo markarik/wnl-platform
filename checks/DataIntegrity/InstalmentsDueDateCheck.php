8dddae43356b38cafd34fc37b06aadccfccb535e
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2019-04-12 13:57:50 +0200

    Disable integrity check for products that don't have instalments enabled

--- checks/DataIntegrity/InstalmentsDueDateCheck.php
+++ checks/DataIntegrity/InstalmentsDueDateCheck.php
@@ -7,8 +7,10 @@ class InstalmentsDueDateCheck extends DataIntegrityCheck {
 			->selectRaw('orders.id')
 			->join('order_instalments', 'orders.id', '=', 'order_instalments.order_id')
 			->join('products', 'orders.product_id', '=', 'products.id')
+			->join('payment_method_product', 'orders.product_id', '=', 'payment_method_product.product_id')
+			->join('payment_methods', 'payment_method_product.payment_method_id', '=', 'payment_methods.id')
 			->whereRaw('order_instalments.due_date < products.signups_start')
-			->whereRaw('orders.product_id <> 8')
+			->whereRaw('payment_methods.slug = \'instalments\'')
 			->groupBy('orders.id')
 			->get();
 

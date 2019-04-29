837beca9ea89d1609aa91db8d651ddbba2a681cc
Kuba Karmiński <jlkarminski@gmail.com>
2019-04-29 13:47:29 +0200

    [PLAT-1247] exclude pages

--- database/migrations/2019_04_29_105922_mark_content_verified.php
+++ database/migrations/2019_04_29_105922_mark_content_verified.php
@@ -1,31 +1,51 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use App\Models\Comment;
+use App\Models\QnaAnswer;
+use App\Models\Page;
+
 
 class MarkContentVerified extends Migration
 {
 	// TODO: Update users list when it's ready!
 	const USERS_TO_MARK = [374];
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-    	$now = now();
-        \App\Models\QnaAnswer::whereIn('user_id', self::USERS_TO_MARK)->update(['verified_at' => $now]);
-        \App\Models\Comment::whereIn('user_id', self::USERS_TO_MARK)->update(['verified_at' => $now]);
-    }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-	    \App\Models\QnaAnswer::whereIn('user_id', self::USERS_TO_MARK)->update(['verified_at' => NULL]);
-	    \App\Models\Comment::whereIn('user_id', self::USERS_TO_MARK)->update(['verified_at' => NULL]);
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$now = now();
+		$excludedDiscussions = Page::whereNotNull('discussion_id')->pluck('discussion_id')->toArray();
+		$excludedAnswers = QnaAnswer::whereIn('user_id', self::USERS_TO_MARK)
+			->whereDoesntHave('question', function ($query) use ($excludedDiscussions) {
+				$query->whereNotIn('discussion_id', $excludedDiscussions);
+			})->pluck('id')->toArray();
+
+		QnaAnswer::whereIn('user_id', self::USERS_TO_MARK)
+			->whereNotIn('id', $excludedAnswers)
+			->update(['verified_at' => $now]);
+
+		Comment::whereIn('user_id', self::USERS_TO_MARK)
+			->whereDoesntHave('commentable', function($query) use ($excludedAnswers) {
+				$query
+					->where('commentable_type', QnaAnswer::class)
+					->whereIn('commentable_id', $excludedAnswers);
+			})
+			->update(['verified_at' => $now]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		QnaAnswer::whereIn('user_id', self::USERS_TO_MARK)->update(['verified_at' => NULL]);
+		Comment::whereIn('user_id', self::USERS_TO_MARK)->update(['verified_at' => NULL]);
+	}
 }

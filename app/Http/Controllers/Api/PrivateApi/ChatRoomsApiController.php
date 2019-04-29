58009b9182a28ce08f2fbb05e2bf4a2c5b9a13dd
rogatty <7030884+rogatty@users.noreply.github.com>
2019-04-29 09:57:31 +0200

    PLAT-1124 | use eagerLoadIncludes instead of with

--- app/Http/Controllers/Api/PrivateApi/ChatRoomsApiController.php
+++ app/Http/Controllers/Api/PrivateApi/ChatRoomsApiController.php
@@ -29,7 +29,7 @@ class ChatRoomsApiController extends ApiController
 		];
 
 		/** @var Builder $rooms */
-		$rooms = ChatRoom::with('users.profile', 'users.profile.roles')
+		$rooms = $this->eagerLoadIncludes(ChatRoom::class)
 			->select(\DB::raw(implode(',', $select)))
 			->join(
 				'chat_messages',
